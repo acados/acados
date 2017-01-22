459e5d127ed017f1b86d2c522fdf5548004b0445
Dimitris Kouzoupis <dimitris.kouzoupis@imtek.uni-freiburg.de>
2017-01-22 19:35:46 +0100

    Revert "Revert "forgot install command in ext project""
    This reverts commit 7abe8a30dada5cbbb41edce782501ee5904babf9.

--- cmake/external/ma27.cmake
+++ cmake/external/ma27.cmake
@@ -7,6 +7,7 @@ ExternalProject_Add(
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/coinhsl"
     BUILD_IN_SOURCE 1
     BUILD_COMMAND make
+    INSTALL_COMMAND ""
 )
 
 ExternalProject_Add_Step(ma27_project cp_lib
