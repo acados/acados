516b4dda5263c423be36d22e797a7233af449c12
Robin <robin.verschueren@gmail.com>
2017-01-30 11:55:45 +0100

    Try to find numpy on travis

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -53,7 +53,8 @@ elseif(__numpy_out)
   message(STATUS "Python executable not found.")
 endif(PYTHON_EXECUTABLE)
 
-set(PYTHON_NUMPY_INCLUDE_DIR ${__numpy_path})
+find_path(PYTHON_NUMPY_INCLUDE_DIR numpy/arrayobject.h
+  HINTS "${__numpy_path}" "${PYTHON_INCLUDE_PATH}" NO_DEFAULT_PATH)
 
 if(PYTHON_NUMPY_INCLUDE_DIR)
   set(PYTHON_NUMPY_FOUND 1 CACHE INTERNAL "Python numpy found")
