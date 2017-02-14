c1217f0324395fbbd1a07f3af88f8ff3a5fb1a21
Robin <robin.verschueren@gmail.com>
2017-02-14 18:38:18 +0100

    Remove unecessary include in ocp qpOASES

--- acados/ocp_qp/ocp_qp_condensing_qpoases.c
+++ acados/ocp_qp/ocp_qp_condensing_qpoases.c
@@ -29,7 +29,6 @@
     #pragma clang diagnostic ignored "-Wtautological-pointer-compare"
     #pragma clang diagnostic ignored "-Wunused-parameter"
     #pragma clang diagnostic ignored "-Wunused-function"
-    #include "qpOASES_e/Constants.h"
     #include "qpOASES_e/QProblemB.h"
     #include "qpOASES_e/QProblem.h"
     #pragma clang diagnostic pop
@@ -39,14 +38,12 @@
         #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
         #pragma GCC diagnostic ignored "-Wunused-parameter"
         #pragma GCC diagnostic ignored "-Wunused-function"
-        #include "qpOASES_e/Constants.h"
         #include "qpOASES_e/QProblemB.h"
         #include "qpOASES_e/QProblem.h"
         #pragma GCC diagnostic pop
     #else
         #pragma GCC diagnostic ignored "-Wunused-parameter"
         #pragma GCC diagnostic ignored "-Wunused-function"
-        #include "qpOASES_e/Constants.h"
         #include "qpOASES_e/QProblemB.h"
         #include "qpOASES_e/QProblem.h"
     #endif
