783483e655d00cbeebf4115621a339a443036127
Robin <robin.verschueren@gmail.com>
2017-03-14 16:03:40 +0100

    Some cosmetics

--- acados/ocp_qp/ocp_qp_common.h
+++ acados/ocp_qp/ocp_qp_common.h
@@ -58,7 +58,7 @@ typedef struct {
 } ocp_qp_out;
 
 typedef struct {
-    int_t (*fun)(ocp_qp_in*, ocp_qp_out*, void *, void*, void*);
+    int_t (*fun)(ocp_qp_in *, ocp_qp_out *, void *, void *, void *);
     ocp_qp_in *qp_in;
     ocp_qp_out *qp_out;
     void *args;
