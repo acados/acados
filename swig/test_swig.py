c22723697384b1fe3089cc5796d16fc1940e3e05
Robin <robin.verschueren@gmail.com>
2017-03-22 09:59:01 +0100

    Test casadi python on travis

--- swig/test_swig.py
+++ swig/test_swig.py
@@ -1,23 +1,23 @@
 from acados import *
 from numpy import array, ones, zeros, diag
 
-qp_in = ocp_qp_in({'N':5, 'nx':2, 'nu':1})
-x0 = array([1,1])
+# qp_in = ocp_qp_in({'N':5, 'nx':2, 'nu':1})
+# x0 = array([1,1])
 
-# specify OCP
-qp_in.A = array([[0,1],[0,0]])
-qp_in.B = array([[0],[1]])
-qp_in.Q = diag([1,1])
-qp_in.R = diag([1])
+# # specify OCP
+# qp_in.A = array([[0,1],[0,0]])
+# qp_in.B = array([[0],[1]])
+# qp_in.Q = diag([1,1])
+# qp_in.R = diag([1])
 
-# specify initial condition
-x0 = array([1,1])
-qp_in.lb[0] = x0
-qp_in.ub[0] = x0
+# # specify initial condition
+# x0 = array([1,1])
+# qp_in.lb[0] = x0
+# qp_in.ub[0] = x0
 
-solver = ocp_qp_solver("condensing_qpoases", qp_in)
-result = solver.solve()
-print(result)
+# solver = ocp_qp_solver("condensing_qpoases", qp_in)
+# result = solver.solve()
+# print(result)
 
 from casadi import *
 
@@ -32,19 +32,24 @@ u = SX.sym('u', nu)
 mu = 0.5
 rhs = vertcat(x[1] + u*(mu + (1.-mu)*x[0]), x[0] + u*(mu - 4.*(1.-mu)*x[1]))
 
-ode = Function('ode', [x,u], [rhs])
 
-Sx = SX.sym('Sx', nx, nx)
-Su = SX.sym('Su', nx, nu)
+print(rhs)
 
-vde_x = jtimes(rhs, x, Sx)
-vde_u = jacobian(rhs, u) + jtimes(rhs, x, Su)
+# ode = Function('ode', [x, u], [rhs])
+# jac_ode = Function('jac_ode', [x, u], [rhs, jacobian(rhs, x)])
 
-vde = Function('vde', [x, Sx, Su, u], [rhs, vde_x, vde_u])
+# Sx = SX.sym('Sx', nx, nx)
+# Su = SX.sym('Su', nx, nu)
 
-ode.generate('ode.c', {'with_header':True})
-vde.generate('vde.c', {'with_header':True})
+# vde_x = jtimes(rhs, x, Sx)
+# vde_u = jacobian(rhs, u) + jtimes(rhs, x, Su)
 
-nlp = ocp_nlp_in()
-a = nlp.set_model("ode")
-print(a)
+# vde = Function('vde', [x, Sx, Su, u], [rhs, vde_x, vde_u])
+
+# ode.generate('ode.c')
+# jac_ode.generate('jac_ode.c')
+# vde.generate('vde.c')
+
+# nlp = ocp_nlp_in()
+# a = nlp.set_model('ode', 5)
+# print(a)
