f67e2f68e15116c316569912d04704d98520d2d0
Dang Doan <minhdang@linux.com>
2017-04-04 11:03:40 +0200

    Cleanup old codes for esp32

--- experimental/dang/esp32/test_ocp_qp_hpmpc/include/blasfeo_target.h
+++
@@ -1,7 +0,0 @@
-// acados/external/blasfeo/include/blasfeo_target.h
-#ifndef TARGET_GENERIC
-#define TARGET_GENERIC
-#endif
-#ifndef LA_REFERENCE
-#define LA_REFERENCE
-#endif
--- experimental/dang/esp32/test_ocp_qp_hpmpc/include/target.h
+++
@@ -1,7 +0,0 @@
-// acados/external/hpmpc/include/target.h
-#ifndef TARGET_C99_4X4
-#define TARGET_C99_4X4
-#endif
-#ifndef BLASFEO
-#define BLASFEO
-#endif
