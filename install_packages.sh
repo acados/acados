b25be7f2eb128ca50a29273ed11a725b516f2496
Robin <robin.verschueren@gmail.com>
2017-04-15 19:39:27 -0700

    Supply install script to travis

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,22 @@
-#/usr/bin/env bash
+#!/bin/sh
 
-sudo apt-get install cppcheck
+pip install numpy
+sudo add-apt-repository -y ppa:octave/stable
+sudo apt-get update -yq
+sudo apt-get install octave liboctave-dev valgrind
+
+pushd external
+wget http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
+mkdir eigen
+tar -xvf 3.2.10.tar.gz --strip-components=1 -C eigen
+
+wget http://files.casadi.org/3.1.1/linux/casadi-octave-v3.1.1.tar.gz
+mkdir casadi-octave-v3.1.1
+tar -xvf casadi-octave-v3.1.1.tar.gz -C casadi-octave-v3.1.1
+
+wget http://files.casadi.org/3.1.1/linux/casadi-py35-np1.9.1-v3.1.1.tar.gz
+mkdir casadi-py35-np1.9.1-v3.1.1
+tar -xvf casadi-py35-np1.9.1-v3.1.1.tar.gz -C casadi-py35-np1.9.1-v3.1.1
+
+export PYTHONPATH=$(pwd)/casadi-py35-np1.9.1-v3.1.1:$PYTHONPATH
+popd
