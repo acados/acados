1e9138154ed74919bc751f2a59c0d63f22c876b5
Robin <robin.verschueren@gmail.com>
2017-04-19 16:14:28 -0700

    Fix Dang's script: had to rename some of the examples

--- cmake/acados_sources.cmake
+++
@@ -1,8 +0,0 @@
-# Build list with all source files to go into the acados library
-file(GLOB_RECURSE ACADOS_SRC ${PROJECT_SOURCE_DIR}/acados/*.c)
-# Exclude helper files
-list(REMOVE_ITEM ACADOS_SRC ${PROJECT_SOURCE_DIR}/acados/ocp_qp/condensing_helper_functions.c)
-
-if (NOT EXISTS ${PROJECT_SOURCE_DIR}/external/OOQP)
-    list(REMOVE_ITEM ACADOS_SRC ${PROJECT_SOURCE_DIR}/acados/ocp_qp/ocp_qp_ooqp.c)
-endif()
