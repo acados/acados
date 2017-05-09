30e55533e2f4f554c6876e1650c1d5b892825475
Robin <robin.verschueren@gmail.com>
2017-05-09 13:46:22 -0700

    Limit build output

--- install_packages.sh
+++ install_packages.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash -xe
 
-sudo add-apt-repository -y ppa:octave/stable
-sudo apt-get update -yq
-sudo apt-get install $CXX $CC libgsl0-dev liblapack-dev libopenblas-dev liboctave-dev
+sudo add-apt-repository -yqq ppa:octave/stable
+sudo apt-get update -yqq
+sudo apt-get install -yqq $CXX $CC libgsl0-dev liblapack-dev libopenblas-dev liboctave-dev
 
 pip install numpy scipy matplotlib
 
 pushd external
-wget http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
+wget -q http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
 mkdir -p eigen
-tar -xvf 3.2.10.tar.gz --strip-components=1 -C eigen
+tar -xf 3.2.10.tar.gz --strip-components=1 -C eigen
 
-wget http://files.casadi.org/3.1.1/linux/casadi-octave-v3.1.1.tar.gz
+wget -q http://files.casadi.org/3.1.1/linux/casadi-octave-v3.1.1.tar.gz
 mkdir -p casadi-octave-v3.1.1
-tar -xvf casadi-octave-v3.1.1.tar.gz -C casadi-octave-v3.1.1
+tar -xf casadi-octave-v3.1.1.tar.gz -C casadi-octave-v3.1.1
 
-wget http://files.casadi.org/3.1.1/linux/casadi-py35-np1.9.1-v3.1.1.tar.gz
+wget -q http://files.casadi.org/3.1.1/linux/casadi-py35-np1.9.1-v3.1.1.tar.gz
 mkdir -p casadi-py35-np1.9.1-v3.1.1
-tar -xvf casadi-py35-np1.9.1-v3.1.1.tar.gz -C casadi-py35-np1.9.1-v3.1.1
+tar -xf casadi-py35-np1.9.1-v3.1.1.tar.gz -C casadi-py35-np1.9.1-v3.1.1
 export PYTHONPATH=$(pwd)/casadi-py35-np1.9.1-v3.1.1:$PYTHONPATH
 
-wget https://sourceforge.net/projects/casadi/files/CasADi/3.1.1/linux/casadi-matlabR2014b-v3.1.1.tar.gz
+wget -q https://sourceforge.net/projects/casadi/files/CasADi/3.1.1/linux/casadi-matlabR2014b-v3.1.1.tar.gz
 mkdir -p casadi-matlabR2014b-v3.1.1
-tar -xvf casadi-matlabR2014b-v3.1.1.tar.gz -C casadi-matlabR2014b-v3.1.1
+tar -xf casadi-matlabR2014b-v3.1.1.tar.gz -C casadi-matlabR2014b-v3.1.1
 export MATLABPATH=$(pwd)/casadi-matlabR2014b-v3.1.1:$MATLABPATH
 
 pushd swig
@@ -30,7 +30,7 @@ pushd swig
 ./autogen.sh
 ./configure --prefix=$(pwd)/swig_install --enable-silent-rules
 make
-make install
+make install > /dev/null # quiet installation
 export PATH=$(pwd):$PATH
 
 popd # swig
