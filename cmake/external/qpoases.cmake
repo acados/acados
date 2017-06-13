aad7b5b6dff78aabc4cdca8ffcf741f47ef50078
Robin <robin.verschueren@gmail.com>
2017-06-13 14:54:33 -0700

    Install qpOASES the proper way

--- cmake/external/qpoases.cmake
+++ cmake/external/qpoases.cmake
@@ -6,12 +6,12 @@ ExternalProject_Add(
     CONFIGURE_COMMAND ""
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/qpOASES"
     BINARY_DIR "${PROJECT_SOURCE_DIR}/external/qpOASES/build"
-    CONFIGURE_COMMAND cmake ..
+    CONFIGURE_COMMAND cmake -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ..
     BUILD_COMMAND cmake --build .
-    INSTALL_COMMAND ""
+    INSTALL_COMMAND cmake --build . --target install
 )
 
-ExternalProject_Get_Property(qpoases_project source_dir)
-add_library(qpoases STATIC IMPORTED GLOBAL)
-add_dependencies(qpoases qpoases_project)
-set_property(TARGET qpoases PROPERTY IMPORTED_LOCATION "${source_dir}/build/lib/libqpOASES_e.a")
+# ExternalProject_Get_Property(qpoases_project source_dir)
+# add_library(qpoases STATIC IMPORTED GLOBAL)
+# add_dependencies(qpoases qpoases_project)
+# set_property(TARGET qpoases PROPERTY IMPORTED_LOCATION "${source_dir}/build/lib/libqpOASES_e.a")
