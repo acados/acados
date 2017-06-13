b1e755799238d16cad1c77c890d7c313cb356ad8
Robin <robin.verschueren@gmail.com>
2017-06-13 16:27:00 -0700

    Revive unit tests

--- cmake/external/qpoases.cmake
+++ cmake/external/qpoases.cmake
@@ -10,8 +10,3 @@ ExternalProject_Add(
     BUILD_COMMAND cmake --build .
     INSTALL_COMMAND cmake --build . --target install
 )
-
-# ExternalProject_Get_Property(qpoases_project source_dir)
-# add_library(qpoases STATIC IMPORTED GLOBAL)
-# add_dependencies(qpoases qpoases_project)
-# set_property(TARGET qpoases PROPERTY IMPORTED_LOCATION "${source_dir}/build/lib/libqpOASES_e.a")
