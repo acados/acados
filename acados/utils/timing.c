cffc7c3bdd7695f2c212f019a02b5aeb729fdefa
rienq <rien.quirynen@gmail.com>
2017-06-12 21:04:09 -0400

    towards Windows compatibility

--- acados/utils/timing.c
+++ acados/utils/timing.c
@@ -19,13 +19,33 @@
 
 #include "acados/utils/timing.h"
 
-#if defined(__APPLE__)
-void acado_tic(acado_timer* t) {
+#if !(defined _DSPACE)
+#if (defined _WIN32 || defined _WIN64) && !(defined __MINGW32__ || defined __MINGW64__)
+
+void acado_tic( acado_timer* t )
+{
+	QueryPerformanceFrequency(&t->freq);
+	QueryPerformanceCounter(&t->tic);
+}
+
+real_t acado_toc( acado_timer* t )
+{
+	QueryPerformanceCounter(&t->toc);
+	return ((t->toc.QuadPart - t->tic.QuadPart) / (real_t)t->freq.QuadPart);
+}
+
+
+#elif (defined __APPLE__)
+
+void acado_tic( acado_timer* t )
+{
     /* read current clock cycles */
     t->tic = mach_absolute_time();
 }
 
-real_t acado_toc(acado_timer* t) {
+real_t acado_toc( acado_timer* t )
+{
+
     uint64_t duration; /* elapsed time in clock cycles*/
 
     t->toc = mach_absolute_time();
@@ -38,25 +58,72 @@ real_t acado_toc(acado_timer* t) {
 
     return (real_t)duration / 1e9;
 }
+
 #else
-void acado_tic(acado_timer* t) {
-    /* read current clock cycles */
-    gettimeofday(&t->tic, 0);
+
+#if __STDC_VERSION__ >= 199901L
+/* C99 mode */
+
+/* read current time */
+void acado_tic( acado_timer* t )
+{
+	gettimeofday(&t->tic, 0);
 }
 
-real_t acado_toc(acado_timer* t) {
-    struct timeval temp;
+/* return time passed since last call to tic on this timer */
+real_t acado_toc( acado_timer* t )
+{
+	struct timeval temp;
 
-    gettimeofday(&t->toc, 0);
+	gettimeofday(&t->toc, 0);
 
-    if ((t->toc.tv_usec - t->tic.tv_usec) < 0) {
-        temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec - 1;
-        temp.tv_usec = 1000000 + t->toc.tv_usec - t->tic.tv_usec;
-    } else {
-        temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec;
-        temp.tv_usec = t->toc.tv_usec - t->tic.tv_usec;
-    }
+	if ((t->toc.tv_usec - t->tic.tv_usec) < 0)
+	{
+		temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec - 1;
+		temp.tv_usec = 1000000 + t->toc.tv_usec - t->tic.tv_usec;
+	}
+	else
+	{
+		temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec;
+		temp.tv_usec = t->toc.tv_usec - t->tic.tv_usec;
+	}
 
-    return (real_t)temp.tv_sec + (real_t)temp.tv_usec / 1e6;
+	return (real_t)temp.tv_sec + (real_t)temp.tv_usec / 1e6;
 }
+
+#else
+/* ANSI */
+
+/* read current time */
+void acado_tic( acado_timer* t )
+{
+	clock_gettime(CLOCK_MONOTONIC, &t->tic);
+}
+
+
+/* return time passed since last call to tic on this timer */
+real_t acado_toc( acado_timer* t )
+{
+	struct timespec temp;
+
+	clock_gettime(CLOCK_MONOTONIC, &t->toc);
+
+	if ((t->toc.tv_nsec - t->tic.tv_nsec) < 0)
+	{
+		temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec - 1;
+		temp.tv_nsec = 1000000000+t->toc.tv_nsec - t->tic.tv_nsec;
+	}
+	else
+	{
+		temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec;
+		temp.tv_nsec = t->toc.tv_nsec - t->tic.tv_nsec;
+	}
+
+	return (real_t)temp.tv_sec + (real_t)temp.tv_nsec / 1e9;
+}
+
+#endif /* __STDC_VERSION__ >= 199901L */
+
+#endif /* (defined _WIN32 || _WIN64) */
+
 #endif
