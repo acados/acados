548dcb294a7fd3c937e177be32a43b79ee15af8b
Robin <robin.verschueren@gmail.com>
2017-06-13 18:49:55 -0700

    include qpOASES as subdirectory instead of external project

--- cmake/external/qpoases.cmake
+++ cmake/external/qpoases.cmake
@@ -8,7 +8,8 @@ ExternalProject_Add(
     BINARY_DIR "${PROJECT_SOURCE_DIR}/external/qpOASES/build"
     CONFIGURE_COMMAND cmake -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ..
     BUILD_COMMAND cmake --build .
-    INSTALL_COMMAND cmake --build . --target install
+    # INSTALL_COMMAND cmake --build . --target install
+    INSTALL_COMMAND ""
     LOG_CONFIGURE 1  # suppress output
     LOG_BUILD 1
 )
