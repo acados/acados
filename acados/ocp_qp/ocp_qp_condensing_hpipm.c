36c914a0d0a2b5fd403b691d8c37fdd121c96972
giaf <gianfri2008@hotmail.it>
2017-06-30 22:21:39 +0200

    added condensing hpipm ipm interface

--- acados/ocp_qp/ocp_qp_condensing_hpipm.c
+++ acados/ocp_qp/ocp_qp_condensing_hpipm.c
@@ -17,7 +17,7 @@
  *
  */
 
-#include "acados/ocp_qp/ocp_qp_hpipm.h"
+#include "acados/ocp_qp/ocp_qp_condensing_hpipm.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,7 +31,10 @@
 
 #include "hpipm/include/hpipm_d_ocp_qp.h"
 #include "hpipm/include/hpipm_d_ocp_qp_sol.h"
-#include "hpipm/include/hpipm_d_ocp_qp_ipm_hard.h"
+#include "hpipm/include/hpipm_d_dense_qp.h"
+#include "hpipm/include/hpipm_d_dense_qp_sol.h"
+#include "hpipm/include/hpipm_d_dense_qp_ipm_hard.h"
+#include "hpipm/include/hpipm_d_cond.h"
 
 #include "acados/ocp_qp/ocp_qp_common.h"
 #include "acados/utils/types.h"
@@ -40,38 +43,61 @@
 
 
 
-int ocp_qp_hpipm_calculate_workspace_size(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args) {
+int ocp_qp_condensing_hpipm_calculate_workspace_size(ocp_qp_in *qp_in, ocp_qp_condensing_hpipm_args *args) {
 //
 	return 0;
 }
 
 
 
-int ocp_qp_hpipm_calculate_memory_size(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args) {
+int ocp_qp_condensing_hpipm_calculate_memory_size(ocp_qp_in *qp_in, ocp_qp_condensing_hpipm_args *args) {
 //
 	int ii;
 
+	// extract ocp qp in size
 	int N = qp_in->N;
 	int *nx = (int *) qp_in->nx;
 	int *nu = (int *) qp_in->nu;
 	int *nb = (int *) qp_in->nb;
 	int *ng = (int *) qp_in->nc;
+	int **hidxb = (int **) qp_in->idxb;
 
+	// dummy ocp qp
 	struct d_ocp_qp qp;
 	qp.N = N;
 	qp.nx = nx;
 	qp.nu = nu;
 	qp.nb = nb;
 	qp.ng = ng;
+	qp.idxb = hidxb;
 
-	struct d_ipm_hard_ocp_qp_arg ipm_arg;
+	// compute dense qp size
+	int nvd = 0;
+	int ned = 0;
+	int nbd = 0;
+	int ngd = 0;
+	d_compute_qp_size_ocp2dense(N, nx, nu, nb, hidxb, ng, &nvd, &ned, &nbd, &ngd);
+
+	// dummy dense qp
+	struct d_dense_qp qpd;
+	qpd.nv = nvd;
+	qpd.ne = ned;
+	qpd.nb = nbd;
+	qpd.ng = ngd;
+
+	// dummy ipm arg
+	struct d_ipm_hard_dense_qp_arg ipm_arg;
 	ipm_arg.iter_max = args->iter_max;
 
+	// size in bytes
 	int size = 0;
 
 	size += d_memsize_ocp_qp(N, nx, nu, nb, ng);
 	size += d_memsize_ocp_qp_sol(N, nx, nu, nb, ng);
-	size += d_memsize_ipm_hard_ocp_qp(&qp, &ipm_arg);
+	size += d_memsize_dense_qp(nvd, ned, nbd, ngd);
+	size += d_memsize_dense_qp_sol(nvd, ned, nbd, ngd);
+	size += d_memsize_cond_qp_ocp2dense(&qp, &qpd);
+	size += d_memsize_ipm_hard_dense_qp(&qpd, &ipm_arg);
 	size += 4*(N+1)*sizeof(double *); // lam_lb lam_ub lam_lg lam_ug
 	size += 1*(N+1)*sizeof(int *); // hidxb
 
@@ -87,7 +113,7 @@ int ocp_qp_hpipm_calculate_memory_size(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args
 
 
 
-void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_qp_hpipm_memory *hpipm_memory, void *memory) {
+void ocp_qp_condensing_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_condensing_hpipm_args *args, ocp_qp_condensing_hpipm_memory *hpipm_memory, void *memory) {
 //
 
 	// loop indexed
@@ -100,6 +126,15 @@ void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_q
     int *nu = (int *) qp_in->nu;
     int *nb = (int *) qp_in->nb;
     int *ng = (int *) qp_in->nc;
+	int **hsidxb = (int **) qp_in->idxb;
+
+
+	// compute dense qp size
+	int nvd = 0;
+	int ned = 0;
+	int nbd = 0;
+	int ngd = 0;
+	d_compute_qp_size_ocp2dense(N, nx, nu, nb, hsidxb, ng, &nvd, &ned, &nbd, &ngd);
 
 
 	// char pointer
@@ -113,11 +148,20 @@ void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_q
 	hpipm_memory->qp_sol = (struct d_ocp_qp_sol *) c_ptr;
 	c_ptr += 1*sizeof(struct d_ocp_qp_sol);
 	//
-	hpipm_memory->ipm_arg = (struct d_ipm_hard_ocp_qp_arg *) c_ptr;
-	c_ptr += 1*sizeof(struct d_ipm_hard_ocp_qp_arg);
+	hpipm_memory->qpd = (struct d_dense_qp *) c_ptr;
+	c_ptr += 1*sizeof(struct d_dense_qp);
 	//
-	hpipm_memory->ipm_workspace = (struct d_ipm_hard_ocp_qp_workspace *) c_ptr;
-	c_ptr += 1*sizeof(struct d_ipm_hard_ocp_qp_workspace);
+	hpipm_memory->qpd_sol = (struct d_dense_qp_sol *) c_ptr;
+	c_ptr += 1*sizeof(struct d_dense_qp_sol);
+	//
+	hpipm_memory->cond_workspace = (struct d_cond_qp_ocp2dense_workspace *) c_ptr;
+	c_ptr += 1*sizeof(struct d_cond_qp_ocp2dense_workspace);
+	//
+	hpipm_memory->ipm_arg = (struct d_ipm_hard_dense_qp_arg *) c_ptr;
+	c_ptr += 1*sizeof(struct d_ipm_hard_dense_qp_arg);
+	//
+	hpipm_memory->ipm_workspace = (struct d_ipm_hard_dense_qp_workspace *) c_ptr;
+	c_ptr += 1*sizeof(struct d_ipm_hard_dense_qp_workspace);
 	//
 	hpipm_memory->hlam_lb = (double **) c_ptr;
 	c_ptr += (N+1)*sizeof(double *);
@@ -142,9 +186,15 @@ void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_q
 	//
 	struct d_ocp_qp_sol *qp_sol = hpipm_memory->qp_sol;
 	//
-	struct d_ipm_hard_ocp_qp_arg *ipm_arg = hpipm_memory->ipm_arg;
+	struct d_dense_qp *qpd = hpipm_memory->qpd;
 	//
-	struct d_ipm_hard_ocp_qp_workspace *ipm_workspace = hpipm_memory->ipm_workspace;
+	struct d_dense_qp_sol *qpd_sol = hpipm_memory->qpd_sol;
+	//
+	struct d_cond_qp_ocp2dense_workspace *cond_workspace = hpipm_memory->cond_workspace;
+	//
+	struct d_ipm_hard_dense_qp_arg *ipm_arg = hpipm_memory->ipm_arg;
+	//
+	struct d_ipm_hard_dense_qp_workspace *ipm_workspace = hpipm_memory->ipm_workspace;
 
 
 	//
@@ -167,13 +217,22 @@ void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_q
 	// ocp qp sol structure
 	d_create_ocp_qp_sol(N, nx, nu, nb, ng, qp_sol, c_ptr);
 	c_ptr += qp_sol->memsize;
+	// dense qp structure
+	d_create_dense_qp(nvd, ned, nbd, ngd, qpd, c_ptr);
+	c_ptr += qpd->memsize;
+	// dense qp sol structure
+	d_create_dense_qp_sol(nvd, ned, nbd, ngd, qpd_sol, c_ptr);
+	c_ptr += qpd_sol->memsize;
+	// cond workspace structure
+	d_create_cond_qp_ocp2dense(qp, qpd, cond_workspace, c_ptr);
+	c_ptr += cond_workspace->memsize;
 	// ipm arg structure
 	ipm_arg->iter_max = args->iter_max;
 	ipm_arg->alpha_min = args->alpha_min;
 	ipm_arg->mu_max = args->mu_max;
 	ipm_arg->mu0 = args->mu0;
 	// ipm workspace structure
-	d_create_ipm_hard_ocp_qp(qp, ipm_arg, ipm_workspace, c_ptr);
+	d_create_ipm_hard_dense_qp(qpd, ipm_arg, ipm_workspace, c_ptr);
 	c_ptr += ipm_workspace->memsize;
 
 
@@ -183,8 +242,8 @@ void ocp_qp_hpipm_create_memory(ocp_qp_in *qp_in, ocp_qp_hpipm_args *args, ocp_q
 
 
 
-int ocp_qp_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
-        ocp_qp_hpipm_args *args, ocp_qp_hpipm_memory *memory, void *workspace_) {
+int ocp_qp_condensing_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
+        ocp_qp_condensing_hpipm_args *args, ocp_qp_condensing_hpipm_memory *memory, void *workspace_) {
 //
     // initialize return code
     int acados_status = ACADOS_SUCCESS;
@@ -200,8 +259,11 @@ int ocp_qp_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
 	double **hlam_ug = memory->hlam_ug;
 	struct d_ocp_qp *qp = memory->qp;
 	struct d_ocp_qp_sol *qp_sol = memory->qp_sol;
-//	struct d_ipm_hard_ocp_qp_arg *ipm_arg = memory->ipm_arg;
-	struct d_ipm_hard_ocp_qp_workspace *ipm_workspace = memory->ipm_workspace;
+	struct d_dense_qp *qpd = memory->qpd;
+	struct d_dense_qp_sol *qpd_sol = memory->qpd_sol;
+	struct d_cond_qp_ocp2dense_workspace *cond_workspace = memory->cond_workspace;
+//	struct d_ipm_hard_dense_qp_arg *ipm_arg = memory->ipm_arg;
+	struct d_ipm_hard_dense_qp_workspace *ipm_workspace = memory->ipm_workspace;
 
     // extract problem size
     int N = qp_in->N;
@@ -258,6 +320,9 @@ int ocp_qp_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
 	d_cvt_colmaj_to_ocp_qp(hA, hB, hb, hQ, hS, hR, hq, hr, hidxb, hd_lb, hd_ub, hC, hD, hd_lg, hd_ug, qp);
 
 	// ocp qp sol structure
+	d_cond_qp_ocp2dense(qp, qpd, cond_workspace);
+
+	// dense qp structure
 
 	// arg structure // XXX fixed in memory !!!
 //	ipm_arg->alpha_min = args->alpha_min;
@@ -269,7 +334,11 @@ int ocp_qp_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
 
 
 	// solve ipm
-	d_solve_ipm2_hard_ocp_qp(qp, qp_sol, ipm_workspace);
+	d_solve_ipm2_hard_dense_qp(qpd, qpd_sol, ipm_workspace);
+
+
+	// expand solution
+	d_expand_sol_dense2ocp(qp, qpd_sol, qp_sol, cond_workspace);
 
 
 	// extract solution
@@ -286,112 +355,46 @@ int ocp_qp_hpipm(ocp_qp_in *qp_in, ocp_qp_out *qp_out,
 	//
 	double *res_g;
 	inf_norm_res[0] = 0;
-	for(ii=0; ii<=N; ii++)
+	res_g = ipm_workspace->res_g->pa;
+	for(jj=0; jj<qpd->nv; jj++)
 		{
-		res_g = (ipm_workspace->res_g+ii)->pa;
-		for(jj=0; jj<nu[ii]+nx[ii]; jj++)
+		res_tmp = fabs(res_g[jj]);
+		if(res_tmp>inf_norm_res[0])
 			{
-			res_tmp = fabs(res_g[jj]);
-			if(res_tmp>inf_norm_res[0])
-				{
-				inf_norm_res[0] = res_tmp;
-				}
+			inf_norm_res[0] = res_tmp;
 			}
 		}
 	double *res_b;
 	inf_norm_res[1] = 0;
-	for(ii=0; ii<N; ii++)
+	res_b = ipm_workspace->res_b->pa;
+	for(jj=0; jj<qpd->ne; jj++)
 		{
-		res_b = (ipm_workspace->res_b+ii)->pa;
-		for(jj=0; jj<nx[ii+1]; jj++)
+		res_tmp = fabs(res_b[jj]);
+		if(res_tmp>inf_norm_res[1])
 			{
-			res_tmp = fabs(res_b[jj]);
-			if(res_tmp>inf_norm_res[1])
-				{
-				inf_norm_res[1] = res_tmp;
-				}
+			inf_norm_res[1] = res_tmp;
 			}
 		}
 	double *res_d;
 	inf_norm_res[2] = 0;
-	for(ii=0; ii<=N; ii++)
+	res_d = ipm_workspace->res_d->pa;
+	for(jj=0; jj<2*qpd->nb+2*qpd->ng; jj++)
 		{
-		res_d = (ipm_workspace->res_d_lb+ii)->pa;
-		for(jj=0; jj<nb[ii]; jj++)
+		res_tmp = fabs(res_d[jj]);
+		if(res_tmp>inf_norm_res[2])
 			{
-			res_tmp = fabs(res_d[jj]);
-			if(res_tmp>inf_norm_res[2])
-				{
-				inf_norm_res[2] = res_tmp;
-				}
-			}
-		res_d = (ipm_workspace->res_d_ub+ii)->pa;
-		for(jj=0; jj<nb[ii]; jj++)
-			{
-			res_tmp = fabs(res_d[jj]);
-			if(res_tmp>inf_norm_res[2])
-				{
-				inf_norm_res[2] = res_tmp;
-				}
-			}
-		res_d = (ipm_workspace->res_d_lg+ii)->pa;
-		for(jj=0; jj<ng[ii]; jj++)
-			{
-			res_tmp = fabs(res_d[jj]);
-			if(res_tmp>inf_norm_res[2])
-				{
-				inf_norm_res[2] = res_tmp;
-				}
-			}
-		res_d = (ipm_workspace->res_d_ug+ii)->pa;
-		for(jj=0; jj<ng[ii]; jj++)
-			{
-			res_tmp = fabs(res_d[jj]);
-			if(res_tmp>inf_norm_res[2])
-				{
-				inf_norm_res[2] = res_tmp;
-				}
+			inf_norm_res[2] = res_tmp;
 			}
 		}
 	double *res_m;
 	inf_norm_res[3] = 0;
-	for(ii=0; ii<=N; ii++)
+	res_m = ipm_workspace->res_m->pa;
+	for(jj=0; jj<2*qpd->nb+2*qpd->ng; jj++)
 		{
-		res_m = (ipm_workspace->res_m_lb+ii)->pa;
-		for(jj=0; jj<nb[ii]; jj++)
+		res_tmp = fabs(res_m[jj]);
+		if(res_tmp>inf_norm_res[3])
 			{
-			res_tmp = fabs(res_m[jj]);
-			if(res_tmp>inf_norm_res[3])
-				{
-				inf_norm_res[3] = res_tmp;
-				}
-			}
-		res_m = (ipm_workspace->res_m_ub+ii)->pa;
-		for(jj=0; jj<nb[ii]; jj++)
-			{
-			res_tmp = fabs(res_m[jj]);
-			if(res_tmp>inf_norm_res[3])
-				{
-				inf_norm_res[3] = res_tmp;
-				}
-			}
-		res_m = (ipm_workspace->res_m_lg+ii)->pa;
-		for(jj=0; jj<ng[ii]; jj++)
-			{
-			res_tmp = fabs(res_m[jj]);
-			if(res_tmp>inf_norm_res[3])
-				{
-				inf_norm_res[3] = res_tmp;
-				}
-			}
-		res_m = (ipm_workspace->res_m_ug+ii)->pa;
-		for(jj=0; jj<ng[ii]; jj++)
-			{
-			res_tmp = fabs(res_m[jj]);
-			if(res_tmp>inf_norm_res[3])
-				{
-				inf_norm_res[3] = res_tmp;
-				}
+			inf_norm_res[3] = res_tmp;
 			}
 		}
 	inf_norm_res[4] = ipm_workspace->res_mu;
