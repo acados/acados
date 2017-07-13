9e40a9be7f0fcc88e06498da7c67a107e4e0982a
Robin <robin.verschueren@gmail.com>
2017-07-13 16:49:48 +0200

    Build blasfeo with CMake

--- cmake/external/blasfeo.cmake
+++ cmake/external/blasfeo.cmake
@@ -37,4 +37,4 @@ ExternalProject_Add(
 ExternalProject_Get_Property(blasfeo_project source_dir)
 add_library(blasfeo STATIC IMPORTED GLOBAL)
 add_dependencies(blasfeo blasfeo_project)
-set_property(TARGET blasfeo PROPERTY IMPORTED_LOCATION "${source_dir}/libblasfeo.a")
+set_property(TARGET blasfeo PROPERTY IMPORTED_LOCATION "${source_dir}/build/libblasfeo.a")
