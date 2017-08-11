608d547fd0ff936269cb960342c465d5d78dd1ca
roversch <robin.verschueren@gmail.com>
2017-08-11 17:57:34 +0200

    First time compilation acados.lib on dSpace

--- cmake/Platform/dSpace.cmake
+++ cmake/Platform/dSpace.cmake
@@ -1,8 +1,11 @@
 message(STATUS "DSPACE")
 
-find_program(CMAKE_MAKE_PROGRAM ${DSPACE_TOOLS}/Exe/DSMAKE.EXE)
-
-set(CMAKE_MAKE_PROGRAM ${CMAKE_MAKE_PROGRAM})
+set(CMAKE_STATIC_LIBRARY_PREFIX "" CACHE STRING "Static library prefix")
+set(CMAKE_STATIC_LIBRARY_SUFFIX ".lib" CACHE STRING "Static library suffix")
 
 set(CMAKE_INCLUDE_FLAG_C "-J")
 set(CMAKE_INCLUDE_FLAG_CXX "-J")
+
+add_definitions(-D__DSPACE__)
+remove_definitions(-DLINUX)
+remove_definitions(-D__LINUX__)
