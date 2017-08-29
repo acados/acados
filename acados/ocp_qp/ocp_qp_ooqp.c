caea3fecc5b84147c9996d1a28e5db4c03c5ea70
Robin Verschueren <robin.verschueren@gmail.com>
2017-08-30 00:18:38 +0200

    remove unused variable

--- acados/ocp_qp/ocp_qp_ooqp.c
+++ acados/ocp_qp/ocp_qp_ooqp.c
@@ -652,21 +652,12 @@ int_t ocp_qp_ooqp(ocp_qp_in *in, ocp_qp_out *out, void *args_, void *memory_, vo
     int return_value;
     // printf("$$ FIRST RUN FLAG %d\n", mem->firstRun);
 
-    acados_timer timer;
-    real_t cputime;
-
-    acados_tic(&timer);
     // NOTE: has to be called after setting up the memory which contains the problem dimensions
     ocp_qp_ooqp_cast_workspace(work, mem);
-    cputime = acados_toc(&timer);
-
-    acados_tic(&timer);
     ocp_qp_ooqp_update_memory(in, args, mem, work);
-    cputime = acados_toc(&timer);
 
     if (0) print_inputs(mem);
 
-    acados_tic(&timer);
     // TODO(dimitris): implement dense OOQP
     // call sparse OOQP
     qpsolvesp(mem->c, mem->nx,
@@ -678,7 +669,6 @@ int_t ocp_qp_ooqp(ocp_qp_in *in, ocp_qp_out *out, void *args_, void *memory_, vo
         mem->clow, mem->mz, mem->iclow, mem->cupp, mem->icupp,
         work->x, work->gamma, work->phi, work->y, work->z, work->lambda, work->pi,
         &work->objectiveValue, args->printLevel, &return_value);
-    cputime = acados_toc(&timer);
 
     if (0) print_outputs(mem, work, return_value);
     fill_in_qp_out(in, out, work);
