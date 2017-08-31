063f1bf55e675ef32ff2ac69a6d5095d9643091c
Robin <robin.verschueren@gmail.com>
2017-08-31 12:27:53 +0200

    Build also hpmpc in build folder

--- cmake/external/hpmpc.cmake
+++ cmake/external/hpmpc.cmake
@@ -17,23 +17,38 @@ elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
     set(OS "MAC")
 elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
     set(OS "WINDOWS")
-    set(RANLIB_COMMAND ${CMAKE_RANLIB} libhpmpc.a)
+    set(RANLIB_COMMAND ${CMAKE_RANLIB} libhpmpc.a)  # for mingw-w64
 endif()
 
 ExternalProject_Add(
     hpmpc_project
 
     DEPENDS blasfeo
+    PREFIX "${PROJECT_BINARY_DIR}/external/hpmpc"
     CONFIGURE_COMMAND make clean
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/hpmpc"
-    BUILD_IN_SOURCE 1
     BUILD_COMMAND make static_library -j 2 OS=${OS} CC=${CMAKE_C_COMPILER} TARGET=${HPMPC_TARGET} BLASFEO_PATH=${HPMPC_BLASFEO_PATH}
-    INSTALL_COMMAND "${RANLIB_COMMAND}"
+    INSTALL_COMMAND make install_static PREFIX=${PROJECT_BINARY_DIR}/external
     # LOG_CONFIGURE 1  # suppress output
     # LOG_BUILD 1
 )
 
-ExternalProject_Get_Property(hpmpc_project source_dir)
+ExternalProject_Get_Property(hpmpc_project BINARY_DIR)
+
+ExternalProject_Add_Step(hpmpc_project copy_hpmpc
+    COMMAND cp -r "${EXTERNAL_SRC_DIR}/hpmpc/" "${BINARY_DIR}"
+    DEPENDERS configure
+)
+
+ExternalProject_Add_Step(hpmpc_project ranlib_step
+    COMMAND "${RANLIB_COMMAND}"
+    DEPENDERS install
+)
+
 add_library(hpmpc STATIC IMPORTED GLOBAL)
 add_dependencies(hpmpc hpmpc_project blasfeo_project)
-set_property(TARGET hpmpc PROPERTY IMPORTED_LOCATION "${source_dir}/libhpmpc.a")
+set_property(TARGET hpmpc PROPERTY IMPORTED_LOCATION "${BINARY_DIR}/libhpmpc.a")
+set_property(TARGET hpmpc
+    PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+        $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/external/>
+        $<INSTALL_INTERFACE:include>)
