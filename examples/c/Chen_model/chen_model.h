fca6ff5e8e521ccaee1f55334d25cb3877895a3e
Robin <robin.verschueren@gmail.com>
2017-09-13 12:17:31 +0200

    Delete chen

--- examples/c/Chen_model/chen_model.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXAMPLES_C_CHEN_MODEL_CHEN_MODEL_H_
-#define EXAMPLES_C_CHEN_MODEL_CHEN_MODEL_H_
-
-#include "acados/utils/types.h"
-
-void VDE_fun(const int_t nx, const int_t nu, const real_t* in, real_t* out,
-             int (*vde)(const real_t**, real_t**, int*, real_t*, int));
-
-#endif  // EXAMPLES_C_CHEN_MODEL_CHEN_MODEL_H_
