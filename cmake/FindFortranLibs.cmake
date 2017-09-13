6c829bfec976b37189b179ca4f1b4da8bb68efff
Robin <robin.verschueren@gmail.com>
2017-09-13 12:31:50 +0200

    Add path for Raspbian

--- cmake/FindFortranLibs.cmake
+++ cmake/FindFortranLibs.cmake
@@ -21,6 +21,7 @@ find_library(FORTRAN_LIBRARY NAMES libgfortran.so libgfortran.dylib gfortran
     HINTS
         /usr/lib/gcc/x86_64-linux-gnu/*
         /usr/local/lib/gcc/*
+        /usr/lib/gcc/arm-linux-gnueabihf/* # for Raspbian
         ${CMAKE_FIND_ROOT_PATH}
         $ENV{PATH}
     CMAKE_FIND_ROOT_PATH_BOTH)
