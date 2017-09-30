3853341ef4870c19879a613ca1681426bb6a212f
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2017-09-30 16:04:14 +0200

    fixed prototype

--- examples/c/acados_gnuplot/acados_gnuplot.c
+++ examples/c/acados_gnuplot/acados_gnuplot.c
@@ -3,7 +3,7 @@
 
 #define _GNU_SOURCE
 
-static void acados_gnuplot(real_t **data, int_t n_data, real_t T,
+void acados_gnuplot(real_t **data, int_t n_data, real_t T,
     int_t N, char **labels, int_t layout_x, int_t layout_y) {
 
     double t_grid[N];
--- examples/c/acados_gnuplot/acados_gnuplot.h
+++ examples/c/acados_gnuplot/acados_gnuplot.h
@@ -3,7 +3,7 @@
 
 #include "acados/utils/types.h"
 
-static void acados_gnuplot(real_t **data, int_t n_data, real_t T,
+void acados_gnuplot(real_t **data, int_t n_data, real_t T,
     int_t N, char **labels, int_t layout_x, int_t layout_y);
 
 #endif  // ACADOS_GNUPLOT_H_
