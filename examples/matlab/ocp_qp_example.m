c3199d7e8b2c1005a3686fa393998335eb9d87ee
Robin <robin.verschueren@gmail.com>
2018-02-08 14:33:37 +0100

    update matlab example

--- examples/matlab/ocp_qp_example.m
+++ examples/matlab/ocp_qp_example.m
@@ -8,13 +8,18 @@ qp.set('B', [0; 1]);
 qp.set('Q', eye(2));
 qp.set('R', 1);
 
+% specify bounds
+qp.set('lbx', [0.5; -inf]);
+qp.set('ubx', [2.0; +inf]);
+
 % specify initial condition
 x0 = [1; 1];
 qp.set('lbx', 0, x0);
 qp.set('ubx', 0, x0);
 
 % solve QP
-output = qp.solve('qpoases');
+qp.initialize_solver('qpoases');
+output = qp.solve();
 
 xopt = output.states();
 
