616486660ec6b04e7f5413b81a6312c2d2f0769c
dkouzoup <dimitris.kz@gmail.com>
2018-03-09 17:36:06 +0100

    Enable MDL_START in Simulink

--- interfaces/simulink/ocp_qp_s_function.c
+++ interfaces/simulink/ocp_qp_s_function.c
@@ -4,6 +4,7 @@
 
 #ifndef MATLAB_MEX_FILE
 #include <brtenv.h>
+#define printf(...) msg_info_printf(MSG_SM_USER, 0, __VA_ARGS__);
 #endif
 
 #define AC_HORIZON_LENGTH 5
@@ -20,16 +21,22 @@ static void mdlInitializeSizes(SimStruct *S)
     if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
         return; /* Parameter mismatch will be reported by Simulink */
     }
+    const mxArray *B = ssGetSFcnParam(S, 3);
+
+    int nx = mxGetM(B);
+    int nu = mxGetN(B);
 
     if (!ssSetNumInputPorts(S, 1)) return;
-    ssSetInputPortWidth(S, 0, DYNAMICALLY_SIZED);
+    ssSetInputPortWidth(S, 0, nx);
     ssSetInputPortDirectFeedThrough(S, 0, true);
     ssSetInputPortRequiredContiguous(S, 0, true);
 
     ssSetNumPWork(S, 5);
 
-    if (!ssSetNumOutputPorts(S,1)) return;
-    ssSetOutputPortWidth(S, 0, 1);
+    if (!ssSetNumOutputPorts(S, 3)) return;
+    ssSetOutputPortWidth(S, 0, nu);
+    ssSetOutputPortWidth(S, 1, 1);
+    ssSetOutputPortWidth(S, 2, 1);
 
     ssSetNumSampleTimes(S, 1);
 }
@@ -40,55 +47,9 @@ static void mdlInitializeSampleTimes(SimStruct *S)
     ssSetOffsetTime(S, 0, 0.0);
 }
 
-// #define MDL_START
-// static void mdlStart(SimStruct *S) {
-//     const mxArray *Q = ssGetSFcnParam(S, 0);
-//     const mxArray *R = ssGetSFcnParam(S, 1);
-//     const mxArray *A = ssGetSFcnParam(S, 2);
-//     const mxArray *B = ssGetSFcnParam(S, 3);
-// 
-//     int nx = mxGetM(B);
-//     ssSetInputPortWidth(S, 0, nx);
-//     int nu = mxGetN(B);
-//     ssSetOutputPortWidth(S, 0, nu);
-//     
-//     ocp_qp_dims *qp_dims = create_ocp_qp_dims(AC_HORIZON_LENGTH);
-//     qp_dims->nbx[0] = nx;
-//     qp_dims->nb[0] = nx;
-//     for (int i = 0; i < AC_HORIZON_LENGTH; ++i) {
-//         qp_dims->nx[i] = nx;
-//         qp_dims->nu[i] = nu;
-//     }
-//     qp_dims->nx[AC_HORIZON_LENGTH] = nx;
-//     
-//     ocp_qp_in *qp_in = create_ocp_qp_in(qp_dims);
-//     for (int i = 0; i < nx; ++i)
-//         qp_in->idxb[0][i] = nu+i;
-// 
-//     for (int i = 0; i < AC_HORIZON_LENGTH; ++i) {
-//         d_cvt_colmaj_to_ocp_qp_Q(i, (double *) mxGetData(Q), qp_in);
-//         d_cvt_colmaj_to_ocp_qp_R(i, (double *) mxGetData(R), qp_in);
-//         d_cvt_colmaj_to_ocp_qp_A(i, (double *) mxGetData(A), qp_in);
-//         d_cvt_colmaj_to_ocp_qp_B(i, (double *) mxGetData(B), qp_in);
-//     }
-//     d_cvt_colmaj_to_ocp_qp_Q(AC_HORIZON_LENGTH, (double *) mxGetData(Q), qp_in);
-//     d_cvt_colmaj_to_ocp_qp_R(AC_HORIZON_LENGTH, (double *) mxGetData(R), qp_in);
-// 
-//     ocp_qp_out *qp_out = create_ocp_qp_out(qp_dims);
-//     
-//     ocp_qp_solver_plan plan = {PARTIAL_CONDENSING_HPIPM};
-//     void *qp_args = ocp_qp_create_args(&plan, qp_dims);    
-//     ocp_qp_solver *qp_solver = ocp_qp_create(&plan, qp_dims, qp_args);
-//     
-//     ssGetPWork(S)[0] = (void *) qp_dims;
-//     ssGetPWork(S)[1] = (void *) qp_in;
-//     ssGetPWork(S)[2] = (void *) qp_out;
-//     ssGetPWork(S)[3] = (void *) qp_args;
-//     ssGetPWork(S)[4] = (void *) qp_solver;
-// }
-
-static void mdlOutputs(SimStruct *S, int_T tid)
-{
+#define MDL_START
+static void mdlStart(SimStruct *S) {
+    printf("mdlStart: Read parameters");
     const mxArray *Q = ssGetSFcnParam(S, 0);
     const mxArray *R = ssGetSFcnParam(S, 1);
     const mxArray *A = ssGetSFcnParam(S, 2);
@@ -125,7 +86,8 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     
     ocp_qp_solver_plan plan = {PARTIAL_CONDENSING_HPIPM};
     void *qp_args = ocp_qp_create_args(&plan, qp_dims);
-        
+    
+    printf("mdlStart: Create acados solver");
     ocp_qp_solver *qp_solver = ocp_qp_create(&plan, qp_dims, qp_args);
     
     ssGetPWork(S)[0] = (void *) qp_dims;
@@ -133,10 +95,13 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     ssGetPWork(S)[2] = (void *) qp_out;
     ssGetPWork(S)[3] = (void *) qp_args;
     ssGetPWork(S)[4] = (void *) qp_solver;
-    
-    qp_in = (ocp_qp_in *) ssGetPWork(S)[1];
-    qp_out = (ocp_qp_out *) ssGetPWork(S)[2];
-    qp_solver = (ocp_qp_solver *) ssGetPWork(S)[4];
+}
+
+static void mdlOutputs(SimStruct *S, int_T tid)
+{    
+    ocp_qp_in *qp_in = (ocp_qp_in *) ssGetPWork(S)[1];
+    ocp_qp_out *qp_out = (ocp_qp_out *) ssGetPWork(S)[2];
+    ocp_qp_solver *qp_solver = (ocp_qp_solver *) ssGetPWork(S)[4];
 
     const double *x0 = ssGetInputPortRealSignal(S,0);
     d_cvt_colmaj_to_ocp_qp_lbx(0, x0, qp_in);
@@ -144,23 +109,23 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     
     int status = ocp_qp_solve(qp_solver, qp_in, qp_out);
     
-    double *u0_opt = ssGetOutputPortRealSignal(S,0);
+    ocp_qp_info *info = (ocp_qp_info *) qp_out->misc;
+    
+    double *u0_opt = ssGetOutputPortRealSignal(S, 0);
+    double *time = ssGetOutputPortRealSignal(S, 1);
+    double *status_out = ssGetOutputPortRealSignal(S, 2);
 
     d_cvt_ocp_qp_sol_to_colmaj_u(qp_out, u0_opt, 0);
+    *time = info->total_time;
+    *status_out = (double) status;
+}
 
+static void mdlTerminate(SimStruct *S) {
     free(ssGetPWork(S)[0]);
     free(ssGetPWork(S)[1]);
     free(ssGetPWork(S)[2]);
     free(ssGetPWork(S)[3]);
     free(ssGetPWork(S)[4]);
-}
-
-static void mdlTerminate(SimStruct *S) {
-//     free(ssGetPWork(S)[0]);
-//     free(ssGetPWork(S)[1]);
-//     free(ssGetPWork(S)[2]);
-//     free(ssGetPWork(S)[3]);
-//     free(ssGetPWork(S)[4]);
 }
 
 #ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
