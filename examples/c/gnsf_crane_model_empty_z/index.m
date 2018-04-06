16283c1e402fe70601f80f843e42d14ab19aa2c3
Jonathan Frey <jonathanpaulfrey@gmail.com>
2018-04-06 17:22:17 +0200

    cleaned examples/c folder

--- examples/c/gnsf_crane_model_empty_z/index.m
+++
@@ -1,5 +0,0 @@
-      function [ind] = index( n, j)
-            % index(nx,j) returns the ind, such that s(ind) corresponds
-            % to the jth stage vector
-           ind = 1 + (j-1) * n : j * n; 
-      end
