0414b34117e844da375838ab520a3de3ba22072f
Robin <robin.verschueren@gmail.com>
2018-04-18 15:04:56 +0200

    First attempt at fixing lint

--- cmake/Lint.cmake
+++ cmake/Lint.cmake
@@ -6,7 +6,7 @@ if(NOT PYTHONINTERP_FOUND)
     return()
 endif()
 
-set(FIND_FILES_TO_LINT find acados test examples swig -type f -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.i")
+set(FIND_FILES_TO_LINT find acados examples interfaces swig test -type f -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.i")
 set(FIND_FILES_TO_LINT ${CMAKE_COMMAND} -E chdir ${PROJECT_SOURCE_DIR} ${FIND_FILES_TO_LINT})
 execute_process(COMMAND ${FIND_FILES_TO_LINT} OUTPUT_VARIABLE FILES_TO_LINT)
 string(REPLACE "\n" " " FILES_TO_LINT ${FILES_TO_LINT})
