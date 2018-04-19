7a274aa2344e856c37e5ef032c14c3b7bb5f1302
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-04-19 14:26:58 +0200

    Update Sphinx Website

---
+++ docs/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.5a1',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: ''
+};
