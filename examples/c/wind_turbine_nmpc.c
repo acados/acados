37350d4602798972dc4caa5e44f67c5a72f2863e
Jonathan Frey <jonathanpaulfrey@gmail.com>
2018-04-22 16:16:23 +0200

    wind_turbine_nmpc fixed appropriately

--- examples/c/wind_turbine_nmpc.c
+++ examples/c/wind_turbine_nmpc.c
@@ -53,7 +53,7 @@
 
 #define NN 40
 
-#define MAX_SQP_ITERS 10
+#define MAX_SQP_ITERS 15
 #define NREP 1
 
 
@@ -481,8 +481,8 @@ int main()
 	{
 		plan->nlp_dynamics[i] = CONTINUOUS_MODEL;
 //		plan->sim_solver_plan[i].sim_solver = ERK;
-		plan->sim_solver_plan[i].sim_solver = IRK;
-//		plan->sim_solver_plan[i].sim_solver = NEW_LIFTED_IRK;
+		// plan->sim_solver_plan[i].sim_solver = IRK;
+		plan->sim_solver_plan[i].sim_solver = NEW_LIFTED_IRK;
 	}
 
 	ocp_nlp_solver_config *config = ocp_nlp_config_create(*plan, NN);
