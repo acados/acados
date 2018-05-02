de370aa8368c3bfea059a78f673d6f5344088343
Jonathan Frey <jonathanpaulfrey@gmail.com>
2018-05-02 16:31:27 +0200

    wt_nx6: removed unnecessary impl_ode fcns

--- examples/c/wt_model_nx6/full_turbine_model.m
+++ examples/c/wt_model_nx6/full_turbine_model.m
@@ -79,25 +79,6 @@ expl_vde_adj.generate('expl_vde_adj', opts);
 impl_ode_fun = Function('casadi_impl_ode_fun', {x, dx, u, p}, {fi});
 impl_ode_fun.generate('impl_ode_fun', opts);
 
-
-% impl_ode_jac_x
-
-impl_ode_jac_x = Function('casadi_impl_ode_jac_x', {x, dx, u, p}, {jacobian(fi, x)});
-impl_ode_jac_x.generate('impl_ode_jac_x', opts);
-
-
-% impl_ode_jac_xdot
-
-impl_ode_jac_xdot = Function('casadi_impl_ode_jac_xdot', {x, dx, u, p}, {jacobian(fi, dx)});
-impl_ode_jac_xdot.generate('impl_ode_jac_xdot', opts);
-
-
-% impl_ode_jac_u
-
-impl_ode_jac_u = Function('casadi_impl_ode_jac_u', {x, dx, u, p}, {jacobian(fi, u)});
-impl_ode_jac_u.generate('impl_ode_jac_u', opts);
-
-
 % impl_ode_fun_jac_x_xdot
 
 impl_ode_fun_jac_x_xdot = Function('casadi_impl_ode_fun_jac_x_xdot', {x, dx, u, p}, {fi, jacobian(fi, x), jacobian(fi, dx)});
@@ -114,13 +95,6 @@ impl_ode_jac_x_xdot_u.generate('impl_ode_jac_x_xdot_u', opts);
 
 impl_ode_fun_jac_x_xdot_u = Function('casadi_impl_ode_fun_jac_x_xdot_u', {x, dx, u, p}, {fi, jacobian(fi, x), jacobian(fi, dx), jacobian(fi, u)});
 impl_ode_fun_jac_x_xdot_u.generate('impl_ode_fun_jac_x_xdot_u', opts);
-
-
-% impl_ode_jac_x_u
-
-impl_ode_jac_x_u = Function('casadi_impl_ode_jac_x_u', {x, dx, u, p}, {jacobian(fi, x), jacobian(fi, u)});
-impl_ode_jac_x_u.generate('impl_ode_jac_x_u', opts);
-
 
 return
 
