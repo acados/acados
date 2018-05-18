b6d7cb769174eb83e348a92cd792ef3d9ff9b8d9
Gianluca Frison <gianfri2008@hotmail.it>
2018-05-18 09:58:16 +0200

    recovered deleted comment

--- acados/ocp_nlp/ocp_nlp_common.h
+++ acados/ocp_nlp/ocp_nlp_common.h
@@ -41,7 +41,7 @@ typedef struct
     void **cost;
     void **dynamics;
     void **constraints;
-    ocp_qp_dims *qp_solver;
+    ocp_qp_dims *qp_solver;  // xcond solver instead ??
     int *nv;  // number of primal variables (states+controls+slacks)
     int *nx;  // number of states
     int *nu;  // number of inputs
