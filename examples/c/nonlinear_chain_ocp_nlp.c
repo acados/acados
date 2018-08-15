9af8bf9d7248a111ea2dce6bc839e5a50d894331
bnovoselnik <bnovoselnik@gmail.com>
2018-08-15 12:12:19 +0200

    updated nonlinear_chain_ocp_nlp

--- examples/c/nonlinear_chain_ocp_nlp.c
+++ examples/c/nonlinear_chain_ocp_nlp.c
@@ -1134,11 +1134,11 @@ int main()
 			plan->nlp_cost[i] = NONLINEAR_LS;  // also implements linear LS for this example
 	}
 
-	// plan->ocp_qp_solver_plan.qp_solver = PARTIAL_CONDENSING_HPIPM;
+	plan->ocp_qp_solver_plan.qp_solver = PARTIAL_CONDENSING_HPIPM;
 	// plan->ocp_qp_solver_plan.qp_solver = FULL_CONDENSING_HPIPM;
 	// plan->ocp_qp_solver_plan.qp_solver = FULL_CONDENSING_QPOASES;
 	// plan->ocp_qp_solver_plan.qp_solver = FULL_CONDENSING_OOQP;
-	plan->ocp_qp_solver_plan.qp_solver = PARTIAL_CONDENSING_OOQP;
+	// plan->ocp_qp_solver_plan.qp_solver = PARTIAL_CONDENSING_OOQP;
 
 	// NOTE(dimitris): switching between different integrators on each stage to test everything
 	for (int i = 0; i < NN; i++)
