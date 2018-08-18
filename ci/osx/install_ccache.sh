051c4de0aaaf397f4096d6d6d1df1191d364b1e5
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-18 20:10:26 +0200

    Prevent Homebrew from updating automatically

--- ci/osx/install_ccache.sh
+++ ci/osx/install_ccache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 # Taken from: https://docs.travis-ci.com/user/caching/#ccache-cache
+export HOMEBREW_NO_AUTO_UPDATE=1; # do not let homebrew update
 brew install ccache;
 export PATH="/usr/local/opt/ccache/libexec:$PATH";
