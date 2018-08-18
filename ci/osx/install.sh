83f0ba6d4f3b6ce8e0d907675ae2607e70084e8d
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-18 19:22:06 +0200

    Add casadi install script for osx

--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -2,3 +2,4 @@
 
 travis_retry source "${SCRIPT_DIR}/install_ccache.sh";
 travis_retry source "${SHARED_SCRIPT_DIR}/install_swig.sh";
+travis_retry source "${SCRIPT_DIR}/install_casadi.sh";
---
+++ ci/osx/install_casadi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+CASADI_INSTALL_DIR="${HOME}/casadi";
+CASADI_VERSION='3.4.0';
+CASADI_DOWNLOAD_URL="https://github.com/casadi/casadi/archive/${CASADI_VERSION}.zip";
+
+# run only if casadi build was not cached
+pushd "${TRAVIS_BUILD_DIR}/external";
+	curl -o casadi.zip -Ls "${CASADI_DOWNLOAD_URL}";
+	unzip -qq casadi.zip;
+	rm -f casadi.zip;
+	pushd "./casadi-${CASADI_VERSION}";
+		mkdir build;
+		pushd ./build;
+			cmake -DWITH_SELFCONTAINED=ON -DWITH_PYTHON=ON -DWITH_PYTHON3=ON -DCMAKE_INSTALL_PREFIX="${CASADI_INSTALL_DIR}" ..;
+			make -j 4 && make install;
+		popd;
+	popd;
+popd;
+export PYTHONPATH="${CASADI_INSTALL_DIR}:$PYTHONPATH";
