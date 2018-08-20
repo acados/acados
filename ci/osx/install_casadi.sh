e31a3b27d691513ddb2c69ca94d0d771a835180c
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-20 08:10:14 +0200

    Rename CASADI_INSTALL_DIR to CASADIPATH
    Acados uses CASADIPATH in its CMake files.

--- ci/osx/install_casadi.sh
+++ ci/osx/install_casadi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
-CASADI_INSTALL_DIR="${CASADI_INSTALL_DIR:-${HOME}/casadi}";
+CASADIPATH="${CASADIPATH:-${HOME}/casadi}";
 CASADI_VERSION='3.4.0';
 CASADI_DOWNLOAD_URL="https://github.com/casadi/casadi/archive/${CASADI_VERSION}.zip";
 
 # run only if casadi build was not cached
-if [ ! -d "${CASADI_INSTALL_DIR}" ]; then
+if [ ! -d "${CASADIPATH}" ]; then
 	pushd "${TRAVIS_BUILD_DIR}/external";
 		curl -o casadi.zip -Ls "${CASADI_DOWNLOAD_URL}";
 		unzip -qq casadi.zip;
@@ -12,11 +12,10 @@ if [ ! -d "${CASADI_INSTALL_DIR}" ]; then
 		pushd "./casadi-${CASADI_VERSION}";
 			mkdir build;
 			pushd ./build;
-				cmake -DWITH_SELFCONTAINED=ON -DWITH_PYTHON=ON -DWITH_PYTHON3=ON -DCMAKE_INSTALL_PREFIX="${CASADI_INSTALL_DIR}" ..;
+				cmake -DWITH_SELFCONTAINED=ON -DWITH_PYTHON=ON -DWITH_PYTHON3=ON -DCMAKE_INSTALL_PREFIX="${CASADIPATH}" ..;
 				make -j 4 && make install;
 			popd;
 		popd;
 	popd;
 fi
-export PYTHONPATH="${CASADI_INSTALL_DIR}:$PYTHONPATH";
-export CASADIPATH="${CASADI_INSTALL_DIR}";
+export PYTHONPATH="${CASADIPATH}:$PYTHONPATH";
