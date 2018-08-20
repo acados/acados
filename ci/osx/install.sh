de38851616767ae3699caa14c695a9edc6f8e95a
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-20 08:11:01 +0200

    Remove sensless travis_retry for sourcing scripts

--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-travis_retry source "${SCRIPT_DIR}/install_ccache.sh";
-travis_retry source "${SHARED_SCRIPT_DIR}/install_swig.sh";
-travis_retry source "${SCRIPT_DIR}/install_casadi.sh";
+source "${SCRIPT_DIR}/install_ccache.sh";
+source "${SHARED_SCRIPT_DIR}/install_swig.sh";
+source "${SCRIPT_DIR}/install_casadi.sh";
