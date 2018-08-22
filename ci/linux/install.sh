982069d69c4e3593bd67f7897ad90f85f70b52bc
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-22 10:10:52 +0200

    Install numpy and scipy

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -1,1 +1,4 @@
 #!/bin/bash -e
+
+source "${SCRIPT_DIR}/install_apt_dependencies.sh";
+source "${SHARED_SCRIPT_DIR}/install_python_dependencies.sh";
---
+++ ci/linux/install_python_dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+pip install numpy scipy matplotlib;
