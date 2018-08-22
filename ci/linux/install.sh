31fb542f65031795c7c9bee4182c2497af5d2f53
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-22 10:51:06 +0200

    Download and install casadi binaries for linux

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -3,3 +3,4 @@
 source "${SCRIPT_DIR}/install_apt_dependencies.sh";
 source "${SHARED_SCRIPT_DIR}/install_python_dependencies.sh";
 source "${SHARED_SCRIPT_DIR}/install_eigen.sh";
+source "${SHARED_SCRIPT_DIR}/install_casadi.sh";
