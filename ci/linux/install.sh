4423349874c44e0c4224661968d0f9071133b4c7
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-22 10:58:22 +0200

    Install casadi on linux too (shared script)

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -4,3 +4,4 @@ source "${SCRIPT_DIR}/install_apt_dependencies.sh";
 source "${SHARED_SCRIPT_DIR}/install_python_dependencies.sh";
 source "${SHARED_SCRIPT_DIR}/install_eigen.sh";
 source "${SHARED_SCRIPT_DIR}/install_casadi.sh";
+source "${SHARED_SCRIPT_DIR}/install_swig.sh";
