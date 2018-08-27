e675eeae877e4117c6c4c42e6e54a0a8e7ce2c9b
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-27 08:07:06 +0200

    Make build script capable for debug and release
    The acados build script can now be used for both debug and release
    build. In both runs it will run ctest.

--- ci/shared/script_acados_release.sh
+++ ci/shared/script_acados_release.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -e
 ACADOS_INSTALL_DIR="${ACADOS_INSTALL_DIR:-${HOME}/acados}";
 
-cmake -E make_directory build;
-cmake -E chdir build cmake \
-		-D CMAKE_BUILD_TYPE=Release \
-		-D CMAKE_INSTALL_PREFIX="${ACADOS_INSTALL_DIR}" \
-		-D SWIG_PYTHON=ON \
-		-D BUILD_SHARED_LIBS=ON \
-		..;
-cmake --build build --target install;
+function build_acados {
+	BUILD_TYPE="Debug";  # Release or Debug
+	UNIT_TESTS='ON';
+	if [ "${BUILD_TYPE}" = 'Release' ]; then
+		BUILD_TYPE='Release';
+		UNIT_TESTS='OFF';
+	fi
+
+	cmake -E make_directory build;
+	cmake -E chdir build cmake \
+			-D CMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+			-D UNIT_TESTS="${UNIT_TESTS}" \
+			-D CMAKE_INSTALL_PREFIX="${ACADOS_INSTALL_DIR}" \
+			-D SWIG_PYTHON=ON \
+			-D BUILD_SHARED_LIBS=ON \
+			..;
+	cmake --build build;
+	cmake -E chdir build ctest --output-on-failure;
+	# only install release version
+	[ "${BUILD_TYPE}" = 'Release' ] && cmake --build build --target install;
+}
+
+build_acados Debug;
+build_acados Release;
