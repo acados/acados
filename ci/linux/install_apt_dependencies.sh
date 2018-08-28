fd228c8bf5702ea7beb2c63e15afde162d2e568b
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-08-28 08:28:06 +0200

    Install tkinter and dev files for py3.5 on linux

--- ci/linux/install_apt_dependencies.sh
+++ ci/linux/install_apt_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash -e
 sudo apt-get update -yqq;
-sudo apt-get --allow-unauthenticated install -yqq $CXX $CC python3.5;
+sudo apt-get --allow-unauthenticated install -yqq $CXX $CC python3.5 python3.5-tk python3.5-dev;
