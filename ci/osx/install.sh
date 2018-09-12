77ced961d8ba5b0f2a27749f1f170b6d62de1761
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-09-12 15:46:18 +0200

    Install matplotlib on osx on travis

--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 source "${SCRIPT_DIR}/install_ccache.sh";
+source "${SCRIPT_DIR}/install_python_dependencies.sh";
 source "${SHARED_SCRIPT_DIR}/install_eigen.sh";
 source "${SHARED_SCRIPT_DIR}/install_swig.sh";
 source "${SCRIPT_DIR}/install_casadi.sh";
---
+++ ci/osx/install_python_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+which python;
+which pip3;
+python --version;
+pip install --user matplotlib;
