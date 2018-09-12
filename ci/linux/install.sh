efcd5fcaa47c3ff1d73feb13e6642708ddf35e9b
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2018-09-12 15:19:15 +0200

    Run tests without using miniconda

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -2,6 +2,6 @@
 
 source "${SCRIPT_DIR}/install_apt_dependencies.sh";
 source "${SCRIPT_DIR}/install_python_dependencies.sh";
-source "${SCRIPT_DIR}/install_eigen.sh";
+source "${SHARED_SCRIPT_DIR}/install_eigen.sh";
 source "${SCRIPT_DIR}/install_casadi.sh";
 source "${SHARED_SCRIPT_DIR}/install_swig.sh";
--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 source "${SCRIPT_DIR}/install_ccache.sh";
+source "${SHARED_SCRIPT_DIR}/install_eigen.sh";
 source "${SHARED_SCRIPT_DIR}/install_swig.sh";
 source "${SCRIPT_DIR}/install_casadi.sh";
