d66c9920f10bce3e41f35fc640f695399aa36906
giaf <gianfri2008@hotmail.it>
2018-10-13 10:10:41 +0200

    use wrapper to decouple user_name and acados_name of casadi functions

--- interfaces/python/acados_integrator.py
+++ interfaces/python/acados_integrator.py
@@ -66,13 +66,14 @@ class acados_integrator:
 		casadi_ode_expr = -2*x
 
 		# Form a function and generate C code
-		name = 'ode_expr'
-		python_ode_expr = Function(name, [x], [casadi_ode_expr], ['x'], ['ode_expr'])
+		user_name = 'ode_expr'
+		python_ode_expr = Function(user_name, [x], [casadi_ode_expr], ['x'], ['ode_expr'])
 		cname = python_ode_expr.generate()
 
 
 		## generate wrapper
-		generate_wrapper(name)
+		acados_name = 'expl_ode_fun'
+		generate_wrapper(user_name, acados_name)
 
 
 
@@ -93,39 +94,40 @@ class acados_integrator:
 		self.ext_fun = ext_fun_struct
 
 		# addressof does not work !
-#		tmp_ptr = cast(addressof(__model.ode_expr_fun), c_void_p)
+#		tmp_ptr = cast(addressof(__model.expl_ode_fun_fun), c_void_p)
 #		print(tmp_ptr)
 		# pointer does not work !
-#		tmp_ptr = pointer(__model.ode_expr_fun)
+#		tmp_ptr = pointer(__model.expl_ode_fun_fun)
 #		print(tmp_ptr)
 #		print(addressof(tmp_ptr))
-		__model.get_ode_expr_fun.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_fun()
+		__model.get_expl_ode_fun_fun.restype = c_void_p
+#		eval("__model.get_"+name+"_fun.restype = c_void_p")
+		tmp_ptr = __model.get_expl_ode_fun_fun()
 		__acados.external_function_casadi_set_fun.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_fun(self.ext_fun, tmp_ptr)
 
-		__model.get_ode_expr_work.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_work()
+		__model.get_expl_ode_fun_work.restype = c_void_p
+		tmp_ptr = __model.get_expl_ode_fun_work()
 		__acados.external_function_casadi_set_work.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_work(self.ext_fun, tmp_ptr)
 
-		__model.get_ode_expr_sparsity_in.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_sparsity_in()
+		__model.get_expl_ode_fun_sparsity_in.restype = c_void_p
+		tmp_ptr = __model.get_expl_ode_fun_sparsity_in()
 		__acados.external_function_casadi_set_sparsity_in.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_sparsity_in(self.ext_fun, tmp_ptr)
 
-		__model.get_ode_expr_sparsity_out.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_sparsity_out()
+		__model.get_expl_ode_fun_sparsity_out.restype = c_void_p
+		tmp_ptr = __model.get_expl_ode_fun_sparsity_out()
 		__acados.external_function_casadi_set_sparsity_out.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_sparsity_out(self.ext_fun, tmp_ptr)
 
-		__model.get_ode_expr_n_in.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_n_in()
+		__model.get_expl_ode_fun_n_in.restype = c_void_p
+		tmp_ptr = __model.get_expl_ode_fun_n_in()
 		__acados.external_function_casadi_set_n_in.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_n_in(self.ext_fun, tmp_ptr)
 
-		__model.get_ode_expr_n_out.restype = c_void_p
-		tmp_ptr = __model.get_ode_expr_n_out()
+		__model.get_expl_ode_fun_n_out.restype = c_void_p
+		tmp_ptr = __model.get_expl_ode_fun_n_out()
 		__acados.external_function_casadi_set_n_out.argtypes = [c_void_p, c_void_p]
 		__acados.external_function_casadi_set_n_out(self.ext_fun, tmp_ptr)
 
--- interfaces/python/generate_wrapper.py
+++ interfaces/python/generate_wrapper.py
@@ -1,21 +1,23 @@
-def generate_wrapper(name):
-	print(name)
+def generate_wrapper(user_name, acados_name):
 
-	file = open("wrapper_"+name+".c", "w")
+#	print(user_name)
+#	print(acados_name)
 
-	file.write("int "+name+"(const double** arg, double** res, int* iw, double* w, void* mem);\n")
-	file.write("int "+name+"_work(int *sz_arg, int* sz_res, int *sz_iw, int *sz_w);\n")
-	file.write("const int* "+name+"_sparsity_in(int i);\n")
-	file.write("const int* "+name+"_sparsity_out(int i);\n")
-	file.write("int "+name+"_n_in(void);\n")
-	file.write("int "+name+"_n_out(void);\n")
+	file = open("wrapper_"+user_name+".c", "w")
+
+	file.write("int "+user_name+"(const double** arg, double** res, int* iw, double* w, void* mem);\n")
+	file.write("int "+user_name+"_work(int *sz_arg, int* sz_res, int *sz_iw, int *sz_w);\n")
+	file.write("const int* "+user_name+"_sparsity_in(int i);\n")
+	file.write("const int* "+user_name+"_sparsity_out(int i);\n")
+	file.write("int "+user_name+"_n_in(void);\n")
+	file.write("int "+user_name+"_n_out(void);\n")
 	file.write("\n")
-	file.write("void *get_"+name+"_fun() { return &"+name+"; }\n")
-	file.write("void *get_"+name+"_work() { return &"+name+"_work; }\n")
-	file.write("void *get_"+name+"_sparsity_in() { return &"+name+"_sparsity_in; }\n")
-	file.write("void *get_"+name+"_sparsity_out() { return &"+name+"_sparsity_out; }\n")
-	file.write("void *get_"+name+"_n_in() { return &"+name+"_n_in; }\n")
-	file.write("void *get_"+name+"_n_out() { return &"+name+"_n_out; }\n")
+	file.write("void *get_"+acados_name+"_fun() { return &"+user_name+"; }\n")
+	file.write("void *get_"+acados_name+"_work() { return &"+user_name+"_work; }\n")
+	file.write("void *get_"+acados_name+"_sparsity_in() { return &"+user_name+"_sparsity_in; }\n")
+	file.write("void *get_"+acados_name+"_sparsity_out() { return &"+user_name+"_sparsity_out; }\n")
+	file.write("void *get_"+acados_name+"_n_in() { return &"+user_name+"_n_in; }\n")
+	file.write("void *get_"+acados_name+"_n_out() { return &"+user_name+"_n_out; }\n")
 
 
 	file.close()
