9f250add75614355e54106242b2b5f168606e3c1
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-10-16 12:01:01 +0200

    adding acados_integrator MATLAB class

---
+++ interfaces/matlab/+acados/sim/acados_integrator.m
@@ -0,0 +1,29 @@
+classdef acados_integrator
+	
+	properties
+		hp
+		np
+		py_acados_integrator
+	end
+
+	methods
+
+		function obj = acados_integrator(model, opts)
+			obj.np = py.importlib.import_module('numpy');
+			obj.ac = py.importlib.import_module('acados');
+			obj.py_acados_integrator = obj.ac.acados_integrator(model.py_acados_model, opts.py_acados_otps);
+		end
+
+		function set(obj, field, value)
+            obj.py_acados_intergrator.set(field, value);
+		end
+
+		function solve()
+            obj.py_acados_intergrator.solve();
+		end
+
+		function get(obj, field, value)
+            obj.py_acados_intergrator.get(field);
+		end
+	end
+end
