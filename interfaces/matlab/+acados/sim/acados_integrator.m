8b4e88a4195845c7f349973cf8da177dc6c00471
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-10-16 12:13:22 +0200

    added acados_integrator_opts class

--- interfaces/matlab/+acados/sim/acados_integrator.m
+++ interfaces/matlab/+acados/sim/acados_integrator.m
@@ -19,6 +19,27 @@ classdef acados_integrator_model
 	end
 end
 
+classdef acados_integrator_opts
+	
+	properties
+		ac
+		np
+		py_acados_integrator_opts
+	end
+
+	methods
+		function obj = acados_integrator_opts()
+			obj.np = py.importlib.import_module('numpy');
+			obj.ac = py.importlib.import_module('acados');
+			obj.py_acados_integrator_opts = obj.ac.acados_integrator_opts();
+		end
+
+		function set(obj, field, value)
+            obj.py_acados_intergrator_opts.set(field, value);
+		end
+	end
+end
+
 classdef acados_integrator
 	
 	properties
