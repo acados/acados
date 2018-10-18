5f976118715cab1509f4056ca3a304c218da0f8e
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-10-18 14:30:50 +0200

    adapt example

--- interfaces/matlab/examples/test.m
+++ interfaces/matlab/examples/test.m
@@ -20,29 +20,29 @@ tic
 
 sim_model = acados_integrator_model();
 sim_model.set('type', 'explicit');
-%sim_model = sim_model.set('type', 'implicit');
-sim_model = sim_model.set('ode_expr', expl_ode_expr);
-%sim_model = sim_model.set('ode_expr', impl_ode_expr);
-sim_model = sim_model.set('x', x);
-%sim_model = sim_model.set('xdot', xdot);
-sim_model = sim_model.set('model_name', 'expl_model');
-%sim_model = sim_model.set('model_name', 'impl_model');
+% sim_model.set('type', 'implicit');
+sim_model.set('ode_expr', expl_ode_expr);
+% sim_model.set('ode_expr', impl_ode_expr);
+sim_model.set('x', x);
+% sim_model.set('xdot', xdot);
+sim_model.set('model_name', 'expl_model');
+% sim_model.set('model_name', 'impl_model');
 
 sim_model_create_time = toc;
 fprintf('sim_model time %e\n', sim_model_create_time);
 
-%sim_model
+% sim_model
 
 
 tic
 
 sim_opts = acados_integrator_opts();
-sim_opts = sim_opts.set('scheme', 'erk');
-%sim_opts = sim_opts.set('scheme', 'irk');
-sim_opts = sim_opts.set('sens_forw', 'true');
-%sim_opts = sim_opts.set('sens_forw', 'false');
-sim_opts = sim_opts.set('codgen_model', 'true');
-%sim_opts = sim_opts.set('codgen_model', 'false');
+sim_opts.set('scheme', 'erk');
+% sim_opts.set('scheme', 'irk');
+sim_opts.set('sens_forw', 'true');
+% sim_opts.set('sens_forw', 'false');
+sim_opts.set('codgen_model', 'true');
+% sim_opts.set('codgen_model', 'false');
 
 sim_opts_create_time = toc;
 fprintf('sim_opts time %e\n', sim_opts_create_time);
