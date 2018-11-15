493b1a0e12d3c686c244994d2f8d8d52a3915af3
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-11-15 15:03:44 +0100

    using ocp_nlp_dims class to pass dimensions

--- examples/c/templating/generate_c_code.py
+++ examples/c/templating/generate_c_code.py
@@ -1,12 +1,18 @@
 from jinja2 import Environment, FileSystemLoader
+from ocp_nlp_dims import *
 
+# setting up loader and environment
 file_loader = FileSystemLoader('c_templates')
-
 env = Environment(loader = file_loader)
-
 template = env.get_template('template_example.in.c')
 
-output = template.render(nx=4)
+# set ocp_nlp_dimensions
+nlp_dims = ocp_nlp_dims()
+nlp_dims.nx = 4
 
+# render template
+output = template.render(ocp_nlp_dims=nlp_dims)
+
+# output file
 out_file = open('./c_generated_code/template_example.c', 'w+')
 out_file.write(output)
