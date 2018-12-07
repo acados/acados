dc931115206eb17690a6859ebf729d2ddcc521b6
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-12-07 12:18:28 +0100

    fixed ctypes example

--- interfaces/acados_template/examples/run_acados.py
+++ interfaces/acados_template/examples/run_acados.py
@@ -3,5 +3,5 @@ from ctypes import *
 acados   = CDLL('c_generated_code/acados_solver_pendulum_ode.so')
 import pdb; pdb.set_trace()
 acados.acados_create()
-acados.call_solve()
-acados.call_destroy()
+acados.acados_solve()
+acados.acados_free()
