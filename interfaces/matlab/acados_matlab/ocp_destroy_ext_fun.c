c74cbe030b06ceea8d5f97f818063ba809fe82fc
giaf <gianfri2008@hotmail.it>
2018-12-17 19:43:40 +0100

    fix bug to ocp_destroy_ext_fun (thanks tommy)

--- interfaces/matlab/acados_matlab/ocp_destroy_ext_fun.c
+++ interfaces/matlab/acados_matlab/ocp_destroy_ext_fun.c
@@ -25,49 +25,49 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
 	external_function_casadi *ext_fun_ptr;
 
-	if (mxGetField( prhs[1], 0, "expl_ode_fun" )!=NULL)
+	if (mxGetField( prhs[0], 0, "expl_ode_fun" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "expl_ode_fun" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "expl_vde_for" )!=NULL)
+	if (mxGetField( prhs[0], 0, "expl_vde_for" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "expl_vde_for" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "expl_vde_adj" )!=NULL)
+	if (mxGetField( prhs[0], 0, "expl_vde_adj" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "expl_vde_adj" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "impl_ode_fun" )!=NULL)
+	if (mxGetField( prhs[0], 0, "impl_ode_fun" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "impl_ode_fun" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "impl_ode_fun_jac_x_xdot" )!=NULL)
+	if (mxGetField( prhs[0], 0, "impl_ode_fun_jac_x_xdot" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "impl_ode_fun_jac_x_xdot" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "impl_ode_jac_x_xdot_u" )!=NULL)
+	if (mxGetField( prhs[0], 0, "impl_ode_jac_x_xdot_u" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "impl_ode_jac_x_xdot_u" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
 		external_function_casadi_free(ext_fun_ptr);
 		free(ext_fun_ptr);
 		}
-	if (mxGetField( prhs[1], 0, "h_fun_jac_ut_xt" )!=NULL)
+	if (mxGetField( prhs[0], 0, "h_fun_jac_ut_xt" )!=NULL)
 		{
 		ptr = (long long *) mxGetData( mxGetField( prhs[0], 0, "h_fun_jac_ut_xt" ) );
 		ext_fun_ptr = (external_function_casadi *) ptr[0];
