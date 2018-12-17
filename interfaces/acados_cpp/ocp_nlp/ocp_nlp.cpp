f9f8fcc0fc7aea985897857ebffead2f06d8cf54
Jonathan Frey <jonathanpaulfrey@gmail.com>
2018-12-17 18:00:47 +0100

    adapted ocp_nlp.cpp to modified setter

--- interfaces/acados_cpp/ocp_nlp/ocp_nlp.cpp
+++ interfaces/acados_cpp/ocp_nlp/ocp_nlp.cpp
@@ -449,7 +449,7 @@ void ocp_nlp::set_dynamics(const casadi::Function &model, std::map<std::string, 
     cached_model_ = module_["expl_vde_for"].name();
 
     for (int stage = 0; stage < N; ++stage)
-        ocp_nlp_dynamics_model_set(config_.get(), nlp_.get(), stage, "expl_vde_for",
+        ocp_nlp_dynamics_model_set(config_.get(), dims_.get(), nlp_.get(), stage, "expl_vde_for",
                                (void *) module_["expl_vde_for"].as_external_function());
 
 };
