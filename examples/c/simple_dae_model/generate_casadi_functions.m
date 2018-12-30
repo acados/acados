3783c61c80b85ebeb0b73ad3317d1fd16c21f42c
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2018-12-30 12:57:50 +0100

    adding code for simple dae model

---
+++ examples/c/simple_dae_model/generate_casadi_functions.m
@@ -0,0 +1,60 @@
+%
+%   This file is part of acados.
+%
+%   acados is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU Lesser General Public
+%   License as published by the Free Software Foundation; either
+%   version 3 of the License, or (at your option) any later version.
+%
+%   acados is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with acados; if not, write to the Free Software Foundation,
+%   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+%
+
+clc;
+clear all;
+close all;
+
+% addpath('/home/andrea/casadi-linux-octave-v3.4.0') 
+% addpath('~/casadi/swig/octave')
+
+import casadi.*
+
+% casadi opts for code generation
+if CasadiMeta.version()=='3.4.0'
+	% casadi 3.4
+	opts = struct('mex', false, 'casadi_int', 'int', 'casadi_real', 'double');
+else
+	% old casadi versions
+	error('Please download and install Casadi 3.4.0')
+end
+
+NX = 2;
+NU = 2;
+NZ = 2;
+
+% define model 
+dae = export_simple_dae_model();
+
+% Set up DAE
+ode_impl    = dae.f_impl_expr; 
+
+jac_x = SX.zeros(NX+NZ, NX) + jacobian(ode_impl, dae.x);
+jac_xdot = SX.zeros(NX+NZ, NX) + jacobian(ode_impl, dae.xdot);
+jac_z = SX.zeros(NX+NZ, NZ) + jacobian(ode_impl, dae.z);
+jac_u = SX.zeros(NX+NZ, NU) + jacobian(ode_impl, dae.u);
+
+impl_ode_fun = Function('casadi_impl_ode_fun_simple_dae', {dae.x, dae.xdot, dae.z, dae.u}, {ode_impl});
+impl_ode_fun_jac_x_xdot_z = Function('casadi_impl_ode_fun_jac_x_xdot_z_simple_dae', {dae.x, dae.xdot, dae.z, dae.u}, {ode_impl, jac_x, jac_xdot, jac_z});
+impl_ode_fun_jac_x_xdot_z_u = Function('casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae', {dae.x, dae.xdot, dae.z, dae.u}, {ode_impl, jac_x, jac_xdot, jac_z, jac_u});
+impl_ode_jac_x_xdot_z_u = Function('casadi_impl_ode_jac_x_xdot_z_u_simple_dae', {dae.x, dae.xdot, dae.z, dae.u}, {jac_x, jac_xdot, jac_z, jac_u});
+
+impl_ode_fun.generate('impl_ode_fun_simple_dae', opts);
+impl_ode_fun_jac_x_xdot_z.generate('impl_ode_fun_jac_x_xdot_z_simple_dae', opts);
+impl_ode_fun_jac_x_xdot_z_u.generate('impl_ode_fun_jac_x_xdot_z_u_simple_dae', opts);
+impl_ode_jac_x_xdot_z_u.generate('impl_ode_jac_x_xdot_z_u_simple_dae', opts);
---
+++ examples/c/simple_dae_model/simple_dae_model.h
@@ -0,0 +1,45 @@
+
+#ifndef EXAMPLES_C_SIMPLE_DAE
+#define EXAMPLES_C_SIMPLE_DAE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// implicit ODE
+int casadi_impl_ode_fun_simple_dae(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int casadi_impl_ode_fun_simple_dae_work(int *, int *, int *, int *);
+const int *casadi_impl_ode_fun_simple_dae_sparsity_in(int);
+const int *casadi_impl_ode_fun_simple_dae_sparsity_out(int);
+int casadi_impl_ode_fun_simple_dae_n_in();
+int casadi_impl_ode_fun_simple_dae_n_out();
+
+// implicit ODE
+int casadi_impl_ode_fun_jac_x_xdot_z_simple_dae(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int casadi_impl_ode_fun_jac_x_xdot_z_simple_dae_work(int *, int *, int *, int *);
+const int *casadi_impl_ode_fun_jac_x_xdot_z_simple_dae_sparsity_in(int);
+const int *casadi_impl_ode_fun_jac_x_xdot_z_simple_dae_sparsity_out(int);
+int casadi_impl_ode_fun_jac_x_xdot_z_simple_dae_n_in();
+int casadi_impl_ode_fun_jac_x_xdot_z_simple_dae_n_out();
+
+// implicit ODE
+int casadi_impl_ode_jac_x_xdot_z_u_simple_dae(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int casadi_impl_ode_jac_x_xdot_z_u_simple_dae_work(int *, int *, int *, int *);
+const int *casadi_impl_ode_jac_x_xdot_z_u_simple_dae_sparsity_in(int);
+const int *casadi_impl_ode_jac_x_xdot_z_u_simple_dae_sparsity_out(int);
+int casadi_impl_ode_jac_x_xdot_z_u_simple_dae_n_in();
+int casadi_impl_ode_jac_x_xdot_z_u_simple_dae_n_out();
+
+// implicit ODE - for new_lifted_irk
+int casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae_work(int *, int *, int *, int *);
+const int *casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae_sparsity_in(int);
+const int *casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae_sparsity_out(int);
+int casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae_n_in();
+int casadi_impl_ode_fun_jac_x_xdot_z_u_simple_dae_n_out();
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif  // EXAMPLES_C_SIMPLE_DAE
