bb4efeba701ca9e9eff0a5d3ba400b9a82d17375
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-01-31 17:37:37 +0100

    adding code for generation of pd part of constraints

--- interfaces/acados_template/acados_template/generate_c_code_p_constraint.py
+++ interfaces/acados_template/acados_template/generate_c_code_p_constraint.py
@@ -18,7 +18,7 @@
 from casadi import *
 import os
 
-def generate_c_code_constraint( model ):
+def generate_c_code_p_constraint( p_constraint ):
 
     casadi_version = CasadiMeta.version()
     casadi_opts = dict(mex=False, casadi_int='int', casadi_real='double')
@@ -27,71 +27,32 @@ def generate_c_code_constraint( model ):
         # old casadi versions
         raise Exception('Please download and install Casadi 3.4.0 to ensure compatibility with acados. Version ' + casadi_version + ' currently in use.')
 
-    # load model
-    x = model.x
-    u = model.u
-    f_expl = model.expr
-    model_name = model.name
+    # load constraint variables and expression
+    x = p_contraint.x
+    u = p_constraint.u
+    np = p_constraint.np
+    p_exp = p_constraint.expr
+    con_name = p_constraint.name
 
-    ## get model dimensions
+    ## get dimensions
     nx = x.size()[0]
     nu = u.size()[0]
 
     ## set up functions to be exported
-    Sx = SX.sym('Sx', nx, nx)
-    Sp = SX.sym('Sp', nx, nu)
-    lambdaX = SX.sym('lambdaX', nx, 1)
-
-    fun_name = model_name + '_expl_ode_fun'
-    expl_ode_fun = Function(fun_name, [x,u], [f_expl])
-    # TODO: Polish: get rid of SX.zeros
-    vdeX = SX.zeros(nx,nx)
-    vdeX = vdeX + jtimes(f_expl,x,Sx)
-
-    vdeP = SX.zeros(nx,nu) + jacobian(f_expl,u)
-    vdeP = vdeP + jtimes(f_expl,x,Sp)
-
-    fun_name = model_name + '_expl_vde_forw'
-    expl_vde_forw = Function(fun_name, [x,Sx,Sp,u], [f_expl,vdeX,vdeP])
-
-    jacX = SX.zeros(nx,nx) + jacobian(f_expl,x)
-
-    adj = jtimes(f_expl, vertcat(x, u), lambdaX, True)
-
-    fun_name = model_name + '_expl_vde_adj'
-    expl_vde_adj = Function(fun_name, [x,lambdaX,u], [adj])
-
-    S_forw = vertcat(horzcat(Sx, Sp), horzcat(DM.zeros(nu,nx), DM.eye(nu)))
-    hess = mtimes(transpose(S_forw),jtimes(adj, vertcat(x,u), S_forw))
-    hess2 = []
-    for j in range(nx+nu):
-        for i in range(j,nx+nu):
-            hess2 = vertcat(hess2, hess[i,j])
-
-    fun_name = model_name + '_expl_ode_hess'
-    expl_ode_hess = Function(fun_name, [x, Sx, Sp, lambdaX, u], [adj, hess2])
+    fun_name = con_name
+    # TODO(andrea): first output seems to be ignored in the C code
+    p_constraint_fun_jac = Function(fun_name, [x,u], [p_exp, SX.zeros(nx+nu, np) + jacobian(p_exp, vertcat(x, u))])
 
     ## generate C code
     if not os.path.exists('c_generated_code'):
         os.mkdir('c_generated_code')
 
     os.chdir('c_generated_code')
-    model_dir = model_name + '_model'
-    if not os.path.exists(model_dir):
-        os.mkdir(model_dir)
-    model_dir_location = './' + model_dir
-    os.chdir(model_dir_location)
-    fun_name = model_name + '_expl_ode_fun'
-    expl_ode_fun.generate(fun_name, casadi_opts)
-
-    fun_name = model_name + '_expl_vde_forw'
-    expl_vde_forw.generate(fun_name, casadi_opts)
-    
-    fun_name = model_name + '_expl_vde_adj'
-    expl_vde_adj.generate(fun_name, casadi_opts)
-    
-    fun_name = model_name + '_expl_ode_hess'
-    expl_ode_hess.generate(fun_name, casadi_opts)
-    os.chdir('../..')
+    gen_dir = con_name 
+    if not os.path.exists(gen_dir):
+        os.mkdir(gen_dir)
+    gen_dir_location = './' + gen_dir
+    os.chdir(gen_dir_location)
+    p_constraint_fun_jac.generate(gen_name, casadi_opts)
 
     return
