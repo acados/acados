c6c18377fb5ef8b588990b691543777d1016be8e
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-04-03 12:04:20 +0200

    add casadi code generation to MATLAB interface

--- interfaces/acados_template/examples/matlab/pendulum_example.m
+++ interfaces/acados_template/examples/matlab/pendulum_example.m
@@ -2,7 +2,11 @@ clear all
 close all
 clc
 import acados_template.*
+import casadi.*
 
+% export model 
+model = export_ode_model();
+    
 % create ocp_nlp object
 acados_ocp_nlp = acados_ocp_nlp();
 
@@ -94,5 +98,62 @@ if fid == -1, error('Cannot create JSON file'); end
 fwrite(fid, json_string, 'char');
 fclose(fid);
 
-% generate_solver(model, acados_ocp_nlp)
+opts.generate_hess = 1;
+generate_c_code_implicit_ode(model, opts);
+generate_c_code_explicit_ode(model);
+
+function model = export_ode_model()
+    import casadi.*
+    import acados_template.*
+    model_name = 'pendulum_ode';
+
+    % constants
+    M = 1;
+    m = 0.1;
+    g = 9.81;
+    l = 0.8;
+
+    % set up states & controls
+    x1      = SX.sym('x1');
+    theta   = SX.sym('theta');
+    v1      = SX.sym('v1');
+    dtheta  = SX.sym('dtheta');
+    
+    x = vertcat(x1, v1, theta, dtheta);
+
+    % controls
+    F = SX.sym('F');
+    u = vertcat(F);
+    
+    % xdot
+    x1_dot      = SX.sym('x1_dot');
+    theta_dot   = SX.sym('theta_dot');
+    v1_dot      = SX.sym('v1_dot');
+    dtheta_dot  = SX.sym('dtheta_dot');
+
+    xdot = vertcat(x1_dot, theta_dot, v1_dot, dtheta_dot);
+    
+    % algebraic variables
+    z = [];
+
+    % parameters
+    p = [];
+    
+    % dynamics     
+    denominator = M + m - m*cos(theta)*cos(theta);
+    f_expl = vertcat(v1, (-m*l*sin(theta)*dtheta*dtheta + m*g*cos(theta)*sin(theta)+F)/denominator, dtheta, (-m*l*cos(theta)*sin(theta)*dtheta*dtheta + F*cos(theta)+(M+m)*g*sin(theta))/(l*denominator));
+    
+    f_impl = xdot - f_expl;
+   
+    model = acados_dae();
+
+    model.f_impl_expr = f_impl;
+    model.f_expl_expr = f_expl;
+    model.x = x;
+    model.xdot = xdot;
+    model.u = u;
+    model.z = z;
+    model.p = p;
+    model.name = model_name;
+end
 
