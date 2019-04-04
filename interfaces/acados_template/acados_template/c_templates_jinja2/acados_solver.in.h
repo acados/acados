53af7dccc3c0797ff33bbd3704dc2f44022995a1
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-04-04 18:17:27 +0200

    adding jinja2 templates

---
+++ interfaces/acados_template/acados_template/c_templates_jinja2/acados_solver.in.h
@@ -0,0 +1,64 @@
+#ifndef ACADOS_SOLVER_{{ocp.model_name.upper()}}_H_
+#define ACADOS_SOLVER_{{ocp.model_name.upper()}}_H_
+
+#include "acados_c/ocp_nlp_interface.h"
+#include "acados_c/external_function_interface.h"
+
+int acados_create();
+int acados_solve();
+int acados_free();
+
+ocp_nlp_in * acados_get_nlp_in();
+ocp_nlp_out * acados_get_nlp_out();
+ocp_nlp_solver * acados_get_nlp_solver();
+ocp_nlp_config * acados_get_nlp_config();
+void * acados_get_nlp_opts();
+ocp_nlp_dims * acados_get_nlp_dims();
+
+// ** global data **
+extern ocp_nlp_in * nlp_in;
+extern ocp_nlp_out * nlp_out;
+extern ocp_nlp_solver * nlp_solver;
+extern void * nlp_opts;
+extern ocp_nlp_plan * nlp_solver_plan;
+extern ocp_nlp_config * nlp_config;
+extern ocp_nlp_dims * nlp_dims;
+{% if ocp.solver_config.integrator_type == 'ERK': %}
+{% if ocp.dims.np < 1: %}
+extern external_function_casadi * forw_vde_casadi;
+{% else: %}
+extern external_function_param_casadi * forw_vde_casadi;
+{% endif %}
+{% if ocp.solver_config.hessian_approx == 'EXACT': %} 
+{% if ocp.dims.np < 1: %}
+extern external_function_casadi * hess_vde_casadi;
+{% else: %}
+extern external_function_param_casadi * hess_vde_casadi;
+{% endif %}
+{% endif %}
+{% elif ocp.solver_config.integrator_type == 'IRK': %}
+{% if ocp.dims.np < 1: %}
+extern external_function_casadi * impl_dae_fun;
+extern external_function_casadi * impl_dae_fun_jac_x_xdot_z;
+extern external_function_casadi * impl_dae_jac_x_xdot_u_z;
+{% else: %}
+extern external_function_param_casadi * impl_dae_fun;
+extern external_function_param_casadi * impl_dae_fun_jac_x_xdot_z;
+extern external_function_param_casadi * impl_dae_jac_x_xdot_u_z;
+{% endif %}
+{% endif %}
+{% if ocp.dims.npd > 0: %}
+extern external_function_casadi * p_constraint;
+{% endif %}
+{% if ocp.dims.npdN > 0: %}
+extern external_function_casadi * p_constraint_N;
+{% endif %}
+{% if ocp.dims.nh > 0: %}
+extern external_function_casadi * h_constraint;
+{% endif %}
+{% if ocp.dims.nhN > 0: %}
+extern external_function_casadi * h_constraint_N;
+{% endif %}
+
+
+#endif  // ACADOS_SOLVER_{{ocp.model_name.upper()}}_H_
---
+++ interfaces/acados_template/acados_template/c_templates_jinja2/h_constraint.in.h
@@ -0,0 +1,22 @@
+#ifndef {{ ocp.con_h_name.upper() }}_H_CONSTRAINT
+#define {{ ocp.con_h_name.upper() }}_H_CONSTRAINT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+{% if ocp.dims.nh > 0: %}
+// implicit ODE
+int {{ ocp.con_h_name }}_h_constraint(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int {{ ocp.con_h_name }}_h_constraint_work(int *, int *, int *, int *);
+const int *{{ ocp.con_h_name }}_h_constraint_sparsity_in(int);
+const int *{{ ocp.con_h_name }}_h_constraint_sparsity_out(int);
+int {{ ocp.con_h_name }}_h_constraint_n_in();
+int {{ ocp.con_h_name }}_h_constraint_n_out();
+{% endif %}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif  // {{ ocp.con_h_name.upper() }}_H_CONSTRAINT
