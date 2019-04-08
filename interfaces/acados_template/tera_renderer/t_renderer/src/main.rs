3c3b5509dd7608088995477e16332eccef7931fd
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-04-08 20:34:55 +0200

    polishing rust renderer, using optimization in release

--- interfaces/acados_template/tera_renderer/t_renderer/src/main.rs
+++ interfaces/acados_template/tera_renderer/t_renderer/src/main.rs
@@ -1,5 +1,5 @@
 #[macro_use] extern crate tera;
-#[macro_use] extern crate lazy_static;
+// #[macro_use] extern crate lazy_static;
 
 use std::collections::HashMap;
 
@@ -13,15 +13,6 @@ use std::io::Read;
 
 use std::env;
 
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let mut tera = compile_templates!("../../acados_template/c_templates_tera/*");
-        tera.autoescape_on(vec![".swp"]);
-        tera.register_filter("do_nothing", do_nothing_filter);
-        tera
-    };
-}
-
 pub fn do_nothing_filter(value: Value, _: HashMap<String, Value>) -> Result<Value> {
     let s = try_get_value!("do_nothing_filter", "value", String, value);
     Ok(to_value(&s).unwrap())
@@ -31,44 +22,30 @@ fn main() -> io::Result<()> {
     // read command line arguments
     let args: Vec<String> = env::args().collect();
 
-    let template_path = &args[1];
-    let template_file = &args[2];
-    let json_file     = &args[3];
-    let out_file      = &args[4];
+    let template_path = &args[1]; // relative path to template file
+    let template_file = &args[2]; // (absolute) template file path
+    let json_file     = &args[3]; // relative path json file
+    let out_file      = &args[4]; // relative path to output file
 
+    // print arguments
     println!("template path: {}", template_path);
     println!("template file: {}", template_file);
     println!("json file {}:"    , json_file);
     println!("out file {}:"     , out_file);
 
-    let mut file = File::open("../../acados_template/c_templates/acados_ocp_nlp.json")?;
+    let mut file = File::open(json_file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("{}", contents);
     
     // Parse the string of data into serde_json::Value.
     let v: Value = serde_json::from_str(&contents)?;
-    // let result = Tera::one_off(template_file, &v, true).unwrap();
-    // println!("{}", result);
 
-    // let mut tera = compile_templates!(template_path);
-    // tera.render(template_file, &v) {
-    //     Ok(s) => {
-    //         let string_list = vec!["Foo".to_string(),"Bar".to_string()];
-    //         let joined = string_list.join("-");
-    //         let mut f_out = File::create("acados_solver.c").expect("Unable to create file");
-    //         f_out.write_all(s.as_bytes())?;
-    //     },
-    //     Err(e) => {
-    //         println!("Error: {}", e);
-    //         for e in e.iter().skip(1) {
-    //             println!("Reason: {}", e);
-    //         }
-    //     }
-    // };
+    let mut tera = compile_templates!(template_path);
+    tera.autoescape_on(vec![".swp"]);
+    tera.register_filter("do_nothing", do_nothing_filter);
 
-    let args: Vec<String> = env::args().collect();
-    match TEMPLATES.render(template_file, &v) {
+    match tera.render(template_file, &v) {
         Ok(s) => {
             let string_list = vec!["Foo".to_string(),"Bar".to_string()];
             let joined = string_list.join("-");
@@ -82,6 +59,7 @@ fn main() -> io::Result<()> {
             }
         }
     };
+
     println!("\n -> successfully rendered templates!");
     Ok(())
 }
