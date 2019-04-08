fd5e567caeaa1ac04c6a4d1ba010ed751eb524ec
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-04-08 17:21:20 +0200

    adding cmd arguments

--- interfaces/acados_template/test_rust/hello_world/src/main.rs
+++ interfaces/acados_template/test_rust/hello_world/src/main.rs
@@ -7,12 +7,14 @@ use tera::{Tera, Context, Result};
 use serde_json::{Value, to_value};
 
 use std::fs::File;
+use std::io::Write;
 use std::{fs, io, path::Path};
 use std::io::Read;
 
+use std::env;
+
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        // let mut tera = compile_templates!("templates/*");
         let mut tera = compile_templates!("../../acados_template/c_templates_tera/*");
         tera.autoescape_on(vec![".swp"]);
         tera.register_filter("do_nothing", do_nothing_filter);
@@ -26,16 +28,34 @@ pub fn do_nothing_filter(value: Value, _: HashMap<String, Value>) -> Result<Valu
 }
 
 fn main() -> io::Result<()> {
+    // read command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    let template_file = &args[1];
+    let json_file     = &args[2];
+    let out_file      = &args[3];
+
+    println!("template file: {}", template_file);
+    println!("json file {}:"    , json_file);
+    println!("out file {}:"     , out_file);
+
     let mut file = File::open("../../acados_template/c_templates/acados_ocp_nlp.json")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    println!("{}", contents);
+    // println!("{}", contents);
+    
     // Parse the string of data into serde_json::Value.
     let v: Value = serde_json::from_str(&contents)?;
 
-    // match TEMPLATES.render("hello_world.in", &v) {
+
+    let args: Vec<String> = env::args().collect();
     match TEMPLATES.render("acados_solver.in.c", &v) {
-        Ok(s) => println!("{:?}", s),
+        Ok(s) => {
+            let string_list = vec!["Foo".to_string(),"Bar".to_string()];
+            let joined = string_list.join("-");
+            let mut f_out = File::create("acados_solver.c").expect("Unable to create file");
+            f_out.write_all(s.as_bytes())?;
+        },
         Err(e) => {
             println!("Error: {}", e);
             for e in e.iter().skip(1) {
