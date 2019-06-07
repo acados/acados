f9a2c90797cbbc109afaf7ce3a2402c1179fc6b7
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-06-07 11:46:51 +0200

    fix bug in bhgp dims getter

--- acados/ocp_nlp/ocp_nlp_constraints_bghp.c
+++ acados/ocp_nlp/ocp_nlp_constraints_bghp.c
@@ -266,17 +266,17 @@ static void ocp_nlp_constraints_bghp_get_nb(void *config_, void *dims_, int* val
 
 
 
-static void ocp_nlp_constraints_bgh_get_nbx(void *config_, void *dims_, int* value)
+static void ocp_nlp_constraints_bghp_get_nbx(void *config_, void *dims_, int* value)
 {
-    ocp_nlp_constraints_bgh_dims *dims = (ocp_nlp_constraints_bgh_dims *) dims_;
+    ocp_nlp_constraints_bghp_dims *dims = (ocp_nlp_constraints_bghp_dims *) dims_;
     *value = dims->nbx;
 }
 
 
 
-static void ocp_nlp_constraints_bgh_get_nbu(void *config_, void *dims_, int* value)
+static void ocp_nlp_constraints_bghp_get_nbu(void *config_, void *dims_, int* value)
 {
-    ocp_nlp_constraints_bgh_dims *dims = (ocp_nlp_constraints_bgh_dims *) dims_;
+    ocp_nlp_constraints_bghp_dims *dims = (ocp_nlp_constraints_bghp_dims *) dims_;
     *value = dims->nbu;
 }
 
