cce284a692fa103a866a8db23d5be4bb95b8ffe7
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-24 11:32:11 +0200

    acados mex clear all loaded mex files, #411

--- interfaces/acados_matlab/acados_ocp.m
+++ interfaces/acados_matlab/acados_ocp.m
@@ -21,7 +21,17 @@ classdef acados_ocp < handle
       clear ocp_create_ext_fun
       clear ocp_destroy
       clear ocp_destroy_ext_fun
-      
+      clear ocp_get
+      clear ocp_model_ocp_set_ext_fun_cost_0_ext_cost_jac_hes
+      clear ocp_model_ocp_set_ext_fun_cost_1_ext_cost_jac_hes
+      clear ocp_model_ocp_set_ext_fun_dyn_0_expl_ode_fun
+      clear ocp_model_ocp_set_ext_fun_dyn_0_expl_ode_hes
+      clear ocp_model_ocp_set_ext_fun_dyn_0_expl_vde_adj
+      clear ocp_model_ocp_set_ext_fun_dyn_0_expl_vde_for
+      clear ocp_precompute
+      clear ocp_set
+      clear ocp_solve
+
       % create build folder and add to path
       addpath('build');
       rmpath('build')
