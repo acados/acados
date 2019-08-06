1a26a17337f222c7c011b3559c40228d65b0a4eb
Jonathan Frey <jonathanpaulfrey@gmail.com>
2019-08-06 18:01:17 +0200

    matlab interface: bug fix - removed spaces

--- interfaces/acados_matlab/sim_generate_casadi_ext_fun.m
+++ interfaces/acados_matlab/sim_generate_casadi_ext_fun.m
@@ -7,28 +7,28 @@ if (strcmp(opts_struct.method, 'erk'))
 	% generate c for function and derivatives using casadi
 	generate_c_code_explicit_ode(model_struct, opts_struct);
 	% compile the code in a shared library
-	c_files{end+1} = [model_name, '_dyn_expl_ode_fun.c '];
-	c_files{end+1} = [model_name, '_dyn_expl_vde_for.c '];
-	c_files{end+1} = [model_name, '_dyn_expl_vde_adj.c '];
-	c_files{end+1} = [model_name, '_dyn_expl_ode_hes.c '];
+	c_files{end+1} = [model_name, '_dyn_expl_ode_fun.c'];
+	c_files{end+1} = [model_name, '_dyn_expl_vde_for.c'];
+	c_files{end+1} = [model_name, '_dyn_expl_vde_adj.c'];
+	c_files{end+1} = [model_name, '_dyn_expl_ode_hes.c'];
 elseif (strcmp(opts_struct.method, 'irk'))
 	% generate c for function and derivatives using casadi
 	generate_c_code_implicit_ode(model_struct, opts_struct);
 	% compile the code in a shared library
-	c_files{end+1} = [model_name, '_dyn_impl_ode_fun.c '];
-	c_files{end+1} = [model_name, '_dyn_impl_ode_fun_jac_x_xdot.c '];
-	c_files{end+1} = [model_name, '_dyn_impl_ode_fun_jac_x_xdot_u.c '];
-	c_files{end+1} = [model_name, '_dyn_impl_ode_jac_x_xdot_u.c '];
-	c_files{end+1} = [model_name, '_dyn_impl_ode_hess.c '];
+	c_files{end+1} = [model_name, '_dyn_impl_ode_fun.c'];
+	c_files{end+1} = [model_name, '_dyn_impl_ode_fun_jac_x_xdot.c'];
+	c_files{end+1} = [model_name, '_dyn_impl_ode_fun_jac_x_xdot_u.c'];
+	c_files{end+1} = [model_name, '_dyn_impl_ode_jac_x_xdot_u.c'];
+	c_files{end+1} = [model_name, '_dyn_impl_ode_hess.c'];
 elseif (strcmp(opts_struct.method, 'irk_gnsf'))
 	% generate c for function and derivatives using casadi
 	generate_c_code_gnsf(model_struct); %, opts_struct);
 	% compile the code in a shared library
-	c_files{end+1} = [model_name, '_dyn_gnsf_f_lo_fun_jac_x1k1uz.c '];
-	c_files{end+1} = [model_name, '_dyn_gnsf_get_matrices_fun.c '];
-	c_files{end+1} = [model_name, '_dyn_gnsf_phi_fun.c '];
-	c_files{end+1} = [model_name, '_dyn_gnsf_phi_fun_jac_y.c '];
-	c_files{end+1} = [model_name, '_dyn_gnsf_phi_jac_y_uhat.c '];
+	c_files{end+1} = [model_name, '_dyn_gnsf_f_lo_fun_jac_x1k1uz.c'];
+	c_files{end+1} = [model_name, '_dyn_gnsf_get_matrices_fun.c'];
+	c_files{end+1} = [model_name, '_dyn_gnsf_phi_fun.c'];
+	c_files{end+1} = [model_name, '_dyn_gnsf_phi_fun_jac_y.c'];
+	c_files{end+1} = [model_name, '_dyn_gnsf_phi_jac_y_uhat.c'];
 else
 	fprintf('\nsim_generate_casadi_ext_fun: method not supported: %s\n', opts_struct.method);
 	return;
