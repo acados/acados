0787224e118f762de866dca1719a3fba1139c83b
giaf <gianfri2008@hotmail.it>
2019-08-07 17:30:02 +0200

    fix example/c/Makefile

--- examples/c/no_interface_examples/nonlinear_chain_ocp_nlp_no_interface.c
+++ examples/c/no_interface_examples/nonlinear_chain_ocp_nlp_no_interface.c
@@ -1281,7 +1281,19 @@ int main() {
 	int dims_size = ocp_nlp_dims_calculate_size(config);
 	void *dims_mem = malloc(dims_size);
 	ocp_nlp_dims *dims = ocp_nlp_dims_assign(config, dims_mem);
-	ocp_nlp_dims_initialize(config, nx, nu, ny, nbx, nbu, ng, nh, nq, ns, nz, dims);
+//	ocp_nlp_dims_initialize(config, nx, nu, ny, nbx, nbu, ng, nh, nq, ns, nz, dims);
+	ocp_nlp_dims_set_opt_vars(config, dims, "nx", nx);
+	ocp_nlp_dims_set_opt_vars(config, dims, "nu", nu);
+	ocp_nlp_dims_set_opt_vars(config, dims, "nz", nz);
+	ocp_nlp_dims_set_opt_vars(config, dims, "ns", ns);
+	for(int ii=0; ii<=NN; ii++)
+		{
+		ocp_nlp_dims_set_constraints(config, dims, ii, "nbx", nbx);
+		ocp_nlp_dims_set_constraints(config, dims, ii, "nbu", nbu);
+		ocp_nlp_dims_set_constraints(config, dims, ii, "ng", ng);
+		ocp_nlp_dims_set_constraints(config, dims, ii, "nh", nh);
+		ocp_nlp_dims_set_cost(config, dims, ii, "ny", ny);
+		}
 
     /************************************************
     * dynamics
