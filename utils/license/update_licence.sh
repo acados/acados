abe8b3951175093c3bcfd077439ec4bd09a18c08
Tommaso Sartor <tmmsartor@gmail.com>
2019-08-16 16:04:35 +0200

    license: Fix update script

--- utils/license/update_licence.sh
+++ utils/license/update_licence.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
+
+LICENSE_TEXT_PATH="$(pwd)/utils/license/acados_license.txt"
+WRONG_FOLDER_MSG="To run this script 'cd' to acados repo main directory"
+
+[ ! -f $LICENSE_TEXT_PATH ] && echo $WRONG_FOLDER_MSG && exit
+
 SCRIPT_REPO_URL=https://gitlab.syscop.de/tmmsartor/licenseheaders.git
-LICENSE_TEXT_PATH="./acados_license.txt"
-SCRIPT_CMD="python licenseheaders/licenseheaders.py"
+SCRIPT_CMD="python licenseheaders/licenseheaders.py -D --dry"
 
 [ ! -d licenseheaders ] && git clone $SCRIPT_REPO_URL
-
 
 $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d acados/
 $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d cmake/
@@ -15,6 +19,8 @@ $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d examples/
 $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d test/
 $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d docs/
 
+mkdir mk
+
 mv CMakeLists.txt Makefile Makefile.rule Makefile.osqp mk
 
 $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d mk/
@@ -22,3 +28,5 @@ $SCRIPT_CMD -t $LICENSE_TEXT_PATH -d mk/
 pushd mk
 mv CMakeLists.txt Makefile Makefile.rule Makefile.osqp ..
 popd
+
+rm -r mk
