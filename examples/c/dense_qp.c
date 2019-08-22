b07f2201d4feb0eff9c0295aa395c3f1265a926c
giaf <gianfri2008@hotmail.it>
2019-08-22 16:23:16 +0200

    update hpipm

--- examples/c/dense_qp.c
+++ examples/c/dense_qp.c
@@ -103,7 +103,7 @@ int main() {
 
     dense_qp_in *qp_in = dense_qp_in_create(config, &dims);
 
-    d_cvt_colmaj_to_dense_qp(H, g, NULL, NULL, idxb, d_lb, d_ub, C, d_lg, d_ug, Zl, Zu, zl, zu, idxs, d_ls, d_us, qp_in);
+    d_dense_qp_set_all(H, g, NULL, NULL, idxb, d_lb, d_ub, C, d_lg, d_ug, Zl, Zu, zl, zu, idxs, d_ls, d_us, qp_in);
 
     print_dense_qp_in(qp_in);
 
