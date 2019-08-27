f53ee6490396c93ce496aee50d41cd637ecf9b5a
Jonathan Frey <jonathanpaulfrey@gmail.com>
2019-08-27 10:37:34 +0200

    gnsf transcription: remove old license

--- interfaces/acados_matlab/determine_input_nonlinearity_function.m
+++ interfaces/acados_matlab/determine_input_nonlinearity_function.m
@@ -30,26 +30,9 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.;
 %
+%   Author: Jonathan Frey: jonathanpaulfrey(at)gmail.com
 
 function [ gnsf ] = determine_input_nonlinearity_function( gnsf )
-%
-%   This file is part of acados.
-%
-%   acados is free software; you can redistribute it and/or
-%   modify it under the terms of the GNU Lesser General Public
-%   License as published by the Free Software Foundation; either
-%   version 3 of the License, or (at your option) any later version.
-%
-%   acados is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%   Lesser General Public License for more details.
-%
-%   You should have received a copy of the GNU Lesser General Public
-%   License along with acados; if not, write to the Free Software Foundation,
-%   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-%
-%   Author: Jonathan Frey: jonathanpaulfrey(at)gmail.com
 
 %% Description
 % this function takes a structure gnsf and updates the matrices L_x,
