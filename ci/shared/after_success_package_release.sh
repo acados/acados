f6fb649402303debc492864a418382d14ef40357
Andrea Zanelli <znllandrea@gmail.com>
2019-08-27 15:26:40 +0200

    fix typo

--- ci/shared/after_success_package_release.sh
+++ ci/shared/after_success_package_release.sh
@@ -36,7 +36,7 @@
 ACADOS_INSTALL_DIR="${ACADOS_INSTALL_DIR:-${HOME}/acados}";
 DEPLOY_FOLDER="${DEPLOY_FOLDER:-${HOME}/deploy}";
 DEPLOY_NAME="${DEPLOY_NAME:-default}";
-echo "in after_success_package_script"
+echo "in after_success_package_release.sh script"
 echo ${ACADOS_INSTALL_DIR}
 pushd "${ACADOS_INSTALL_DIR}";
 tar -zcf ${DEPLOY_FOLDER}/${DEPLOY_NAME}.tar.gz ./lib ./include;
