63b05dd03923c6c3c2a1e88dff8b745ff711eaac
Jonathan Frey <jonathanpaulfrey@gmail.com>
2019-10-14 21:32:44 +0200

    mex: fix set_up_acados_ocp_nlp_json

--- interfaces/acados_matlab_octave/ocp_generate_c_code.m
+++ interfaces/acados_matlab_octave/ocp_generate_c_code.m
@@ -5,19 +5,26 @@ function ocp_generate_c_code(obj)
         elseif (strcmp(obj.model_struct.dyn_type, 'implicit'))
             if (strcmp(obj.opts_struct.sim_method, 'irk'))
                 opts.generate_hess = 1;
-                acados_template_mex.generate_c_code_implicit_ode(obj.acados_ocp_nlp_json.model, opts);
+                acados_template_mex.generate_c_code_implicit_ode(...
+                    obj.acados_ocp_nlp_json.model, opts);
             end
         end
 
         % add checks for
         if ~strcmp( obj.model_struct.cost_type, 'linear_ls' ) || ...
-           ~strcmp( obj.model_struct.cost_type_e, 'linear_ls' )
-            error('mex templating does only support linear_ls cost for now. Got cost_type: %s, cost_type_e: %s.\nNotice that it might still be possible to solve the OCP from MATLAB.', obj.model_struct.cost_type, obj.model_struct.cost_type_e);
+            ~strcmp( obj.model_struct.cost_type_e, 'linear_ls' )
+            error(['mex templating does only support linear_ls cost for now.',...
+                'Got cost_type: %s, cost_type_e: %s.\nNotice that it might still',...
+                'be possible to solve the OCP from MATLAB.'], obj.model_struct.cost_type,...
+                obj.model_struct.cost_type_e);
             % TODO: add
             % nonlinear least-squares
             % external cost
         elseif ~strcmp( obj.opts_struct.nlp_solver_exact_hessian, 'false')
-            error('mex templating does only support nlp_solver_exact_hessian = "false", i.e. Gauss-Newton Hessian approximation for now. Got nlp_solver_exact_hessian = "%s"\n. Notice that it might still be possible to solve the OCP from MATLAB.', obj.opts_struct.nlp_solver_exact_hessian);
+            error(['mex templating does only support nlp_solver_exact_hessian = "false",',...
+                'i.e. Gauss-Newton Hessian approximation for now.\n',...
+                'Got nlp_solver_exact_hessian = "%s"\n. Notice that it might still be possible to solve the OCP from MATLAB.'],...
+                obj.opts_struct.nlp_solver_exact_hessian);
             % TODO: add exact Hessian
         elseif strcmp( obj.model_struct.dyn_type, 'discrete')
             error('mex templating does only support discrete dynamics for now. Notice that it might still be possible to solve the OCP from MATLAB.');
@@ -26,39 +33,39 @@ function ocp_generate_c_code(obj)
             error('mex templating does not support irk_gnsf integrator yet. Notice that it might still be possible to solve the OCP from MATLAB.');
             % TODO: implement
         end
-        
+
         % set include and lib path
         acados_folder = getenv('ACADOS_INSTALL_DIR');
         obj.acados_ocp_nlp_json.acados_include_path = [acados_folder, '/include'];
         obj.acados_ocp_nlp_json.acados_lib_path = [acados_folder, '/lib'];
         % strip non-numerical data
-        
+
         % model
         model.name = obj.acados_ocp_nlp_json.model.name;
-        
+
         obj.acados_ocp_nlp_json.model = [];
         obj.acados_ocp_nlp_json.model = model;
-        
+
         con_h.name = obj.acados_ocp_nlp_json.con_h.name;
-        
+
         obj.acados_ocp_nlp_json.con_h = [];
         obj.acados_ocp_nlp_json.con_h = con_h;
-        
+
         con_h_e.name = obj.acados_ocp_nlp_json.con_h_e.name;
-        
+
         obj.acados_ocp_nlp_json.con_h_e = [];
         obj.acados_ocp_nlp_json.con_h_e = con_h_e;
-        
+
         con_p.name = obj.acados_ocp_nlp_json.con_p.name;
-        
+
         obj.acados_ocp_nlp_json.con_p = [];
         obj.acados_ocp_nlp_json.con_p = con_p;
-        
+
         con_p_e.name = obj.acados_ocp_nlp_json.con_p_e.name;
-        
+
         obj.acados_ocp_nlp_json.con_p_e = [];
         obj.acados_ocp_nlp_json.con_p_e = con_p_e;
-        
+
         % post process numerical data (mostly cast scalars to 1-dimensional cells)
         constr = obj.acados_ocp_nlp_json.constraints;
         % props = properties(constr);
@@ -73,7 +80,7 @@ function ocp_generate_c_code(obj)
             end
         end
         obj.acados_ocp_nlp_json.constraints = constr;
-        
+
         cost = obj.acados_ocp_nlp_json.cost;
         % props = properties(cost);
         props = fieldnames(cost);
@@ -87,7 +94,7 @@ function ocp_generate_c_code(obj)
             end
         end
         obj.acados_ocp_nlp_json.cost = cost;
-        
+
         % load JSON layout
         acados_folder = getenv('ACADOS_INSTALL_DIR');
 
@@ -109,14 +116,15 @@ function ocp_generate_c_code(obj)
                 try
                     constr.(fields{i}) = reshape(constr.(fields{i}), this_dims);
                 catch e
-                    keyboard
                     error(['error while reshaping constr.' fields{i} ...
-                        ' to dimension ' num2str(this_dims), '. ' e.message]);
+                        ' to dimension ' num2str(this_dims), ', got ',...
+                        num2str( size(constr.(fields{i}) )) , ' .\n ',...
+                        e.message ]);
                 end
             end
         end
         obj.acados_ocp_nlp_json.constraints = constr;
-        
+
         % reshape cost
         cost = obj.acados_ocp_nlp_json.cost;
         cost_l = acados_layout.cost;
@@ -128,7 +136,14 @@ function ocp_generate_c_code(obj)
                 else
                     this_dims = [dims.(cost_l.(fields{i}){2}{1}), dims.(cost_l.(fields{i}){2}{2})];
                 end
+                try
                 cost.(fields{i}) = reshape(cost.(fields{i}), this_dims);
+                catch e
+                        error(['error while reshaping cost.' fields{i} ...
+                            ' to dimension ' num2str(this_dims), ', got ',...
+                            num2str( size(cost.(fields{i}) )) , ' .\n ',...
+                            e.message ]);
+                end
                 % convert 1-dimensional arrays to cells
                 if length(cost_l.(fields{i}){2}) == 2 && (this_dims(1) == 1 || this_dims(2) == 1)
                     field_as_cell = {};
@@ -140,7 +155,7 @@ function ocp_generate_c_code(obj)
             end
         end
         obj.acados_ocp_nlp_json.cost = cost;
-        
+
         % dump JSON file
         json_string = jsonencode(obj.acados_ocp_nlp_json);
         fid = fopen('acados_ocp_nlp.json', 'w');
--- interfaces/acados_matlab_octave/set_up_acados_ocp_nlp_json.m
+++ interfaces/acados_matlab_octave/set_up_acados_ocp_nlp_json.m
@@ -32,7 +32,8 @@
 %
 
 function ocp_json = set_up_acados_ocp_nlp_json(obj)
-keyboard
+
+    model = obj.model_struct;
     % create
     ocp_json = acados_template_mex.acados_ocp_nlp_json();
     % TODO(andrea): this is temporary. later on the solver_config
@@ -44,180 +45,252 @@ keyboard
     ocp_json.solver_config.nlp_solver_type = upper(obj.opts_struct.nlp_solver);
     ocp_json.dims.N = upper(obj.opts_struct.param_scheme_N);
 
-    ocp_json.solver_config.tf = obj.model_struct.T;
-    ocp_json.model.name = obj.model_struct.name;
+    ocp_json.solver_config.tf = model.T;
+    ocp_json.model.name = model.name;
 
     %% dims
+    nx = model.dim_nx;
     % path
-    ocp_json.dims.nx = obj.model_struct.dim_nx;
-    ocp_json.dims.nu = obj.model_struct.dim_nu;
-    ocp_json.dims.nz = obj.model_struct.dim_nz;
+    ocp_json.dims.nx = model.dim_nx;
+    ocp_json.dims.nu = model.dim_nu;
+    ocp_json.dims.nz = model.dim_nz;
     % missing in MEX (?!)
-    % ocp_json.dims.np = obj.model_struct.dim_np;
-    ocp_json.dims.ny = obj.model_struct.dim_ny;
-    ocp_json.dims.nbx = obj.model_struct.dim_nbx;
-    ocp_json.dims.nbu = obj.model_struct.dim_nbu;
-    ocp_json.dims.ng = obj.model_struct.dim_ng;
-    ocp_json.dims.nh = obj.model_struct.dim_nh;
-    ocp_json.dims.ns = obj.model_struct.dim_ns;
-    ocp_json.dims.nsbx = obj.model_struct.dim_nsbx;
-    ocp_json.dims.nsbu = obj.model_struct.dim_nsbu;
-
+    % ocp_json.dims.np = model.dim_np;
+    ocp_json.dims.ny = model.dim_ny;
+    ocp_json.dims.nbx = model.dim_nbx;
+    ocp_json.dims.nbu = model.dim_nbu;
+    ocp_json.dims.ng = model.dim_ng;
+    ocp_json.dims.nh = model.dim_nh;
+    if isfield(model, 'dim_ns')
+        ocp_json.dims.ns = model.dim_ns;
+    end
+    if isfield(model, 'dim_nsbx')
+        ocp_json.dims.nsbx = model.dim_nsbx;
+    end
+    if isfield(model, 'dim_nsbx')
+        ocp_json.dims.nsbu = model.dim_nsbu;
+    end
+    
     % missing in template
-    % ocp_json.dims.nsg = obj.model_struct.nsg;
+    % ocp_json.dims.nsg = model.nsg;
 
     % missing in MEX
-    % ocp_json.dims.npd = obj.model_struct.npd;
-    % ocp_json.dims.npd_e = obj.model_struct.npd_e;
+    % ocp_json.dims.npd = model.npd;
+    % ocp_json.dims.npd_e = model.npd_e;
 
-    ocp_json.dims.nsh = obj.model_struct.nsh;
+    if isfield(model, 'dim_nsh')
+        ocp_json.dims.nsh = model.dim_nsh;
+    end
 
     % terminal
-    ocp_json.dims.ng_e = obj.model_struct.ng_e;
-    ocp_json.dims.ny_e = obj.model_struct.ny_e;
-    ocp_json.dims.nh_e = obj.model_struct.nh_e;
-    ocp_json.dims.ns_e = obj.model_struct.ns_e;
-    ocp_json.dims.nsh_e = obj.model_struct.nsh_e;
+    if isfield(model, 'dim_ng_e')
+        ocp_json.dims.ng_e = model.dim_ng_e;
+    end
+    if isfield(model, 'dim_ny_e')
+        ocp_json.dims.ny_e = model.dim_ny_e;
+    end
+    if isfield(model, 'dim_nh_e')
+        ocp_json.dims.nh_e = model.dim_nh_e;
+    end
+    if isfield(model, 'dim_ns_e')
+        ocp_json.dims.ns_e = model.dim_ns_e;
+    end
+    if isfield(model, 'dim_nsh_e')
+        ocp_json.dims.nsh_e = model.dim_nsh_e;
+    end
     % missing in MEX
-    % ocp_json.dims.nbx_e = obj.model_struct.nbx_e;
-    % ocp_json.dims.ns_e = obj.model_struct.ns_e;
+    % ocp_json.dims.nbx_e = model.dim_nbx_e;
+    % ocp_json.dims.ns_e = model.dim_ns_e;
 
     %% types
-    ocp_json.cost.cost_type = upper(obj.model_struct.cost_type);
-    ocp_json.cost.cost_type_e = upper(obj.model_struct.cost_type_e);
-    ocp_json.constraints.constr_type = upper(obj.model_struct.constr_type);
-    ocp_json.constraints.constr_type_e = upper(obj.model_struct.constr_type_e);
+    ocp_json.cost.cost_type = upper(model.cost_type);
+    ocp_json.cost.cost_type_e = upper(model.cost_type_e);
+    ocp_json.constraints.constr_type = upper(model.constr_type);
+    ocp_json.constraints.constr_type_e = upper(model.constr_type_e);
 
     %% constraints
     % path
-    if isfield(obj.model_struct, 'constr_x0')
-        ocp_json.constraints.x0 = obj.model_struct.constr_x0;
+    if isfield(model, 'constr_x0')
+        ocp_json.constraints.x0 = model.constr_x0;
     else
         warning('constr_x0 not defined for ocp json.');
+        warning('using zeros(nx,1) as initial state value.');
+        ocp_json.constraints.x0 = zeros(nx,1);
     end
 
     if ocp_json.dims.nbx > 0
-        ocp_json.constraints.idxbx = J_to_idx( obj.model_struct.constr_Jbx );
-        ocp_json.constraints.lbx = obj.model_struct.constr_lbx;
-        ocp_json.constraints.ubx = obj.model_struct.constr_ubx;
+        ocp_json.constraints.idxbx = J_to_idx( model.constr_Jbx );
+        ocp_json.constraints.lbx = model.constr_lbx;
+        ocp_json.constraints.ubx = model.constr_ubx;
     end
 
     if ocp_json.dims.nbu > 0
-        ocp_json.constraints.idxbu = J_to_idx( obj.model_struct.constr_Jbu );
-        ocp_json.constraints.lbu = obj.model_struct.constr_lbu;
-        ocp_json.constraints.ubu = obj.model_struct.constr_ubu;
+        ocp_json.constraints.idxbu = J_to_idx( model.constr_Jbu );
+        ocp_json.constraints.lbu = model.constr_lbu;
+        ocp_json.constraints.ubu = model.constr_ubu;
     end
 
     if ocp_json.dims.ng > 0
-        ocp_json.constraints.C = obj.model_struct.constr_C;
-        ocp_json.constraints.D = obj.model_struct.constr_D;
-        ocp_json.constraints.lg = obj.model_struct.constr_lg;
-        ocp_json.constraints.ug = obj.model_struct.constr_ug;
+        ocp_json.constraints.C = model.constr_C;
+        ocp_json.constraints.D = model.constr_D;
+        ocp_json.constraints.lg = model.constr_lg;
+        ocp_json.constraints.ug = model.constr_ug;
     end
 
     if ocp_json.dims.nh > 0
         ocp_json.con_h.name = 'expr_h';
-        ocp_json.con_h.expr = obj.model_struct.constr_expr_h;
-        ocp_json.constraints.lh = value;
-        ocp_json.constraints.uh = value;
+        ocp_json.con_h.expr = model.constr_expr_h;
+        keyboard
+        ocp_json.constraints.lh = model.constr_lh;
+        ocp_json.constraints.uh = model.constr_uh;
         % TODO(oj): can we get rid of the following?
-        if isfield(obj.model_struct, 'sym_x')
-            ocp_json.con_h.x = obj.model_struct.sym_x;
+        if isfield(model, 'sym_x')
+            ocp_json.con_h.x = model.sym_x;
         end
-        if isfield(obj.model_struct, 'sym_u')
-            ocp_json.con_h.u = obj.model_struct.sym_u;
+        if isfield(model, 'sym_u')
+            ocp_json.con_h.u = model.sym_u;
         end
-        if isfield(obj.model_struct, 'sym_z')
-            ocp_json.con_h.z = obj.model_struct.sym_z;
+        if isfield(model, 'sym_z')
+            ocp_json.con_h.z = model.sym_z;
         end
     end
 
-    if ocp_json.nsbx > 0
-        ocp_json.constraints.idxsbx = J_to_idx_slack(obj.model_struct.Jsbx);
-        ocp_json.constraints.lsbx = obj.model_struct.lsbx;
-        ocp_json.constraints.usbx = obj.model_struct.usbx;
+    if ocp_json.dims.nsbx > 0
+        ocp_json.constraints.idxsbx = J_to_idx_slack(model.Jsbx);
+        ocp_json.constraints.lsbx = model.lsbx;
+        ocp_json.constraints.usbx = model.usbx;
     end
 
-    if ocp_json.nsbu > 0
-        ocp_json.constraints.idxsbu = J_to_idx_slack(obj.model_struct.Jsbu);
-        ocp_json.constraints.lsbu = obj.model_struct.lsbu;
-        ocp_json.constraints.usbu = obj.model_struct.usbu;
+    if ocp_json.dims.nsbu > 0
+        ocp_json.constraints.idxsbu = J_to_idx_slack(model.Jsbu);
+        ocp_json.constraints.lsbu = model.lsbu;
+        ocp_json.constraints.usbu = model.usbu;
     end
 
-    if ocp_json.nsbh > 0
-        ocp_json.constraints.idxsh = J_to_idx_slack(obj.model_struct.Jsh);
+    if ocp_json.dims.nsh > 0
+        ocp_json.constraints.idxsh = J_to_idx_slack(model.Jsh);
     end
 
-    if ocp_json.nsg > 0
-        error('Jsg not implmented in code-gen backend');
+    if isfield(model, 'dim_nsg') && model.dim_nsg > 0
+        error('dim_nsg > 0 not implmented in code-gen backend');
+        % TODO set Jsg
     end
 
 
     % terminal
     if ocp_json.dims.ng_e > 0
-        ocp_json.constraints.C_e = obj.model_struct.constr_C_e;
-        ocp_json.constraints.lg_e = obj.model_struct.constr_lg_e;
-        ocp_json.constraints.ug_e = obj.model_struct.constr_ug_e;
+        ocp_json.constraints.C_e = model.constr_C_e;
+        ocp_json.constraints.lg_e = model.constr_lg_e;
+        ocp_json.constraints.ug_e = model.constr_ug_e;
     end
 
     if ocp_json.dims.nh_e > 0    
         ocp_json.con_h_e.name = 'expr_h_e';
-        ocp_json.con_h_e.expr = obj.model_struct.constr_expr_h_e;
-        ocp_json.constraints.lh_e = obj.model_struct.constr_lh_e;
-        ocp_json.constraints.uh_e = obj.model_struct.constr_uh_e;
+        ocp_json.con_h_e.expr = model.constr_expr_h_e;
+        ocp_json.constraints.lh_e = model.constr_lh_e;
+        ocp_json.constraints.uh_e = model.constr_uh_e;
         % TODO(oj): can we get rid of the following?
-        if isfield(obj.model_struct, 'sym_x')
-            ocp_json.con_h_e.x = obj.model_struct.sym_x;
+        if isfield(model, 'sym_x')
+            ocp_json.con_h_e.x = model.sym_x;
         end
-        if isfield(obj.model_struct, 'sym_u')
-            ocp_json.con_h_e.u = obj.model_struct.sym_u;
+        if isfield(model, 'sym_u')
+            ocp_json.con_h_e.u = model.sym_u;
         end
-        if isfield(obj.model_struct, 'sym_z')
-            ocp_json.con_h_e.z = obj.model_struct.sym_z;
+        if isfield(model, 'sym_z')
+            ocp_json.con_h_e.z = model.sym_z;
         end
     end
 
-    if ocp_json.nsg_e > 0
-        error('Jsg_e not implmented in code-gen backend')
+    if isfield(model, 'dim_nsg_e') && model.dim_nsg_e > 0
+        error('dim_nsg_e > 0 not implmented in code-gen backend');
+        % TODO set Jsg_e
     end
 
-    if ocp_json.nsh_e > 0
-        ocp_json.constraints.idxsh_e = J_to_idx_slack(obj.model_struct.Jsh_e);
+
+    if ocp_json.dims.nsh_e > 0
+        ocp_json.constraints.idxsh_e = J_to_idx_slack(model.Jsh_e);
     end
 
     %% Cost
-    ocp_json.cost.Vu = obj.model_struct.cost_Vu;
-    ocp_json.cost.Vx = obj.model_struct.cost_Vx;
-    ocp_json.cost.Vx_e = obj.model_struct.cost_Vx_e;
-    ocp_json.cost.W = obj.model_struct.cost_W;
-    ocp_json.cost.Vz = obj.model_struct.cost_Vz;
-    ocp_json.cost.W_e = obj.model_struct.cost_W_e;
-    ocp_json.cost.yref = obj.model_struct.cost_y_ref;
-    ocp_json.cost.yref_e = obj.model_struct.cost_y_ref_e;
-    ocp_json.cost.Zl = obj.model_struct.cost_Zl;
-    ocp_json.cost.Zl_e = obj.model_struct.cost_Zl_e;
-    ocp_json.cost.Zu = obj.model_struct.cost_Zu;
-    ocp_json.cost.Zu_e = obj.model_struct.cost_Zu_e;
-    ocp_json.cost.zl = obj.model_struct.cost_zl;
-    ocp_json.cost.zl_e = obj.model_struct.cost_zl_e;
-    ocp_json.cost.zu = obj.model_struct.cost_zu;
-    ocp_json.cost.zu_e = obj.model_struct.cost_Zu_e;
-
-    ocp_json.model.x = obj.model_struct.sym_x;
-    ocp_json.model.f_impl_expr = value;
-    ocp_json.model.f_expl_expr = value;
-
-    if isfield(obj.model_struct, 'sym_u')
-        ocp_json.model.u = obj.model_struct.sym_u;
-    end
-    if isfield(obj.model_struct, 'sym_z')
-        ocp_json.model.z = obj.model_struct.sym_z;
-    end
-    if isfield(obj.model_struct, 'sym_xdot')
-        ocp_json.model.xdot = obj.model_struct.sym_xdot;
+    if strcmp(model.cost_type, 'linear_ls')
+        ocp_json.cost.Vu = model.cost_Vu;
+        ocp_json.cost.Vx = model.cost_Vx;
+        if isfield(model, 'cost_Vz')
+            ocp_json.cost.Vz = model.cost_Vz;
+        end
     end
 
-    keyboard
+    if strcmp(model.cost_type, 'nonlinear_ls') || strcmp(model.cost_type, 'linear_ls')
+        ocp_json.cost.W = model.cost_W;
+        if isfield(model, 'cost_y_ref')
+            ocp_json.cost.yref = model.cost_y_ref;
+        else
+            warning('cost_y_ref not defined for ocp json.');
+            warning('using zeros(ny,1) as initial state value.');
+            ocp_json.cost.yref = zeros(model.dim_ny,1);
+        end
+    end
+
+    if strcmp(model.cost_type_e, 'linear_ls')
+        ocp_json.cost.Vx_e = model.cost_Vx_e;
+    end
+
+    if strcmp(model.cost_type, 'nonlinear_ls') || strcmp(model.cost_type, 'linear_ls')
+        ocp_json.cost.W_e = model.cost_W_e;
+        if isfield(model, 'cost_y_ref')
+            ocp_json.cost.yref_e = model.cost_y_ref_e;
+        else
+            warning('cost_y_ref_e not defined for ocp json.');
+            warning('using zeros(ny_e,1) as initial state value.');
+            ocp_json.cost.yref_e = zeros(model.dim_ny_e,1);
+        end
+    end
+
+    if isfield(model, 'cost_Zl')
+        ocp_json.cost.Zl = model.cost_Zl;
+    end
+    if isfield(model, 'cost_Zu')
+        ocp_json.cost.Zu = model.cost_Zu;
+    end
+    if isfield(model, 'cost_zl')
+        ocp_json.cost.zl = model.cost_zl;
+    end
+    if isfield(model, 'cost_zu')
+        ocp_json.cost.zu = model.cost_zu;
+    end
+
+
+    if isfield(model, 'cost_Zl_e')
+        ocp_json.cost.Zl_e = model.cost_Zl_e;
+    end
+    if isfield(model, 'cost_Zu_e')
+        ocp_json.cost.Zu_e = model.cost_Zu_e;
+    end
+    if isfield(model, 'cost_zl_e')
+        ocp_json.cost.zl_e = model.cost_zl_e;
+    end
+    if isfield(model, 'cost_zu_e')
+        ocp_json.cost.zu_e = model.cost_zu_e;
+    end
+
+    if strcmp(obj.opts_struct.sim_method, 'erk') || strcmp(obj.opts_struct.sim_method, 'irk')
+        ocp_json.model.f_expl_expr = model.dyn_expr_f;
+    else
+        error(['integrator ', obj.opts_struct.sim_method, ' not support for templating backend.'])
+    end
+    %  TODO(oj): add gnsf support;
+
+    ocp_json.model.x = model.sym_x;
+    if isfield(model, 'sym_u')
+        ocp_json.model.u = model.sym_u;
+    end
+    if isfield(model, 'sym_z')
+        ocp_json.model.z = model.sym_z;
+    end
+    if isfield(model, 'sym_xdot')
+        ocp_json.model.xdot = model.sym_xdot;
+    end
+    ocp_json
 end
 
 
