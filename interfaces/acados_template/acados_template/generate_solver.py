cd52cf35fc8d093ba91d1bbbb3a587e6e391e41b
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2019-10-23 17:26:52 +0200

    fix soft constraints example

--- interfaces/acados_template/acados_template/generate_solver.py
+++ interfaces/acados_template/acados_template/generate_solver.py
@@ -35,6 +35,7 @@ from jinja2 import Environment, FileSystemLoader
 from .generate_c_code_explicit_ode import *
 from .generate_c_code_implicit_ode import *
 from .generate_c_code_constraint import *
+from .generate_c_code_constraint_e import *
 from .generate_c_code_nls_cost import *
 from .generate_c_code_nls_cost_e import *
 from .acados_ocp_nlp import *
