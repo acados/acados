81c805a3dd2d5109b1cae29cfc33930a99d9714b
Jonathan Frey <jonathanpaulfrey@gmail.com>
2019-12-13 15:43:11 +0100

    mex templating: simplify acados_dae

--- interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/acados_dae.m
+++ interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/acados_dae.m
@@ -44,15 +44,14 @@ classdef acados_dae < handle
     end
     methods
         function obj = acados_dae()
-        import casadi.*
-        obj.f_impl_expr = [];
-        obj.f_expl_expr = [];
-        obj.x = [];
-        obj.xdot = SX.sym('xdot', 0, 0);
-        obj.u = SX.sym('u', 0, 0);
-        obj.z = SX.sym('z', 0, 0);
-        obj.name = [];
-        obj.p = SX.sym('p', 0, 0);
+            obj.f_impl_expr = [];
+            obj.f_expl_expr = [];
+            obj.x = [];
+            obj.xdot = [];
+            obj.u = [];
+            obj.z = [];
+            obj.name = [];
+            obj.p = [];
         end
     end
 end
