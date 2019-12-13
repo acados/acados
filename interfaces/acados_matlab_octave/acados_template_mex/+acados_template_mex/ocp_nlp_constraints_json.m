615af1563355463f9fe8bf720026c6af89d4f193
Jonathan Frey <jonathanpaulfrey@gmail.com>
2019-12-13 12:23:18 +0100

    mex: ocp_nlp_constraints_json, fix descriptions on ls*, us*, rm trailing white spaces

--- interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/ocp_nlp_constraints_json.m
+++ interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/ocp_nlp_constraints_json.m
@@ -38,81 +38,81 @@ classdef ocp_nlp_constraints_json < handle
         % bounds on x and u
         lbx     % lower bounds on x
         lbu     % lower bounds on u
-        idxbx   % indexes of bounds on x 
-        ubx     % upper bounds on x 
-        ubu     % upper bounds on u 
+        idxbx   % indexes of bounds on x
+        ubx     % upper bounds on x
+        ubu     % upper bounds on u
         idxbu   % indexes of bounds on u
         % bounds on x at t=T
-        lbx_e    % lower bounds on x at t=T 
-        ubx_e    % upper bounds on x at t=T 
-        idxbx_e  % indexes for bounds on x at t=T 
-        % soft bounds on x and u
-        lsbx    % soft lower bounds on x
-        lsbu    % soft lower bounds on u
-        usbx    % soft upper bounds on x 
-        usbu    % soft upper bounds on u 
-        idxsbx  % indexes of soft bounds on x 
+        lbx_e    % lower bounds on x at t=T
+        ubx_e    % upper bounds on x at t=T
+        idxbx_e  % indexes for bounds on x at t=T
+        % bounds on slacks corresponding to softened bounds on x and u
+        idxsbx  % indexes of soft bounds on x
         idxsbu  % indexes of soft bounds on u
-        % soft bounds on x and u at t=T
-        lsbx_e   % soft lower bounds on x at t=T
-        usbx_e   % soft upper bounds on x at t=T
-        idxsbx_e % indexes of soft bounds on x at t=T 
-        % soft bounds on nonlinear constraints 
-        lsh     % soft lower bounds for nonlinear constraints 
-        ush     % soft upper bounds for nonlinear constraints 
-        idxsh   % indexes of soft nonlinear constraints at t=T 
+        lsbx    % lower bounds on slacks corresponding to soft lower bounds on x
+        lsbu    % lower bounds on slacks corresponding to soft lower bounds on u
+        usbx    % lower bounds on slacks corresponding to soft upper bounds on x
+        usbu    % lower bounds on slacks corresponding to soft upper bounds on u
+        % bounds on slacks corresponding to softened bounds on t=T
+        idxsbx_e % indexes of soft bounds on x at t=T
+        lsbx_e   % lower bounds on slacks corresponding to soft lower bounds on x at t=T
+        usbx_e   % lower bounds on slacks corresponding to soft upper bounds on x at t=T
+        % soft bounds on nonlinear constraints
+        lsh     % lower bounds on slacks corresponding to soft lower bounds for nonlinear constraints
+        ush     % lower bounds on slacks corresponding to soft upper bounds for nonlinear constraints
+        idxsh   % indexes of soft nonlinear constraints
         % soft bounds on nonlinear constraints at t=T
-        lsh_e    % soft lower bounds for nonlinear constraints 
-        ush_e    % soft upper bounds for nonlinear constraints 
-        idxsh_e  % indexes of soft nonlinear constraints at t=T 
-        % polytopic constraints 
+        lsh_e    % lower bounds on slacks corresponding to soft lower bounds for nonlinear constraints
+        ush_e    % lower bounds on slacks corresponding to soft upper bounds for nonlinear constraints
+        idxsh_e  % indexes of soft nonlinear constraints at t=T
+        % polytopic constraints
         D       % D matrix in lg <= D * u + C * x <= ug
         C       % C matrix in lg <= D * u + C * x <= ug
-        lg      % lower bound for general inequalities 
-        ug      % upper bound for general inequalities 
-        % polytopic constraints at t=T 
-        C_e      % C matrix at t=T 
-        lg_e     % lower bound on general inequalities at t=T 
-        ug_e     % upper bound on general inequalities at t=T 
+        lg      % lower bound for general inequalities
+        ug      % upper bound for general inequalities
+        % polytopic constraints at t=T
+        C_e      % C matrix at t=T
+        lg_e     % lower bound on general inequalities at t=T
+        ug_e     % upper bound on general inequalities at t=T
         % nonlinear constraints
-        lh      % lower bound for nonlinear inequalities 
-        uh      % upper bound for nonlinear inequalities 
+        lh      % lower bound for nonlinear inequalities
+        uh      % upper bound for nonlinear inequalities
         % nonlinear constraints at t=T
-        lh_e     % lower bound on nonlinear inequalities at t=T 
-        uh_e     % upper bound on nonlinear inequalities at t=T 
-        p       % nominal value of the parameter 
-        x0      % initial state 
+        lh_e     % lower bound on nonlinear inequalities at t=T
+        uh_e     % upper bound on nonlinear inequalities at t=T
+        p       % nominal value of the parameter
+        x0      % initial state
         % convex over nonlinear constraint (BGP) to work with json
         % TODO: implement in MEX..
         lphi   % lower bound on convex over nonlinear constraint
         uphi   % upper bound on convex over nonlinear constraint
         lphi_e   % lower bound on convex over nonlinear constraint at t=T
         uphi_e   % upper bound on convex over nonlinear constraint at t=T
-        lsphi   % lower bound on convex over nonlinear constraint
-        usphi   % upper bound on convex over nonlinear constraint
-        lsphi_e   % lower bound on convex over nonlinear constraint at t=T
-        usphi_e   % upper bound on convex over nonlinear constraint at t=T
+        lsphi   % lower bounds on slacks corresponding to lower bound on convex over nonlinear constraint
+        usphi   % lower bounds on slacks corresponding to upper bound on convex over nonlinear constraint
+        lsphi_e   % lower bounds on slacks corresponding to lower bound on convex over nonlinear constraint at t=T
+        usphi_e   % lower bounds on slacks corresponding to upper bound on convex over nonlinear constraint at t=T
         idxsphi % indexes of soft convex over nonlinear constraints
         idxsphi_e % indexes of soft convex over nonlinear constraints at t=T
 
     end
     methods
         function obj = ocp_nlp_constraints_json()
-            obj.constr_type     = 'BGH';  
-            obj.constr_type_e   = 'BGH';  
-            obj.lbx             = [];  
+            obj.constr_type     = 'BGH';
+            obj.constr_type_e   = 'BGH';
+            obj.lbx             = [];
             obj.lbu             = [];
             obj.idxbx           = [];
             obj.ubx             = [];
             obj.ubu             = [];
             obj.idxbu           = [];
-            obj.lsbx            = [];  
+            obj.lsbx            = [];
             obj.lsbu            = [];
             obj.idxsbx          = [];
             obj.usbx            = [];
             obj.usbu            = [];
             obj.idxsbu          = [];
-            obj.lsbx_e          = [];  
+            obj.lsbx_e          = [];
             obj.idxsbx_e        = [];
             obj.lg              = [];
             obj.ug              = [];
