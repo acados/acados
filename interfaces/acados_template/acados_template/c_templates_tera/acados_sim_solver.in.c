94c13aad53f192a7be37193d93ec1be5276f04f7
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-01-08 15:54:16 +0100

    templates: integrator use double num_steps to have model mismatch

--- interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.c
+++ interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.c
@@ -170,7 +170,7 @@ int {{ model.name }}_acados_sim_create() {
     {{ model.name }}_sim_opts = sim_opts_create({{ model.name }}_sim_config, {{ model.name }}_sim_dims);
 
     {{ model.name }}_sim_opts->ns = {{ solver_options.sim_method_num_stages }}; // number of stages in rk integrator
-    {{ model.name }}_sim_opts->num_steps = {{ solver_options.sim_method_num_steps }}; // number of integration steps
+    {{ model.name }}_sim_opts->num_steps = 2 * {{ solver_options.sim_method_num_steps }}; // number of integration steps
     {{ model.name }}_sim_opts->sens_adj = false;
     {{ model.name }}_sim_opts->sens_forw = true;
     {% if solver_options.integrator_type == "IRK" %}
