675b3ede79dd01c86bb516ee7d3ed03c962d291e
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-01-15 15:26:55 +0100

    mex: move gnsf related files into separate folder

--- interfaces/acados_matlab_octave/detect_gnsf_structure.m
+++ interfaces/acados_matlab_octave/detect_gnsf_structure.m
@@ -60,6 +60,8 @@ function model = detect_gnsf_structure(model, transcribe_opts)
 %       holds. If not, the method will try to reformulate the gnsf model
 %       with a different model, such that the assumption holds.
 
+acados_root_dir = getenv('ACADOS_INSTALL_DIR');
+addpath(fullfile(acados_root_dir, 'interfaces', 'acados_matlab_octave', 'gnsf'));
 
 %% load transcribe_opts
 if ~exist('transcribe_opts', 'var')
