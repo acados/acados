bf3df0497744c15fee17cfce792edaaf7dd764bd
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-01-23 15:57:09 +0100

    python: remove generate_c_code_constraint_e

--- interfaces/acados_template/acados_template/__init__.py
+++ interfaces/acados_template/acados_template/__init__.py
@@ -35,7 +35,6 @@ from .casadi_functions import *
 from .generate_c_code_explicit_ode import *
 from .generate_c_code_implicit_ode import *
 from .generate_c_code_constraint import *
-from .generate_c_code_constraint_e import *
 from .generate_c_code_nls_cost import *
 from .generate_c_code_nls_cost_e import *
 from .acados_ocp_nlp import *
