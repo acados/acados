26e1d8ffb8ffaf5cabd5b35ce1aa97a4e158d9d4
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-02-22 13:24:37 +0100

    template: fix model.h for integrator

--- interfaces/acados_template/acados_template/c_templates_tera/model.in.h
+++ interfaces/acados_template/acados_template/c_templates_tera/model.in.h
@@ -38,6 +38,12 @@
 extern "C" {
 #endif
 
+{%- if solver_options.hessian_approx %}
+	{%- set hessian_approx = solver_options.hessian_approx %}
+{%- else %}
+	{%- set hessian_approx = "GAUSS_NEWTON" %}
+{%- endif %}
+
 {% if solver_options.integrator_type == "IRK" %}
 // implicit ODE
 int {{ model.name }}_impl_dae_fun(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
@@ -71,7 +77,7 @@ int {{ model.name }}_impl_dae_jac_x_xdot_u_z_n_out();
 // int {{ model.name }}_impl_dae_fun_jac_x_xdot_u_n_in();
 // int {{ model.name }}_impl_dae_fun_jac_x_xdot_u_n_out();
 
-{%- if solver_options.hessian_approx == "EXACT" %}
+{%- if hessian_approx == "EXACT" %}
 int {{ model.name }}_impl_dae_hess(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
 int {{ model.name }}_impl_dae_hess_work(int *, int *, int *, int *);
 const int *{{ model.name }}_impl_dae_hess_sparsity_in(int);
@@ -149,7 +155,7 @@ const int *{{ model.name }}_expl_vde_adj_sparsity_out(int);
 int {{ model.name }}_expl_vde_adj_n_in();
 int {{ model.name }}_expl_vde_adj_n_out();
 
-{%- if solver_options.hessian_approx == "EXACT" %}
+{%- if hessian_approx == "EXACT" %}
 int {{ model.name }}_expl_ode_hess(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
 int {{ model.name }}_expl_ode_hess_work(int *, int *, int *, int *);
 const int *{{ model.name }}_expl_ode_hess_sparsity_in(int);
