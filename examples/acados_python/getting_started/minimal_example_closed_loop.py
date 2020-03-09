8526d31845e9e3af75b288fa60411f6a8bf4228f
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-03-09 12:01:53 +0100

    move closed_loop_example.py to getting_started root

--- examples/acados_python/getting_started/minimal_example_closed_loop.py
+++ examples/acados_python/getting_started/minimal_example_closed_loop.py
@@ -32,7 +32,7 @@
 #
 
 import sys
-sys.path.insert(0, '../common')
+sys.path.insert(0, 'common')
 
 from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver
 from export_pendulum_ode_model import export_pendulum_ode_model
