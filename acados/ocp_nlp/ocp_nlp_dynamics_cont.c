9de89f5698545124984454e7ec7c3d0d8a3ca024
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-04-20 12:19:50 +0200

    cost, dynamics align

--- acados/ocp_nlp/ocp_nlp_dynamics_cont.c
+++ acados/ocp_nlp/ocp_nlp_dynamics_cont.c
@@ -664,6 +664,8 @@ int ocp_nlp_dynamics_cont_model_calculate_size(void *config_, void *dims_)
     size += sizeof(ocp_nlp_dynamics_cont_model);
 
     size += config->sim_solver->model_calculate_size(config->sim_solver, dims->sim);
+    size += 1*8;
+    make_int_multiple_of(8, &size);
 
     return size;
 }
@@ -684,6 +686,7 @@ void *ocp_nlp_dynamics_cont_model_assign(void *config_, void *dims_, void *raw_m
     // struct
     ocp_nlp_dynamics_cont_model *model = (ocp_nlp_dynamics_cont_model *) c_ptr;
     c_ptr += sizeof(ocp_nlp_dynamics_cont_model);
+    align_char_to(8, &c_ptr);
 
     model->sim_model = config->sim_solver->model_assign(config->sim_solver, dims->sim, c_ptr);
     c_ptr += config->sim_solver->model_calculate_size(config->sim_solver, dims->sim);
