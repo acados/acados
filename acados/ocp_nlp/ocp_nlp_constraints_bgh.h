0a348f899397f424528908769541dad65dcac388
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-05-05 18:33:05 +0200

    C: BGH: add hessian support for nz>0, change signature of nl_constr_h_fun_jac_hess

--- acados/ocp_nlp/ocp_nlp_constraints_bgh.h
+++ acados/ocp_nlp/ocp_nlp_constraints_bgh.h
@@ -204,6 +204,8 @@ typedef struct
     struct blasfeo_dmat tmp_nv_nv;
     struct blasfeo_dmat tmp_nz_nh;
     struct blasfeo_dmat tmp_nv_nh;
+    struct blasfeo_dmat tmp_nz_nv;
+    struct blasfeo_dmat hess_z;
     struct blasfeo_dvec tmp_ni;
     struct blasfeo_dvec tmp_nh;
 } ocp_nlp_constraints_bgh_workspace;
