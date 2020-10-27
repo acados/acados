02549ea8d05b5e4e825d5b1bdbe26f9ecbd1c0f2
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-10-27 10:35:48 +0100

    OSQP: use polish step to obtain accurate solution (tol <= 1e-3)

--- acados/ocp_qp/ocp_qp_osqp.c
+++ acados/ocp_qp/ocp_qp_osqp.c
@@ -669,17 +669,19 @@ void ocp_qp_osqp_opts_set(void *config_, void *opts_, const char *field, void *v
     else if (!strcmp(field, "tol_stat"))
     {
         double *tol = value;
-        // opts->osqp_opts->eps_abs = *tol;
-        opts->osqp_opts->eps_rel = *tol;
-        opts->osqp_opts->eps_dual_inf = *tol;
-
         // printf("in ocp_qp_osqp_opts_set, tol_stat %e\n", *tol);
 
-        // if (*tol < 1e-2)
-        // {
-        //     opts->osqp_opts->polish = 1;
-        //     opts->osqp_opts->polish_refine_iter = 3;
-        // }
+        // opts->osqp_opts->eps_rel = *tol;
+        // opts->osqp_opts->eps_dual_inf = *tol;
+
+        opts->osqp_opts->eps_rel = fmax(*tol, 1e-5);
+        opts->osqp_opts->eps_dual_inf = fmax(*tol, 1e-5);
+
+        if (*tol <= 1e-3)
+        {
+            opts->osqp_opts->polish = 1;
+            opts->osqp_opts->polish_refine_iter = 5;
+        }
     }
     else if (!strcmp(field, "tol_eq"))
     {
