16dca510718044817ad745e5e6772434939ef64f
Joris Gillis <joris.gillis42@gmail.com>
2020-11-08 20:27:09 +0100

    Python: fixed bug in passing non-fortran ordered matrices

--- interfaces/acados_template/acados_template/acados_ocp_solver.py
+++ interfaces/acados_template/acados_template/acados_ocp_solver.py
@@ -1049,7 +1049,7 @@ class AcadosOcpSolver:
         return
 
 
-    def constraints_set(self, stage_, field_, value_):
+    def constraints_set(self, stage_, field_, value_, api='warn'):
         """
         set numerical data in the constraint module of the solver:
             :param stage_: integer corresponding to shooting node
@@ -1076,7 +1076,25 @@ class AcadosOcpSolver:
         value_shape = value_.shape
         if len(value_shape) == 1:
             value_shape = (value_shape[0], 0)
-
+        elif len(value_shape) == 2:
+            if api=='old':
+                pass
+            elif api=='warn':
+                if not np.all(np.ravel(value_, order='F')==np.ravel(value_, order='K')):
+                    raise Exception("You are relying on a now-fixed bug in constraints_set for field '{}'.\n".format(field_) +
+                                    "acados_template now correctly passes on any matrices to acados in column major format.\n" +
+                                    "Two options to fix this error: \n" +
+                                    " * Add api='old' to constraints_set to restore old incorrect behaviour\n" +
+                                    " * Add api='new' to constraints_set and remove any unnatural manipulation of the value argument " +
+                                    " such as non-mathematical transposes, reshaping, casting to fortran order, etc...")
+                # Get elements in column major order
+                value_ = np.ravel(value_, order='F')                
+            elif api=='new':
+                # Get elements in column major order
+                value_ = np.ravel(value_, order='F')
+            else:
+                raise Exception("Unknown api: '{}'".format(api))
+                
         if value_shape != tuple(dims):
             raise Exception('AcadosOcpSolver.constraints_set(): mismatching dimension' \
                 ' for field "{}" with dimension {} (you have {})'.format(field_, tuple(dims), value_shape))
