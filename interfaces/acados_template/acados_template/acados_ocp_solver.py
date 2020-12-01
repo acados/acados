acados/acados

commit 2ce6e65d4781a2efeb399b170082af6e526261a8

templates: rename function {{ model.name }}_acados*, adjust Python interface to adding ocp solver capsule

interfaces/acados_template/acados_template/acados_ocp_solver.py
@@ -689,28 +689,36 @@ class AcadosOcpSolver:
 
         self.shared_lib_name = 'c_generated_code/libacados_ocp_solver_' + model.name + '.so'
 
-        # get
+        # get shared_lib
         self.shared_lib = CDLL(self.shared_lib_name)
-        self.shared_lib.acados_create()
+
+        # create capsule
+        getattr(self.shared_lib, f"{model.name}_acados_create_capsule").restype = c_void_p
+        self.capsule = getattr(self.shared_lib, f"{model.name}_acados_create_capsule")()
+
+        # create solver
+        getattr(self.shared_lib, f"{model.name}_acados_create").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_create")(self.capsule)
         self.solver_created = True
 
-        self.shared_lib.acados_get_nlp_opts.restype = c_void_p
-        self.nlp_opts = self.shared_lib.acados_get_nlp_opts()
+        # get pointers solver
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts").argtypes = [c_void_p]
+        self.nlp_opts = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts")(self.capsule)
 
-        self.shared_lib.acados_get_nlp_dims.restype = c_void_p
-        self.nlp_dims = self.shared_lib.acados_get_nlp_dims()
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_dims").argtypes = [c_void_p]
+        self.nlp_dims = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_dims")(self.capsule)
 
-        self.shared_lib.acados_get_nlp_config.restype = c_void_p
-        self.nlp_config = self.shared_lib.acados_get_nlp_config()
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_config").argtypes = [c_void_p]
+        self.nlp_config = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_config")(self.capsule)
 
-        self.shared_lib.acados_get_nlp_out.restype = c_void_p
-        self.nlp_out = self.shared_lib.acados_get_nlp_out()
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_out").argtypes = [c_void_p]
+        self.nlp_out = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_out")(self.capsule)
 
-        self.shared_lib.acados_get_nlp_in.restype = c_void_p
-        self.nlp_in = self.shared_lib.acados_get_nlp_in()
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_in").argtypes = [c_void_p]
+        self.nlp_in = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_in")(self.capsule)
 
-        self.shared_lib.acados_get_nlp_solver.restype = c_void_p
-        self.nlp_solver = self.shared_lib.acados_get_nlp_solver()
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver").argtypes = [c_void_p]
+        self.nlp_solver = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver")(self.capsule)
 
         self.acados_ocp = acados_ocp
 
@@ -719,7 +727,10 @@ class AcadosOcpSolver:
         """
         solve the ocp with current input
         """
-        status = self.shared_lib.acados_solve()
+        model = self.acados_ocp.model
+
+        getattr(self.shared_lib, f"{model.name}_acados_solve").argtypes = [c_void_p]
+        status = getattr(self.shared_lib, f"{model.name}_acados_solve")(self.capsule)
         return status
 
 
@@ -944,6 +955,7 @@ class AcadosOcpSolver:
 
         # cast value_ to avoid conversion issues
         value_ = value_.astype(float)
+        model = self.acados_ocp.model
 
         field = field_
         field = field.encode('utf-8')
@@ -952,10 +964,12 @@ class AcadosOcpSolver:
 
         # treat parameters separately
         if field_ == 'p':
-            self.shared_lib.acados_update_params.argtypes = [c_int, POINTER(c_double)]
-            self.shared_lib.acados_update_params.restype = c_int
+            getattr(self.shared_lib, f"{model.name}_acados_update_params").argtypes = [c_void_p, c_int, POINTER(c_double)]
+            getattr(self.shared_lib, f"{model.name}_acados_update_params").restype = c_int
+
             value_data = cast(value_.ctypes.data, POINTER(c_double))
-            self.shared_lib.acados_update_params(stage, value_data, value_.shape[0])
+
+            getattr(self.shared_lib, f"{model.name}_acados_update_params")(self.capsule, stage, value_data, value_.shape[0])
         else:
             if field_ not in constraints_fields + cost_fields + out_fields:
                 raise Exception("AcadosOcpSolver.set(): {} is not a valid argument.\
@@ -1227,8 +1241,14 @@ class AcadosOcpSolver:
 
 
     def __del__(self):
+        model = self.acados_ocp.model
+
         if self.solver_created:
-            self.shared_lib.acados_free()
+            getattr(self.shared_lib, f"{model.name}_acados_free").argtypes = [c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_free")(self.capsule)
+
+            getattr(self.shared_lib, f"{model.name}_acados_free_capsule").argtypes = [c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_free_capsule")(self.capsule)
 
             try:
                 self.dlclose(self.shared_lib._handle)

interfaces/acados_template/acados_template/c_templates_tera/acados_mex_create.in.c
@@ -52,24 +52,24 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     int status = 0;
 
     // create solver
-    nlp_solver_capsule *acados_ocp_capsule = acados_{{ model.name }}_create_capsule();
+    nlp_solver_capsule *acados_ocp_capsule = {{ model.name }}_acados_create_capsule();
 
-    status = acados_{{ model.name }}_create(acados_ocp_capsule);
+    status = {{ model.name }}_acados_create(acados_ocp_capsule);
 
     if (status)
     {
-        mexPrintf("acados_{{ model.name }}_create() returned status %d.\n", status);
+        mexPrintf("{{ model.name }}_acados_create() returned status %d.\n", status);
     }
-    mexPrintf("acados_{{ model.name }}_create() -> success!\n");
+    mexPrintf("{{ model.name }}_acados_create() -> success!\n");
 
     // get pointers to nlp solver related objects
-    ocp_nlp_plan *nlp_plan = acados_{{ model.name }}_get_nlp_plan(acados_ocp_capsule);
-    ocp_nlp_config *nlp_config = acados_{{ model.name }}_get_nlp_config(acados_ocp_capsule);
-    ocp_nlp_dims *nlp_dims = acados_{{ model.name }}_get_nlp_dims(acados_ocp_capsule);
-    ocp_nlp_in *nlp_in = acados_{{ model.name }}_get_nlp_in(acados_ocp_capsule);
-    ocp_nlp_out *nlp_out = acados_{{ model.name }}_get_nlp_out(acados_ocp_capsule);
-    ocp_nlp_solver *nlp_solver = acados_{{ model.name }}_get_nlp_solver(acados_ocp_capsule);
-    void *nlp_opts = acados_{{ model.name }}_get_nlp_opts(acados_ocp_capsule);
+    ocp_nlp_plan *nlp_plan = {{ model.name }}_acados_get_nlp_plan(acados_ocp_capsule);
+    ocp_nlp_config *nlp_config = {{ model.name }}_acados_get_nlp_config(acados_ocp_capsule);
+    ocp_nlp_dims *nlp_dims = {{ model.name }}_acados_get_nlp_dims(acados_ocp_capsule);
+    ocp_nlp_in *nlp_in = {{ model.name }}_acados_get_nlp_in(acados_ocp_capsule);
+    ocp_nlp_out *nlp_out = {{ model.name }}_acados_get_nlp_out(acados_ocp_capsule);
+    ocp_nlp_solver *nlp_solver = {{ model.name }}_acados_get_nlp_solver(acados_ocp_capsule);
+    void *nlp_opts = {{ model.name }}_acados_get_nlp_opts(acados_ocp_capsule);
 
     mexPrintf("acados: got pointer to objectes!\n");
 

interfaces/acados_template/acados_template/c_templates_tera/acados_mex_free.in.c
@@ -53,16 +53,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     ptr = (long long *) mxGetData( mxGetField( C_ocp, 0, "capsule" ) );
     nlp_solver_capsule *capsule = (nlp_solver_capsule *) ptr[0];
 
-    status = acados_{{ model.name }}_free(capsule);
+    status = {{ model.name }}_acados_free(capsule);
     if (status)
     {
-        mexPrintf("acados_{{ model.name }}_free() returned status %d.\n", status);
+        mexPrintf("{{ model.name }}_acados_free() returned status %d.\n", status);
     }
 
-    status = acados_{{ model.name }}_free_capsule(capsule);
+    status = {{ model.name }}_acados_free_capsule(capsule);
     if (status)
     {
-        mexPrintf("acados_{{ model.name }}_free_capsule() returned status %d.\n", status);
+        mexPrintf("{{ model.name }}_acados_free_capsule() returned status %d.\n", status);
     }
 
     return;

interfaces/acados_template/acados_template/c_templates_tera/acados_mex_solve.in.c
@@ -54,6 +54,6 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     nlp_solver_capsule *capsule = (nlp_solver_capsule *) ptr[0];
 
     // solve
-    acados_{{ model.name }}_solve(capsule);
+    {{ model.name }}_acados_solve(capsule);
 
 }

interfaces/acados_template/acados_template/c_templates_tera/acados_solver.in.c
@@ -99,7 +99,7 @@
 
 // ** solver data **
 
-nlp_solver_capsule * acados_{{ model.name }}_create_capsule()
+nlp_solver_capsule * {{ model.name }}_acados_create_capsule()
 {
     void* capsule_mem = malloc(sizeof(nlp_solver_capsule));
     nlp_solver_capsule *capsule = (nlp_solver_capsule *) capsule_mem;
@@ -108,14 +108,14 @@ nlp_solver_capsule * acados_{{ model.name }}_create_capsule()
 }
 
 
-int acados_{{ model.name }}_free_capsule(nlp_solver_capsule *capsule)
+int {{ model.name }}_acados_free_capsule(nlp_solver_capsule *capsule)
 {
     free(capsule);
     return 0;
 }
 
 
-int acados_{{ model.name }}_create(nlp_solver_capsule * capsule)
+int {{ model.name }}_acados_create(nlp_solver_capsule * capsule)
 {
     int status = 0;
 
@@ -1687,7 +1687,7 @@ int acados_{{ model.name }}_create(nlp_solver_capsule * capsule)
 }
 
 
-int acados_{{ model.name }}_update_params(nlp_solver_capsule * capsule, int stage, double *p, int np)
+int {{ model.name }}_acados_update_params(nlp_solver_capsule * capsule, int stage, double *p, int np)
 {
     int solver_status = 0;
 
@@ -1786,7 +1786,7 @@ int acados_{{ model.name }}_update_params(nlp_solver_capsule * capsule, int stag
 
 
 
-int acados_{{ model.name }}_solve(nlp_solver_capsule * capsule)
+int {{ model.name }}_acados_solve(nlp_solver_capsule * capsule)
 {
     // solve NLP 
     int solver_status = ocp_nlp_solve(capsule->nlp_solver, capsule->nlp_in, capsule->nlp_out);
@@ -1795,7 +1795,7 @@ int acados_{{ model.name }}_solve(nlp_solver_capsule * capsule)
 }
 
 
-int acados_{{ model.name }}_free(nlp_solver_capsule * capsule)
+int {{ model.name }}_acados_free(nlp_solver_capsule * capsule)
 {
     // free memory
     ocp_nlp_solver_opts_destroy(capsule->nlp_opts);
@@ -1943,16 +1943,16 @@ int acados_{{ model.name }}_free(nlp_solver_capsule * capsule)
     return 0;
 }
 
-ocp_nlp_in *acados_{{ model.name }}_get_nlp_in(nlp_solver_capsule * capsule) { return capsule->nlp_in; }
-ocp_nlp_out *acados_{{ model.name }}_get_nlp_out(nlp_solver_capsule * capsule) { return capsule->nlp_out; }
-ocp_nlp_solver *acados_{{ model.name }}_get_nlp_solver(nlp_solver_capsule * capsule) { return capsule->nlp_solver; }
-ocp_nlp_config *acados_{{ model.name }}_get_nlp_config(nlp_solver_capsule * capsule) { return capsule->nlp_config; }
-void *acados_{{ model.name }}_get_nlp_opts(nlp_solver_capsule * capsule) { return capsule->nlp_opts; }
-ocp_nlp_dims *acados_{{ model.name }}_get_nlp_dims(nlp_solver_capsule * capsule) { return capsule->nlp_dims; }
-ocp_nlp_plan *acados_{{ model.name }}_get_nlp_plan(nlp_solver_capsule * capsule) { return capsule->nlp_solver_plan; }
+ocp_nlp_in *{{ model.name }}_acados_get_nlp_in(nlp_solver_capsule * capsule) { return capsule->nlp_in; }
+ocp_nlp_out *{{ model.name }}_acados_get_nlp_out(nlp_solver_capsule * capsule) { return capsule->nlp_out; }
+ocp_nlp_solver *{{ model.name }}_acados_get_nlp_solver(nlp_solver_capsule * capsule) { return capsule->nlp_solver; }
+ocp_nlp_config *{{ model.name }}_acados_get_nlp_config(nlp_solver_capsule * capsule) { return capsule->nlp_config; }
+void *{{ model.name }}_acados_get_nlp_opts(nlp_solver_capsule * capsule) { return capsule->nlp_opts; }
+ocp_nlp_dims *{{ model.name }}_acados_get_nlp_dims(nlp_solver_capsule * capsule) { return capsule->nlp_dims; }
+ocp_nlp_plan *{{ model.name }}_acados_get_nlp_plan(nlp_solver_capsule * capsule) { return capsule->nlp_solver_plan; }
 
 
-void acados_{{ model.name }}_print_stats(nlp_solver_capsule * capsule)
+void {{ model.name }}_acados_print_stats(nlp_solver_capsule * capsule)
 {
     int sqp_iter, stat_m, stat_n, tmp_int;
     ocp_nlp_get(capsule->nlp_config, capsule->nlp_solver, "sqp_iter", &sqp_iter);

interfaces/acados_template/acados_template/c_templates_tera/acados_solver.in.h
@@ -101,22 +101,22 @@ typedef struct nlp_solver_capsule
     external_function_param_casadi nl_constr_h_e_fun_jac_hess;
 } nlp_solver_capsule;
 
-nlp_solver_capsule * acados_{{ model.name }}_create_capsule();
-int acados_{{ model.name }}_free_capsule(nlp_solver_capsule *capsule);
+nlp_solver_capsule * {{ model.name }}_acados_create_capsule();
+int {{ model.name }}_acados_free_capsule(nlp_solver_capsule *capsule);
 
-int acados_{{ model.name }}_create(nlp_solver_capsule * capsule);
-int acados_{{ model.name }}_update_params(nlp_solver_capsule * capsule, int stage, double *value, int np);
-int acados_{{ model.name }}_solve(nlp_solver_capsule * capsule);
-int acados_{{ model.name }}_free(nlp_solver_capsule * capsule);
-void acados_{{ model.name }}_print_stats(nlp_solver_capsule * capsule);
+int {{ model.name }}_acados_create(nlp_solver_capsule * capsule);
+int {{ model.name }}_acados_update_params(nlp_solver_capsule * capsule, int stage, double *value, int np);
+int {{ model.name }}_acados_solve(nlp_solver_capsule * capsule);
+int {{ model.name }}_acados_free(nlp_solver_capsule * capsule);
+void {{ model.name }}_acados_print_stats(nlp_solver_capsule * capsule);
 
-ocp_nlp_in *acados_{{ model.name }}_get_nlp_in(nlp_solver_capsule * capsule);
-ocp_nlp_out *acados_{{ model.name }}_get_nlp_out(nlp_solver_capsule * capsule);
-ocp_nlp_solver *acados_{{ model.name }}_get_nlp_solver(nlp_solver_capsule * capsule);
-ocp_nlp_config *acados_{{ model.name }}_get_nlp_config(nlp_solver_capsule * capsule);
-void *acados_{{ model.name }}_get_nlp_opts(nlp_solver_capsule * capsule);
-ocp_nlp_dims *acados_{{ model.name }}_get_nlp_dims(nlp_solver_capsule * capsule);
-ocp_nlp_plan *acados_{{ model.name }}_get_nlp_plan(nlp_solver_capsule * capsule);
+ocp_nlp_in *{{ model.name }}_acados_get_nlp_in(nlp_solver_capsule * capsule);
+ocp_nlp_out *{{ model.name }}_acados_get_nlp_out(nlp_solver_capsule * capsule);
+ocp_nlp_solver *{{ model.name }}_acados_get_nlp_solver(nlp_solver_capsule * capsule);
+ocp_nlp_config *{{ model.name }}_acados_get_nlp_config(nlp_solver_capsule * capsule);
+void *{{ model.name }}_acados_get_nlp_opts(nlp_solver_capsule * capsule);
+ocp_nlp_dims *{{ model.name }}_acados_get_nlp_dims(nlp_solver_capsule * capsule);
+ocp_nlp_plan *{{ model.name }}_acados_get_nlp_plan(nlp_solver_capsule * capsule);
 
 #ifdef __cplusplus
 } /* extern "C" */

interfaces/acados_template/acados_template/c_templates_tera/acados_solver_sfun.in.c
@@ -194,7 +194,7 @@ static void mdlInitializeSampleTimes(SimStruct *S)
 
 static void mdlStart(SimStruct *S)
 {
-    acados_{{ model.name }}_create();
+    {{ model.name }}_acados_create();
 }
 
 static void mdlOutputs(SimStruct *S, int_T tid)
@@ -251,7 +251,7 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     {
         for (int jj = 0; jj < {{ dims.np }}; jj++)
             buffer[jj] = (double)(*in_sign[ii*{{dims.np}}+jj]);
-        acados_{{ model.name }}_update_params(ii, buffer, {{ dims.np }});
+        {{ model.name }}_acados_update_params(ii, buffer, {{ dims.np }});
     }
 {%- endif %}
 
@@ -347,7 +347,7 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     /* call solver */
     int rti_phase = 0;
     ocp_nlp_solver_opts_set({{ model.name }}_ptrs.nlp_config, {{ model.name }}_ptrs.nlp_opts, "rti_phase", &rti_phase);
-    int acados_status = acados_{{ model.name }}_solve();
+    int acados_status = {{ model.name }}_acados_solve();
 
 
     /* set outputs */
@@ -385,7 +385,7 @@ static void mdlOutputs(SimStruct *S, int_T tid)
 
 static void mdlTerminate(SimStruct *S)
 {
-    acados_{{ model.name }}_free();
+    {{ model.name }}_acados_free();
 }
 
 

interfaces/acados_template/acados_template/c_templates_tera/main.in.c
@@ -46,21 +46,21 @@
 int main()
 {
 
-    nlp_solver_capsule acados_ocp_capsule;
-    int status = acados_{{ model.name }}_create(&acados_ocp_capsule);
+    nlp_solver_capsule *acados_ocp_capsule = {{ model.name }}_acados_create_capsule();
+    int status = {{ model.name }}_acados_create(acados_ocp_capsule);
 
     if (status)
     {
-        printf("acados_{{ model.name }}_create() returned status %d. Exiting.\n", status);
+        printf("{{ model.name }}_acados_create() returned status %d. Exiting.\n", status);
         exit(1);
     }
 
-    ocp_nlp_config *nlp_config = acados_{{ model.name }}_get_nlp_config(&acados_ocp_capsule);
-    ocp_nlp_dims *nlp_dims = acados_{{ model.name }}_get_nlp_dims(&acados_ocp_capsule);
-    ocp_nlp_in *nlp_in = acados_{{ model.name }}_get_nlp_in(&acados_ocp_capsule);
-    ocp_nlp_out *nlp_out = acados_{{ model.name }}_get_nlp_out(&acados_ocp_capsule);
-    ocp_nlp_solver *nlp_solver = acados_{{ model.name }}_get_nlp_solver(&acados_ocp_capsule);
-    void *nlp_opts = acados_{{ model.name }}_get_nlp_opts(&acados_ocp_capsule);
+    ocp_nlp_config *nlp_config = {{ model.name }}_acados_get_nlp_config(acados_ocp_capsule);
+    ocp_nlp_dims *nlp_dims = {{ model.name }}_acados_get_nlp_dims(acados_ocp_capsule);
+    ocp_nlp_in *nlp_in = {{ model.name }}_acados_get_nlp_in(acados_ocp_capsule);
+    ocp_nlp_out *nlp_out = {{ model.name }}_acados_get_nlp_out(acados_ocp_capsule);
+    ocp_nlp_solver *nlp_solver = {{ model.name }}_acados_get_nlp_solver(acados_ocp_capsule);
+    void *nlp_opts = {{ model.name }}_acados_get_nlp_opts(acados_ocp_capsule);
 
     // initial condition
     int idxbx0[{{ dims.nbx_0 }}];
@@ -101,7 +101,7 @@ int main()
 
     for (int ii = 0; ii <= {{ dims.N }}; ii++)
     {
-        acados_{{ model.name }}_update_params(&acados_ocp_capsule, ii, p, {{ dims.np }});
+        {{ model.name }}_acados_update_params(acados_ocp_capsule, ii, p, {{ dims.np }});
     }
   {% endif %}{# if np > 0 #}
 
@@ -128,7 +128,7 @@ int main()
             ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "u", u0);
         }
         ocp_nlp_solver_opts_set(nlp_config, nlp_opts, "rti_phase", &rti_phase);
-        status = acados_{{ model.name }}_solve(&acados_ocp_capsule);
+        status = {{ model.name }}_acados_solve(acados_ocp_capsule);
         ocp_nlp_get(nlp_config, nlp_solver, "time_tot", &elapsed_time);
         min_time = MIN(elapsed_time, min_time);
     }
@@ -149,27 +149,32 @@ int main()
 
     if (status == ACADOS_SUCCESS)
     {
-        printf("acados_{{ model.name }}_solve(): SUCCESS!\n");
+        printf("{{ model.name }}_acados_solve(): SUCCESS!\n");
     }
     else
     {
-        printf("acados_{{ model.name }}_solve() failed with status %d.\n", status);
+        printf("{{ model.name }}_acados_solve() failed with status %d.\n", status);
     }
 
     // get solution
     ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "kkt_norm_inf", &kkt_norm_inf);
     ocp_nlp_get(nlp_config, nlp_solver, "sqp_iter", &sqp_iter);
 
-    acados_{{ model.name }}_print_stats(&acados_ocp_capsule);
+    {{ model.name }}_acados_print_stats(acados_ocp_capsule);
 
     printf("\nSolver info:\n");
     printf(" SQP iterations %2d\n minimum time for %d solve %f [ms]\n KKT %e\n",
            sqp_iter, NTIMINGS, min_time*1000, kkt_norm_inf);
 
     // free solver
-    status = acados_{{ model.name }}_free(&acados_ocp_capsule);
+    status = {{ model.name }}_acados_free(acados_ocp_capsule);
     if (status) {
-        printf("acados_{{ model.name }}_free() returned status %d. \n", status);
+        printf("{{ model.name }}_acados_free() returned status %d. \n", status);
+    }
+    // free solver capsule
+    status = {{ model.name }}_acados_free_capsule(acados_ocp_capsule);
+    if (status) {
+        printf("{{ model.name }}_acados_free_capsule() returned status %d. \n", status);
     }
 
     return status;

commit 5024311533c3eaed09575a84143379e983ff926e

Python: fixing some regression introduced in 2ce6e65d4
Fixes segfaults on my linux system

interfaces/acados_template/acados_template/acados_ocp_solver.py
@@ -698,26 +698,33 @@ class AcadosOcpSolver:
 
         # create solver
         getattr(self.shared_lib, f"{model.name}_acados_create").argtypes = [c_void_p]
-        getattr(self.shared_lib, f"{model.name}_acados_create")(self.capsule)
+        getattr(self.shared_lib, f"{model.name}_acados_create").restype = c_int
+        assert getattr(self.shared_lib, f"{model.name}_acados_create")(self.capsule)==0
         self.solver_created = True
 
         # get pointers solver
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts").restype = c_void_p
         self.nlp_opts = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts")(self.capsule)
 
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_dims").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_dims").restype = c_void_p
         self.nlp_dims = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_dims")(self.capsule)
 
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_config").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_config").restype = c_void_p
         self.nlp_config = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_config")(self.capsule)
 
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_out").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_out").restype = c_void_p
         self.nlp_out = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_out")(self.capsule)
 
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_in").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_in").restype = c_void_p
         self.nlp_in = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_in")(self.capsule)
 
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver").restype = c_void_p
         self.nlp_solver = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver")(self.capsule)
 
         self.acados_ocp = acados_ocp
@@ -730,6 +737,7 @@ class AcadosOcpSolver:
         model = self.acados_ocp.model
 
         getattr(self.shared_lib, f"{model.name}_acados_solve").argtypes = [c_void_p]
+        getattr(self.shared_lib, f"{model.name}_acados_solve").restype = c_int
         status = getattr(self.shared_lib, f"{model.name}_acados_solve")(self.capsule)
         return status
 
@@ -969,7 +977,7 @@ class AcadosOcpSolver:
 
             value_data = cast(value_.ctypes.data, POINTER(c_double))
 
-            getattr(self.shared_lib, f"{model.name}_acados_update_params")(self.capsule, stage, value_data, value_.shape[0])
+            assert getattr(self.shared_lib, f"{model.name}_acados_update_params")(self.capsule, stage, value_data, value_.shape[0])==0
         else:
             if field_ not in constraints_fields + cost_fields + out_fields:
                 raise Exception("AcadosOcpSolver.set(): {} is not a valid argument.\
@@ -1245,9 +1253,11 @@ class AcadosOcpSolver:
 
         if self.solver_created:
             getattr(self.shared_lib, f"{model.name}_acados_free").argtypes = [c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_free").restype = c_int
             getattr(self.shared_lib, f"{model.name}_acados_free")(self.capsule)
 
             getattr(self.shared_lib, f"{model.name}_acados_free_capsule").argtypes = [c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_free_capsule").restype = c_int
             getattr(self.shared_lib, f"{model.name}_acados_free_capsule")(self.capsule)
 
             try: