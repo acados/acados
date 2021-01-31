c144193a4f9814b6c1860dc1dd15d27bbb55eac2
schwieni <mschwienbacher@gmail.com>
2021-01-31 12:48:17 +0100

    acados_size_t for missing *calculate_size functions in headers
    This should reduce the size restriction for large problems with memory
    requirement >2GB.
    Note, that qore currently does not seem to be compiled.

--- acados/dense_qp/dense_qp_hpipm.h
+++ acados/dense_qp/dense_qp_hpipm.h
@@ -75,11 +75,11 @@ void dense_qp_hpipm_opts_initialize_default(void *config, void *dims, void *opts
 //
 void dense_qp_hpipm_opts_update(void *config, void *dims, void *opts_);
 //
-size_t dense_qp_hpipm_calculate_memory_size(void *dims, void *opts_);
+acados_size_t dense_qp_hpipm_calculate_memory_size(void *dims, void *opts_);
 //
 void *dense_qp_hpipm_assign_memory(void *dims, void *opts_, void *raw_memory);
 //
-size_t dense_qp_hpipm_calculate_workspace_size(void *dims, void *opts_);
+acados_size_t dense_qp_hpipm_calculate_workspace_size(void *dims, void *opts_);
 //
 int dense_qp_hpipm(void *config, void *qp_in, void *qp_out, void *opts_, void *mem_, void *work_);
 //
