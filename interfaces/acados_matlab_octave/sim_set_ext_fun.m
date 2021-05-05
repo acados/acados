c20225b605711f21c0c6a9da8741902b93af964c
Caspar Gruijthuijsen <caspar.gruijthuijsen@sioux.eu>
2021-05-05 13:55:46 +0200

    cleaned up openmp linker flags

--- interfaces/acados_matlab_octave/sim_set_ext_fun.m
+++ interfaces/acados_matlab_octave/sim_set_ext_fun.m
@@ -175,11 +175,7 @@ if (strcmp(opts_struct.compile_interface, 'true') || strcmp(opts_struct.codgen_m
             input_file = fopen(fullfile(opts_struct.output_dir, 'cflags_octave.txt'), 'r');
             cflags_tmp = fscanf(input_file, '%[^\n]s');
             fclose(input_file);
-            if ~ismac()
-                cflags_tmp = [cflags_tmp, ' -std=c99 -fopenmp'];
-            else
-                cflags_tmp = [cflags_tmp, ' -std=c99'];
-            end
+            cflags_tmp = [cflags_tmp, ' -std=c99'];
             input_file = fopen(fullfile(opts_struct.output_dir, 'cflags_octave.txt'), 'w');
             fprintf(input_file, '%s', cflags_tmp);
             fclose(input_file);
@@ -205,11 +201,7 @@ if (strcmp(opts_struct.compile_interface, 'true') || strcmp(opts_struct.codgen_m
             setenv('CFLAGS', cflags_tmp);
             mex(acados_include, acados_interfaces_include, acados_lib_path, acados_matlab_octave_lib_path, model_lib_path, '-lacados', '-lhpipm', '-lblasfeo', ['-l', model_name], mex_files{1});
         else
-            if ~ismac()
-                FLAGS = 'CFLAGS=$CFLAGS -std=c99 -fopenmp';
-            else
-                FLAGS = 'CFLAGS=$CFLAGS -std=c99';
-            end
+            FLAGS = 'CFLAGS=$CFLAGS -std=c99';
             mex( mex_flags, FLAGS, ['-DSET_FIELD=', set_fields{ii}],...
                  ['-DMEX_FIELD=', mex_fields{ii}], ['-DFUN_NAME=', fun_names{ii}],...
                  acados_include, acados_interfaces_include, acados_lib_path,...
