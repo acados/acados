72a92796b457f329b3fd6fb55361057ed3e9c7f8
Jonathan Frey <jonathanpaulfrey@gmail.com>
2021-05-26 16:02:55 +0200

    test GA: output environment variables

--- examples/acados_matlab_octave/getting_started/test.m
+++ examples/acados_matlab_octave/getting_started/test.m
@@ -1,1 +1,21 @@
 assert(1+1==2)
+
+disp('assertation works')
+
+disp('checking environment variables')
+
+disp('MATLABPATH')
+disp(getenv('MATLABPATH'))
+
+disp('MODEL_FOLDER')
+disp(getenv('MODEL_FOLDER'))
+
+
+disp('ENV_RUN')
+disp(getenv('ENV_RUN'))
+
+disp('LD_LIBRARY_PATH')
+disp(getenv('LD_LIBRARY_PATH'))
+
+disp('pwd')
+disp(pwd)
