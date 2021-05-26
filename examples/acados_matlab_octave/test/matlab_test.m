99f0d5974ae4db4776b6201207242b41dbbee4cc
Jonathan Frey <jonathanpaulfrey@gmail.com>
2021-05-26 18:40:12 +0200

    GA: run all Octave tests in matlab_test.m

--- examples/acados_matlab_octave/test/matlab_test.m
+++ examples/acados_matlab_octave/test/matlab_test.m
@@ -19,3 +19,29 @@ disp(getenv('LD_LIBRARY_PATH'))
 
 disp('pwd')
 disp(pwd)
+
+disp('running tests')
+
+%% run all Octave tests
+test_names = ["run_test_dim_check",
+"run_test_ocp_mass_spring",
+"run_test_ocp_pendulum",
+"run_test_ocp_pendulum_dae",
+"run_test_ocp_simple_dae",
+"run_test_ocp_wtnx6",
+"run_test_sim_adj",
+"run_test_sim_dae",
+"run_test_sim_forw",
+"run_test_sim_hess",
+"run_test_template_ocp_disc_dyn",
+"run_test_template_ocp_linear_dae",
+"run_test_template_ocp_pendulum_auto",
+"run_test_template_ocp_pendulum_exact_hess",
+"run_test_template_ocp_pendulum_ext_cost",
+"run_test_template_ocp_pendulum_gnsf",
+"run_test_template_ocp_pendulum_nls"];
+
+for k = 1:length(test_names)
+    disp(["running test ", test_names(k)]);
+    run(test_names(k))
+end
