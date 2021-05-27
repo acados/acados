1456f1980c187c74dddb12ef21e7b5e4e6eb841e
Jonathan Frey <jonathanpaulfrey@gmail.com>
2021-05-27 18:13:39 +0200

    GA: separate Matlab tests into native MEX and templates

--- examples/acados_matlab_octave/test/matlab_test_native_mex.m
+++ examples/acados_matlab_octave/test/matlab_test_native_mex.m
@@ -32,14 +32,7 @@ test_names = ["run_test_dim_check",
 "run_test_sim_adj",
 "run_test_sim_dae",
 "run_test_sim_forw",
-"run_test_sim_hess",
-"run_test_template_ocp_disc_dyn",
-"run_test_template_ocp_linear_dae",
-"run_test_template_ocp_pendulum_auto",
-"run_test_template_ocp_pendulum_exact_hess",
-"run_test_template_ocp_pendulum_ext_cost",
-"run_test_template_ocp_pendulum_gnsf",
-"run_test_template_ocp_pendulum_nls"];
+"run_test_sim_hess"];
 
 for k = 1:length(test_names)
     disp(["running test ", test_names(k)]);
