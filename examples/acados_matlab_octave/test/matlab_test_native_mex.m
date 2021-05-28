0334f7c7f67a52ee511037fa691552c1805493ea
Jonathan Frey <jonathanpaulfrey@gmail.com>
2021-05-28 15:28:05 +0200

    GA: comment a few test for Matlab to run faster

--- examples/acados_matlab_octave/test/matlab_test_native_mex.m
+++ examples/acados_matlab_octave/test/matlab_test_native_mex.m
@@ -25,13 +25,13 @@ disp('running tests')
 %% run all Octave tests
 test_names = ["run_test_dim_check",
 "run_test_ocp_mass_spring",
-"run_test_ocp_pendulum",
+% "run_test_ocp_pendulum",
 "run_test_ocp_pendulum_dae",
 "run_test_ocp_simple_dae",
 "run_test_ocp_wtnx6",
-"run_test_sim_adj",
+% "run_test_sim_adj",
 "run_test_sim_dae",
-"run_test_sim_forw",
+% "run_test_sim_forw",
 "run_test_sim_hess"];
 
 for k = 1:length(test_names)
