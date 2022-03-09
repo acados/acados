acados/acados

commit 05398550e9c6f766a2c30356c35cc3a12df747fd

acados_template improvements (#747)
* acados_template: identify constant time-steps

This change enables nicer code export with constant time-steps in
acados_solver.in.c. Due to numerical reasons, the difference between
shooting_nodes can lead to different time_step values.

* acados_template: add number of stages to be an argument to acados_create function

Since the model does not depend on the number of stages N, N is application dependent
and it should be possible to reuse the exported code in C with a different
number of stages N. This is made possible here.
As an example main.in.c and main_sim.in.c are modified accordingly.

* acados_template: add and use dimension defines in sim_solver

* acados_template: fix compile error

* Changes requested in PR #747

* c_templates: remove unnecessary defines from main_sim.in.c

* reuse code: add minimal python example including necessary interfaces

* add test for minimal_example_ocp_reuse_code.py

* templates: dont use __FUNCTION__, edit comments

Co-authored-by: schwieni <mschwienbacher@gmail.com>
Co-authored-by: Jonathan Frey <jonathanpaulfrey@gmail.com>

interfaces/acados_template/acados_template/acados_ocp_solver.py
@@ -432,9 +432,15 @@ def make_ocp_dims_consistent(acados_ocp):
         if np.shape(opts.shooting_nodes)[0] != dims.N+1:
             raise Exception('inconsistent dimension N, regarding shooting_nodes.')
 
-        time_steps = np.zeros((dims.N,))
-        for i in range(dims.N):
-            time_steps[i] = opts.shooting_nodes[i+1] - opts.shooting_nodes[i]
+        time_steps = opts.shooting_nodes[1:] - opts.shooting_nodes[0:-1]
+        # identify constant time-steps: due to numerical reasons the content of time_steps might vary a bit
+        delta_time_steps = time_steps[1:] - time_steps[0:-1]
+        avg_time_steps = np.average(time_steps)
+        # criterion for constant time-step detection: the min/max difference in values normalized by the average
+        check_const_time_step = np.max(delta_time_steps)-np.min(delta_time_steps) / avg_time_steps
+        # if the criterion is small, we have a constant time-step
+        if check_const_time_step < 1e-9:
+            time_steps[:] = avg_time_steps  # if we have a constant time-step: apply the average time-step
         opts.time_steps = time_steps
 
     elif (not is_empty(opts.time_steps)) and (not is_empty(opts.shooting_nodes)):
@@ -777,7 +783,9 @@ class AcadosOcpSolver:
         if simulink_opts == None:
             acados_path = get_acados_path()
             json_path = os.path.join(acados_path, 'interfaces/acados_template/acados_template')
-            with open(json_path + '/simulink_default_opts.json', 'r') as f:
+            json_filename = json_path + '/simulink_default_opts.json'
+            print(f'using simulink json-file: "{json_filename}"')
+            with open(json_filename, 'r') as f:
                 simulink_opts = json.load(f)
 
         # make dims consistent
@@ -826,7 +834,18 @@ class AcadosOcpSolver:
         assert getattr(self.shared_lib, f"{model.name}_acados_create")(self.capsule)==0
         self.solver_created = True
 
+        self.acados_ocp = acados_ocp
+
         # get pointers solver
+        self.__get_pointers_solver()
+
+
+    def __get_pointers_solver(self):
+        """
+        Private function to get the pointers for solver
+        """
+        # get pointers solver
+        model = self.acados_ocp.model
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts").argtypes = [c_void_p]
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts").restype = c_void_p
         self.nlp_opts = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_opts")(self.capsule)
@@ -851,8 +870,6 @@ class AcadosOcpSolver:
         getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver").restype = c_void_p
         self.nlp_solver = getattr(self.shared_lib, f"{model.name}_acados_get_nlp_solver")(self.capsule)
 
-        self.acados_ocp = acados_ocp
-
 
     def solve(self):
         """
@@ -864,6 +881,58 @@ class AcadosOcpSolver:
         getattr(self.shared_lib, f"{model.name}_acados_solve").restype = c_int
         status = getattr(self.shared_lib, f"{model.name}_acados_solve")(self.capsule)
         return status
+
+
+    def set_new_time_steps(self, new_time_steps):
+        """
+        Set new time steps before solving. Only reload library without code generation but with new time steps.
+
+            :param new_time_steps: vector of new time steps for the solver
+
+            .. note:: This allows for different use-cases: either set a new size of time-steps or a new distribution of
+                      the shooting nodes without changing the number, e.g., to reach a different final time. Both cases
+                      do not require a new code export and compilation.
+        """
+
+        # unlikely but still possible
+        if not self.solver_created:
+            raise Exception('Solver was not yet created!')
+
+        # check if time steps really changed in value
+        if np.array_equal(self.acados_ocp.solver_options.time_steps, new_time_steps):
+            return
+
+        N = new_time_steps.size
+        model = self.acados_ocp.model
+        new_time_steps_data = cast(new_time_steps.ctypes.data, POINTER(c_double))
+
+        # check if recreation of acados is necessary (no need to recreate acados if sizes are identical)
+        if self.acados_ocp.solver_options.time_steps.size == N:
+            getattr(self.shared_lib, f"{model.name}_acados_update_time_steps").argtypes = [c_void_p, c_int, c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_update_time_steps").restype = c_int
+            assert getattr(self.shared_lib, f"{model.name}_acados_update_time_steps")(self.capsule, N, new_time_steps_data) == 0
+        else:  # recreate the solver with the new time steps
+            self.solver_created = False
+
+            # delete old memory (analog to __del__)
+            getattr(self.shared_lib, f"{model.name}_acados_free").argtypes = [c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_free").restype = c_int
+            getattr(self.shared_lib, f"{model.name}_acados_free")(self.capsule)
+
+            # store N and new time steps
+            self.N = self.acados_ocp.dims.N = N
+            self.acados_ocp.solver_options.time_steps = new_time_steps
+            self.acados_ocp.solver_options.Tsim = self.acados_ocp.solver_options.time_steps[0]
+
+            # create solver with new time steps
+            getattr(self.shared_lib, f"{model.name}_acados_create_with_discretization").argtypes = [c_void_p, c_int, c_void_p]
+            getattr(self.shared_lib, f"{model.name}_acados_create_with_discretization").restype = c_int
+            assert getattr(self.shared_lib, f"{model.name}_acados_create_with_discretization")(self.capsule, N, new_time_steps_data) == 0
+
+            self.solver_created = True
+
+            # get pointers solver
+            self.__get_pointers_solver()
 
 
     def get(self, stage_, field_):

examples/acados_python/getting_started/common/utils.py
@@ -36,7 +36,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_pendulum(shooting_nodes, u_max, U, X_true, X_est=None, Y_measured=None, latexify=False):
+def plot_pendulum(shooting_nodes, u_max, U, X_true, X_est=None, Y_measured=None, latexify=False, plt_show=True, X_true_label=None):
     """
     Params:
         shooting_nodes: time values of the discretization
@@ -77,7 +77,11 @@ def plot_pendulum(shooting_nodes, u_max, U, X_true, X_est=None, Y_measured=None,
         t_mhe = np.linspace(N_mhe * Ts, Tf, N_sim-N_mhe)
 
     plt.subplot(nx+1, 1, 1)
-    plt.step(t, np.append([U[0]], U), color='r')
+    line, = plt.step(t, np.append([U[0]], U))
+    if X_true_label is not None:
+        line.set_label(X_true_label)
+    else:
+        line.set_color('r')
     plt.title('closed-loop simulation')
     plt.ylabel('$u$')
     plt.xlabel('$t$')
@@ -90,7 +94,9 @@ def plot_pendulum(shooting_nodes, u_max, U, X_true, X_est=None, Y_measured=None,
 
     for i in range(nx):
         plt.subplot(nx+1, 1, i+2)
-        plt.plot(t, X_true[:,i], label='true')
+        line, = plt.plot(t, X_true[:, i], label='true')
+        if X_true_label is not None:
+            line.set_label(X_true_label)
 
         if WITH_ESTIMATION:
             plt.plot(t_mhe, X_est[:, i], '--', label='estimated')
@@ -104,5 +110,5 @@ def plot_pendulum(shooting_nodes, u_max, U, X_true, X_est=None, Y_measured=None,
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, hspace=0.4)
 
     # avoid plotting when running on Travis
-    if os.environ.get('ACADOS_ON_TRAVIS') is None:
+    if os.environ.get('ACADOS_ON_TRAVIS') is None and plt_show:
         plt.show()

examples/acados_python/getting_started/ocp/minimal_example_ocp_reuse_code.py
@@ -1,0 +1,208 @@
+# Minimal example showing how to reuse the exported c-code with
+# different time-steps.
+#
+# There are two use-cases demonstrated here. One use-case is to change
+# the length of the time-stamp vector (this results in a different
+# N). Another use-case is to change the final time but keep the number
+# of shooting nodes identical. Reusing the exported code with variing
+# N can be useful especially in a c-only application where the process
+# of code-generation should only be done once.
+#
+# This example is an extension of the 'minimal_example_ocp.py' example.
+
+#
+# Copyright 2021 Markus Schwienbacher, Gianluca Frison, Dimitris Kouzoupis,
+# Robin Verschueren, Andrea Zanelli, Niels van Duijkeren, Jonathan Frey,
+# Tommaso Sartor, Branimir Novoselnik, Rien Quirynen, Rezart Qelibari,
+# Dang Doan, Jonas Koenemann, Yutao Chen, Tobias Schöls, Jonas Schlagenhauf,
+# Moritz Diehl
+#
+# This file is part of acados.
+#
+# The 2-Clause BSD License
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.;
+#
+import os
+import sys
+
+sys.path.insert(0, '../common')
+
+from acados_template import AcadosOcp, AcadosOcpSolver
+from pendulum_model import export_pendulum_ode_model
+import numpy as np
+import scipy.linalg
+from utils import plot_pendulum
+
+print('This example demonstrates 2 use-cases for reuse of the code export.')
+
+# create ocp object to formulate the OCP
+ocp = AcadosOcp()
+
+# set model
+model = export_pendulum_ode_model()
+ocp.model = model
+
+nx = model.x.size()[0]
+nu = model.u.size()[0]
+ny = nx + nu
+ny_e = nx
+
+# define the different options for the use-case demonstration
+N0 = 20  # original number of shooting nodes
+N12 = 15  # change the number of shooting nodes for use-cases 1 and 2
+Tf_01 = 1.0  # original final time and for use-case 1
+Tf_2 = Tf_01 * 0.7  # change final time for use-case 2 (but keep N identical)
+
+# set dimensions
+ocp.dims.N = N0
+
+# set cost
+Q = 2 * np.diag([1e3, 1e3, 1e-2, 1e-2])
+R = 2 * np.diag([1e-2])
+
+ocp.cost.W_e = Q
+ocp.cost.W = scipy.linalg.block_diag(Q, R)
+
+ocp.cost.cost_type = 'LINEAR_LS'
+ocp.cost.cost_type_e = 'LINEAR_LS'
+
+ocp.cost.Vx = np.zeros((ny, nx))
+ocp.cost.Vx[:nx, :nx] = np.eye(nx)
+
+Vu = np.zeros((ny, nu))
+Vu[4, 0] = 1.0
+ocp.cost.Vu = Vu
+
+ocp.cost.Vx_e = np.eye(nx)
+
+ocp.cost.yref = np.zeros((ny,))
+ocp.cost.yref_e = np.zeros((ny_e,))
+
+# set constraints
+Fmax = 80
+ocp.constraints.lbu = np.array([-Fmax])
+ocp.constraints.ubu = np.array([+Fmax])
+ocp.constraints.idxbu = np.array([0])
+
+ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
+
+# set options
+ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'  # FULL_CONDENSING_QPOASES
+# PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
+# PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP
+ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
+ocp.solver_options.integrator_type = 'ERK'
+# ocp.solver_options.print_level = 1
+ocp.solver_options.nlp_solver_type = 'SQP'  # SQP_RTI, SQP
+
+# set prediction horizon
+ocp.solver_options.tf = Tf_01
+
+print(80*'-')
+print('generate code and compile...')
+ocp_solver = AcadosOcpSolver(ocp, json_file='acados_ocp.json')
+
+# --------------------------------------------------------------------------------
+# 0) solve the problem defined here (original from code export), analog to 'minimal_example_ocp.py'
+
+simX0 = np.ndarray((N0 + 1, nx))
+simU0 = np.ndarray((N0, nu))
+
+print(80*'-')
+print(f'solve original code with N = {N0} and Tf = {Tf_01} s:')
+status = ocp_solver.solve()
+
+if status != 0:
+    ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+    raise Exception('acados returned status {}. Exiting.'.format(status))
+
+# get solution
+for i in range(N0):
+    simX0[i, :] = ocp_solver.get(i, "x")
+    simU0[i, :] = ocp_solver.get(i, "u")
+simX0[N0, :] = ocp_solver.get(N0, "x")
+
+ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+
+# plot but don't halt
+plot_pendulum(np.linspace(0, Tf_01, N0 + 1), Fmax, simU0, simX0, latexify=False, plt_show=False, X_true_label=f'original: N={N0}, Tf={Tf_01}')
+
+# --------------------------------------------------------------------------------
+# 1) now reuse the code but set a new time-steps vector, with a new number of elements
+dt1 = Tf_01 / N12
+
+new_time_steps1 = np.tile(dt1, (N12,))  # Matlab's equivalent to repmat
+time1 = np.hstack([0, np.cumsum(new_time_steps1)])
+
+simX1 = np.ndarray((N12 + 1, nx))
+simU1 = np.ndarray((N12, nu))
+
+ocp_solver.set_new_time_steps(new_time_steps1)
+print(80*'-')
+print(f'solve use-case 1 with N = {N12} (instead of {N0}) and Tf = {Tf_01} s:')
+status = ocp_solver.solve()
+
+if status != 0:
+    ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+    raise Exception('acados returned status {}. Exiting.'.format(status))
+
+# get solution
+for i in range(N12):
+    simX1[i, :] = ocp_solver.get(i, "x")
+    simU1[i, :] = ocp_solver.get(i, "u")
+simX1[N12, :] = ocp_solver.get(N12, "x")
+
+ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+
+plot_pendulum(time1, Fmax, simU1, simX1, latexify=False, plt_show=False, X_true_label=f'use-case 1: N={N12}')
+
+# --------------------------------------------------------------------------------
+# 2) reuse the code again, set a new time-steps vector, only with a different final time
+dt2 = Tf_2 / N12
+
+new_time_steps2 = np.tile(dt2, (N12,))  # Matlab's equivalent to repmat
+time2 = np.hstack([0, np.cumsum(new_time_steps2)])
+
+simX2 = np.ndarray((N12 + 1, nx))
+simU2 = np.ndarray((N12, nu))
+
+ocp_solver.set_new_time_steps(new_time_steps2)
+print(80*'-')
+print(f'solve use-case 2 with N = {N12} and Tf = {Tf_2} s (instead of {Tf_01} s):')
+status = ocp_solver.solve()
+
+if status != 0:
+    ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+    raise Exception('acados returned status {}. Exiting.'.format(status))
+
+# get solution
+for i in range(N12):
+    simX2[i, :] = ocp_solver.get(i, "x")
+    simU2[i, :] = ocp_solver.get(i, "u")
+simX2[N12, :] = ocp_solver.get(N12, "x")
+
+ocp_solver.print_statistics()  # encapsulates: stat = ocp_solver.get_stats("statistics")
+
+plot_pendulum(time2, Fmax, simU2, simX2, latexify=False, plt_show=True, X_true_label=f'use-case 2: Tf={Tf_2} s')
+

interfaces/CMakeLists.txt
@@ -141,6 +141,9 @@ add_test(NAME python_test_ocp
 add_test(NAME python_pendulum_ocp_example
         COMMAND "${CMAKE_COMMAND}" -E chdir ${PROJECT_SOURCE_DIR}/examples/acados_python/getting_started/ocp
         python minimal_example_ocp.py)
+add_test(NAME python_pendulum_ocp_example_reuse_code
+        COMMAND "${CMAKE_COMMAND}" -E chdir ${PROJECT_SOURCE_DIR}/examples/acados_python/getting_started/ocp
+        python minimal_example_ocp_reuse_code.py)
 add_test(NAME python_nonuniform_discretization_ocp_example
         COMMAND "${CMAKE_COMMAND}" -E chdir ${PROJECT_SOURCE_DIR}/examples/acados_python/getting_started/ocp
         python nonuniform_discretization_example.py)

interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.c
@@ -78,9 +78,10 @@ int {{ model.name }}_acados_sim_solver_free_capsule(sim_solver_capsule * capsule
 int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
 {
     // initialize
-    int nx = {{ dims.nx }};
-    int nu = {{ dims.nu }};
-    int nz = {{ dims.nz }};
+    const int nx = {{ model.name | upper }}_NX;
+    const int nu = {{ model.name | upper }}_NU;
+    const int nz = {{ model.name | upper }}_NZ;
+    const int np = {{ model.name | upper }}_NP;
     bool tmp_bool;
 
     {#// double Tsim = {{ solver_options.tf / dims.N }};#}
@@ -98,7 +99,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_impl_dae_fun->casadi_sparsity_out = &{{ model.name }}_impl_dae_fun_sparsity_out;
     capsule->sim_impl_dae_fun->casadi_n_in = &{{ model.name }}_impl_dae_fun_n_in;
     capsule->sim_impl_dae_fun->casadi_n_out = &{{ model.name }}_impl_dae_fun_n_out;
-    external_function_param_casadi_create(capsule->sim_impl_dae_fun, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_impl_dae_fun, np);
 
     capsule->sim_impl_dae_fun_jac_x_xdot_z->casadi_fun = &{{ model.name }}_impl_dae_fun_jac_x_xdot_z;
     capsule->sim_impl_dae_fun_jac_x_xdot_z->casadi_work = &{{ model.name }}_impl_dae_fun_jac_x_xdot_z_work;
@@ -106,7 +107,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_impl_dae_fun_jac_x_xdot_z->casadi_sparsity_out = &{{ model.name }}_impl_dae_fun_jac_x_xdot_z_sparsity_out;
     capsule->sim_impl_dae_fun_jac_x_xdot_z->casadi_n_in = &{{ model.name }}_impl_dae_fun_jac_x_xdot_z_n_in;
     capsule->sim_impl_dae_fun_jac_x_xdot_z->casadi_n_out = &{{ model.name }}_impl_dae_fun_jac_x_xdot_z_n_out;
-    external_function_param_casadi_create(capsule->sim_impl_dae_fun_jac_x_xdot_z, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_impl_dae_fun_jac_x_xdot_z, np);
 
     // external_function_param_casadi impl_dae_jac_x_xdot_u_z;
     capsule->sim_impl_dae_jac_x_xdot_u_z->casadi_fun = &{{ model.name }}_impl_dae_jac_x_xdot_u_z;
@@ -115,7 +116,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_impl_dae_jac_x_xdot_u_z->casadi_sparsity_out = &{{ model.name }}_impl_dae_jac_x_xdot_u_z_sparsity_out;
     capsule->sim_impl_dae_jac_x_xdot_u_z->casadi_n_in = &{{ model.name }}_impl_dae_jac_x_xdot_u_z_n_in;
     capsule->sim_impl_dae_jac_x_xdot_u_z->casadi_n_out = &{{ model.name }}_impl_dae_jac_x_xdot_u_z_n_out;
-    external_function_param_casadi_create(capsule->sim_impl_dae_jac_x_xdot_u_z, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_impl_dae_jac_x_xdot_u_z, np);
 
 {%- if hessian_approx == "EXACT" %}
     capsule->sim_impl_dae_hess = (external_function_param_casadi *) malloc(sizeof(external_function_param_casadi));
@@ -126,7 +127,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_impl_dae_hess->casadi_sparsity_out = &{{ model.name }}_impl_dae_hess_sparsity_out;
     capsule->sim_impl_dae_hess->casadi_n_in = &{{ model.name }}_impl_dae_hess_n_in;
     capsule->sim_impl_dae_hess->casadi_n_out = &{{ model.name }}_impl_dae_hess_n_out;
-    external_function_param_casadi_create(capsule->sim_impl_dae_hess, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_impl_dae_hess, np);
 {%- endif %}
 
     {% elif solver_options.integrator_type == "ERK" %}
@@ -140,7 +141,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_forw_vde_casadi->casadi_sparsity_in = &{{ model.name }}_expl_vde_forw_sparsity_in;
     capsule->sim_forw_vde_casadi->casadi_sparsity_out = &{{ model.name }}_expl_vde_forw_sparsity_out;
     capsule->sim_forw_vde_casadi->casadi_work = &{{ model.name }}_expl_vde_forw_work;
-    external_function_param_casadi_create(capsule->sim_forw_vde_casadi, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_forw_vde_casadi, np);
 
     capsule->sim_expl_ode_fun_casadi->casadi_fun = &{{ model.name }}_expl_ode_fun;
     capsule->sim_expl_ode_fun_casadi->casadi_n_in = &{{ model.name }}_expl_ode_fun_n_in;
@@ -148,7 +149,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_expl_ode_fun_casadi->casadi_sparsity_in = &{{ model.name }}_expl_ode_fun_sparsity_in;
     capsule->sim_expl_ode_fun_casadi->casadi_sparsity_out = &{{ model.name }}_expl_ode_fun_sparsity_out;
     capsule->sim_expl_ode_fun_casadi->casadi_work = &{{ model.name }}_expl_ode_fun_work;
-    external_function_param_casadi_create(capsule->sim_expl_ode_fun_casadi, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_expl_ode_fun_casadi, np);
 
 {%- if hessian_approx == "EXACT" %}
     capsule->sim_expl_ode_hess = (external_function_param_casadi *) malloc(sizeof(external_function_param_casadi));
@@ -159,7 +160,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_expl_ode_hess->casadi_sparsity_out = &{{ model.name }}_expl_ode_hess_sparsity_out;
     capsule->sim_expl_ode_hess->casadi_n_in = &{{ model.name }}_expl_ode_hess_n_in;
     capsule->sim_expl_ode_hess->casadi_n_out = &{{ model.name }}_expl_ode_hess_n_out;
-    external_function_param_casadi_create(capsule->sim_expl_ode_hess, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_expl_ode_hess, np);
 {%- endif %}
 
     {% elif solver_options.integrator_type == "GNSF" -%}
@@ -175,7 +176,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_gnsf_phi_fun->casadi_sparsity_in = &{{ model.name }}_gnsf_phi_fun_sparsity_in;
     capsule->sim_gnsf_phi_fun->casadi_sparsity_out = &{{ model.name }}_gnsf_phi_fun_sparsity_out;
     capsule->sim_gnsf_phi_fun->casadi_work = &{{ model.name }}_gnsf_phi_fun_work;
-    external_function_param_casadi_create(capsule->sim_gnsf_phi_fun, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_gnsf_phi_fun, np);
 
     capsule->sim_gnsf_phi_fun_jac_y->casadi_fun = &{{ model.name }}_gnsf_phi_fun_jac_y;
     capsule->sim_gnsf_phi_fun_jac_y->casadi_n_in = &{{ model.name }}_gnsf_phi_fun_jac_y_n_in;
@@ -183,7 +184,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_gnsf_phi_fun_jac_y->casadi_sparsity_in = &{{ model.name }}_gnsf_phi_fun_jac_y_sparsity_in;
     capsule->sim_gnsf_phi_fun_jac_y->casadi_sparsity_out = &{{ model.name }}_gnsf_phi_fun_jac_y_sparsity_out;
     capsule->sim_gnsf_phi_fun_jac_y->casadi_work = &{{ model.name }}_gnsf_phi_fun_jac_y_work;
-    external_function_param_casadi_create(capsule->sim_gnsf_phi_fun_jac_y, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_gnsf_phi_fun_jac_y, np);
 
     capsule->sim_gnsf_phi_jac_y_uhat->casadi_fun = &{{ model.name }}_gnsf_phi_jac_y_uhat;
     capsule->sim_gnsf_phi_jac_y_uhat->casadi_n_in = &{{ model.name }}_gnsf_phi_jac_y_uhat_n_in;
@@ -191,7 +192,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_gnsf_phi_jac_y_uhat->casadi_sparsity_in = &{{ model.name }}_gnsf_phi_jac_y_uhat_sparsity_in;
     capsule->sim_gnsf_phi_jac_y_uhat->casadi_sparsity_out = &{{ model.name }}_gnsf_phi_jac_y_uhat_sparsity_out;
     capsule->sim_gnsf_phi_jac_y_uhat->casadi_work = &{{ model.name }}_gnsf_phi_jac_y_uhat_work;
-    external_function_param_casadi_create(capsule->sim_gnsf_phi_jac_y_uhat, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_gnsf_phi_jac_y_uhat, np);
 
     capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z->casadi_fun = &{{ model.name }}_gnsf_f_lo_fun_jac_x1k1uz;
     capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z->casadi_n_in = &{{ model.name }}_gnsf_f_lo_fun_jac_x1k1uz_n_in;
@@ -199,7 +200,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z->casadi_sparsity_in = &{{ model.name }}_gnsf_f_lo_fun_jac_x1k1uz_sparsity_in;
     capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z->casadi_sparsity_out = &{{ model.name }}_gnsf_f_lo_fun_jac_x1k1uz_sparsity_out;
     capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z->casadi_work = &{{ model.name }}_gnsf_f_lo_fun_jac_x1k1uz_work;
-    external_function_param_casadi_create(capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_gnsf_f_lo_jac_x1_x1dot_u_z, np);
 
     capsule->sim_gnsf_get_matrices_fun->casadi_fun = &{{ model.name }}_gnsf_get_matrices_fun;
     capsule->sim_gnsf_get_matrices_fun->casadi_n_in = &{{ model.name }}_gnsf_get_matrices_fun_n_in;
@@ -207,7 +208,7 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
     capsule->sim_gnsf_get_matrices_fun->casadi_sparsity_in = &{{ model.name }}_gnsf_get_matrices_fun_sparsity_in;
     capsule->sim_gnsf_get_matrices_fun->casadi_sparsity_out = &{{ model.name }}_gnsf_get_matrices_fun_sparsity_out;
     capsule->sim_gnsf_get_matrices_fun->casadi_work = &{{ model.name }}_gnsf_get_matrices_fun_work;
-    external_function_param_casadi_create(capsule->sim_gnsf_get_matrices_fun, {{ dims.np }});
+    external_function_param_casadi_create(capsule->sim_gnsf_get_matrices_fun, np);
     {% endif %}
 
     // sim plan & config
@@ -325,14 +326,14 @@ int {{ model.name }}_acados_sim_create(sim_solver_capsule * capsule)
 
 {% if dims.np > 0 %}
     /* initialize parameter values */
-    double* p = calloc({{ dims.np }}, sizeof(double));
+    double* p = calloc(np, sizeof(double));
     {% for item in parameter_values %}
         {%- if item != 0 %}
     p[{{ loop.index0 }}] = {{ item }};
         {%- endif %}
     {%- endfor %}
 
-    {{ model.name }}_acados_sim_update_params(capsule, p, {{ dims.np }});
+    {{ model.name }}_acados_sim_update_params(capsule, p, np);
     free(p);
 {% endif %}{# if dims.np #}
 
@@ -422,7 +423,7 @@ int {{ model.name }}_acados_sim_free(sim_solver_capsule *capsule)
 int {{ model.name }}_acados_sim_update_params(sim_solver_capsule *capsule, double *p, int np)
 {
     int status = 0;
-    int casadi_np = {{ dims.np }};
+    int casadi_np = {{ model.name | upper }}_NP;
 
     if (casadi_np != np) {
         printf("{{ model.name }}_acados_sim_update_params: trying to set %i parameters for external functions."

interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.h
@@ -37,6 +37,11 @@
 #include "acados_c/sim_interface.h"
 #include "acados_c/external_function_interface.h"
 
+#define {{ model.name | upper }}_NX     {{ dims.nx }}
+#define {{ model.name | upper }}_NZ     {{ dims.nz }}
+#define {{ model.name | upper }}_NU     {{ dims.nu }}
+#define {{ model.name | upper }}_NP     {{ dims.np }}
+
 #ifdef __cplusplus
 extern "C" {
 #endif

commit 43a4c3eedfc0727c63548e49c33f4b99c1ba2b12

Python: fix time_step detection bug introduced in https://github.com/acados/acados/pull/747 commit 05398550e9c6f766a2c30356c35cc3a12df747fd

interfaces/acados_template/acados_template/acados_ocp_solver.py
@@ -436,10 +436,9 @@ def make_ocp_dims_consistent(acados_ocp):
 
         time_steps = opts.shooting_nodes[1:] - opts.shooting_nodes[0:-1]
         # identify constant time-steps: due to numerical reasons the content of time_steps might vary a bit
-        delta_time_steps = time_steps[1:] - time_steps[0:-1]
         avg_time_steps = np.average(time_steps)
-        # criterion for constant time-step detection: the min/max difference in values normalized by the average
-        check_const_time_step = np.max(delta_time_steps)-np.min(delta_time_steps) / avg_time_steps
+        # criterion for constant time step detection: the min/max difference in values normalized by the average
+        check_const_time_step = (np.max(time_steps)-np.min(time_steps)) / avg_time_steps
         # if the criterion is small, we have a constant time-step
         if check_const_time_step < 1e-9:
             time_steps[:] = avg_time_steps  # if we have a constant time-step: apply the average time-step