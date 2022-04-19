d9ad056837bc0aba1d53f97f17f3ff1cbc97c9c0
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-04-19 12:30:48 +0200

    DS1401 Toolchain: set prefixes, suffixes, include flags, remove definitions

--- cmake/Toolchain-dSpaceDS1401.cmake
+++ cmake/Toolchain-dSpaceDS1401.cmake
@@ -39,3 +39,25 @@ set(BLASFEO_EXAMPLES OFF CACHE BOOL "Examples disabled")
 set(EXT_DEP OFF CACHE BOOL "Compile external dependencies in BLASFEO")
 set(ACADOS_INSTALL_DIR "install" CACHE PATH  "Installation path to ACADOS_INSTALL_DIR")
 
+
+# import from platform folder
+set(CMAKE_IMPORT_LIBRARY_PREFIX "")
+set(CMAKE_SHARED_LIBRARY_PREFIX "")
+set(CMAKE_SHARED_MODULE_PREFIX  "")
+set(CMAKE_STATIC_LIBRARY_PREFIX "")
+
+set(CMAKE_EXECUTABLE_SUFFIX     ".exe")
+set(CMAKE_IMPORT_LIBRARY_SUFFIX ".lib")
+set(CMAKE_SHARED_LIBRARY_SUFFIX ".lib")
+set(CMAKE_SHARED_MODULE_SUFFIX  ".lib")
+set(CMAKE_STATIC_LIBRARY_SUFFIX ".lib")
+
+# set(CMAKE_C_FLAGS "\"-J${DSPACE_RTLIB}\"")
+
+set(CMAKE_INCLUDE_FLAG_C "-J")
+set(CMAKE_INCLUDE_FLAG_CXX "-J")
+
+# add_definitions(-D__MABX2__)
+remove_definitions(-DLINUX)
+remove_definitions(-DOS_WINDOWS)
+remove_definitions(-D__LINUX__)
