acados/acados

commit e61abe63e5fb486f1473e569974ecaa6d634a762

Mex interface improvements (#848)
* MEX: fix slack setter

* C: fix error msg

* MEX: add semicolons to reduce output

* Matlab: test slack setter

* make Sfunction: add try and deactivate verbose mode

* C: remove unnecessary includes in sim_gnsf.h

interfaces/acados_template/acados_template/c_templates_tera/make_sfun.in.m
@@ -161,9 +161,18 @@ LIBS{end+1} = '-ldaqp';
     {% endif %}
 {%- endif %}
 
-mex('-v', '-O', CFLAGS, LDFLAGS, COMPFLAGS, COMPDEFINES, INCS{:}, ...
-    LIB_PATH, LIBS{:}, SOURCES{:}, ...
-    '-output', 'acados_solver_sfunction_{{ model.name }}' );
+
+try
+    %     mex('-v', '-O', CFLAGS, LDFLAGS, COMPFLAGS, COMPDEFINES, INCS{:}, ...
+    mex('-O', CFLAGS, LDFLAGS, COMPFLAGS, COMPDEFINES, INCS{:}, ...
+            LIB_PATH, LIBS{:}, SOURCES{:}, ...
+            '-output', 'acados_solver_sfunction_pendulum' );
+catch exception
+    disp('make_sfun failed with the following exception:')
+    disp(exception);
+    disp('Try adding -v to the mex command above to get more information.')
+    keyboard
+end
 
 fprintf( [ '\n\nSuccessfully created sfunction:\nacados_solver_sfunction_{{ model.name }}', '.', ...
     eval('mexext')] );

acados/sim/sim_gnsf.h
@@ -46,12 +46,12 @@ extern "C" {
 #include "acados/sim/sim_common.h"
 
 #include "blasfeo/include/blasfeo_common.h"
-#include "blasfeo/include/blasfeo_d_aux.h"
-#include "blasfeo/include/blasfeo_d_aux_ext_dep.h"
-#include "blasfeo/include/blasfeo_d_blas.h"
-#include "blasfeo/include/blasfeo_d_kernel.h"
-#include "blasfeo/include/blasfeo_i_aux_ext_dep.h"
-#include "blasfeo/include/blasfeo_target.h"
+// #include "blasfeo/include/blasfeo_d_aux.h"
+// #include "blasfeo/include/blasfeo_d_aux_ext_dep.h"
+// #include "blasfeo/include/blasfeo_d_blas.h"
+// #include "blasfeo/include/blasfeo_d_kernel.h"
+// #include "blasfeo/include/blasfeo_i_aux_ext_dep.h"
+// #include "blasfeo/include/blasfeo_target.h"
 
 /* 
 GNSF - Generalized Nonlinear Static Feedback Model

examples/acados_matlab_octave/test/test_ocp_wtnx6.m
@@ -82,7 +82,7 @@ ocp_qp_solver_warm_start = 2;
 ocp_sim_method = 'irk';
 ocp_sim_method_num_stages = 4 * ones(ocp_N, 1); % scalar or vector of size ocp_N;
 ocp_sim_method_num_steps = 1 * ones(ocp_N, 1); % scalar or vector of size ocp_N;
-ocp_sim_method_newton_iter = 3% * ones(ocp_N, 1); % scalar or vector of size ocp_N;
+ocp_sim_method_newton_iter = 3; % * ones(ocp_N, 1); % scalar or vector of size ocp_N;
 %cost_type = 'linear_ls';
 cost_type = 'nonlinear_ls';
 
@@ -477,10 +477,14 @@ ocp.set('cost_zl', ones(2,1), N-1)
 for i = 0:N-1
     sl = ocp.get('sl', i);
     su = ocp.get('su', i);
+    % test setters
+    ocp.set('sl', sl, i);
+    ocp.set('su', su, i);
     t = ocp.get('t', i);
 end
 sl = ocp.get('sl', N);
 su = ocp.get('su', N);
+
 
 
 electrical_power = 0.944*97/100*x_sim(1,:).*x_sim(6,:);

interfaces/acados_c/ocp_nlp_interface.c
@@ -1088,8 +1088,7 @@ void ocp_nlp_set(ocp_nlp_config *config, ocp_nlp_solver *solver,
     }
     else
     {
-        printf("\nerror: ocp_nlp_mem_set: field %s not available\n", field);
+        printf("\nerror: ocp_nlp_set: field %s not available\n", field);
         exit(1);
     }
-    // printf("exit ocp_nlp_set\n");
 }

interfaces/acados_matlab_octave/acados_ocp.m
@@ -234,7 +234,7 @@ classdef acados_ocp < handle
             % set up acados_ocp_nlp_json
             obj.acados_ocp_nlp_json = set_up_acados_ocp_nlp_json(obj, simulink_opts);
             % render templated code
-            ocp_generate_c_code(obj)
+            ocp_generate_c_code(obj);
         end
 
 

interfaces/acados_matlab_octave/acados_sim_opts.m
@@ -50,7 +50,7 @@ classdef acados_sim_opts < handle
             obj.opts_struct.codgen_model = 'true';
             obj.opts_struct.compile_model = 'true';
             obj.opts_struct.method = 'irk';
-            obj.opts_struct.collocation_type = 'gauss_legendre'
+            obj.opts_struct.collocation_type = 'gauss_legendre';
             obj.opts_struct.num_stages = 4;
             obj.opts_struct.num_steps = 1;
             obj.opts_struct.newton_iter = 3;
@@ -75,7 +75,7 @@ classdef acados_sim_opts < handle
             if (strcmp(field, 'compile_interface'))
                 obj.opts_struct.compile_interface = value;
             elseif (strcmp(field, 'ext_fun_compile_flags'))
-                obj.opts_struct.ext_fun_compile_flags = value
+                obj.opts_struct.ext_fun_compile_flags = value;
             elseif (strcmp(field, 'codgen_model'))
                 obj.opts_struct.codgen_model = value;
             elseif (strcmp(field, 'compile_model'))

interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/acados_ocp_nlp_json.m
@@ -62,9 +62,9 @@ classdef acados_ocp_nlp_json < handle
             obj.simulink_opts = simulink_opts;
             obj.cython_include_dirs = '';
             obj.json_file = 'acados_ocp_nlp.json';
-            obj.shared_lib_ext = '.so'
+            obj.shared_lib_ext = '.so';
             if ismac()
-                obj.shared_lib_ext = '.dylib'
+                obj.shared_lib_ext = '.dylib';
             end
             % obj.code_export_directory;
         end

interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/ocp_nlp_solver_options_json.m
@@ -121,7 +121,7 @@ classdef ocp_nlp_solver_options_json < handle
             obj.eps_sufficient_descent = 1e-4;
             obj.hpipm_mode = 'BALANCE';
             obj.nlp_solver_ext_qp_res = 0;
-            obj.ext_fun_compile_flags = '-O2'
+            obj.ext_fun_compile_flags = '-O2';
         end
     end
 end

commit 630ec1f1cbaca8ec1ee3797a37b37dac14c04570

Simulink: fix bug introduced in e61abe63e

interfaces/acados_template/acados_template/c_templates_tera/make_sfun.in.m
@@ -166,7 +166,7 @@ try
     %     mex('-v', '-O', CFLAGS, LDFLAGS, COMPFLAGS, COMPDEFINES, INCS{:}, ...
     mex('-O', CFLAGS, LDFLAGS, COMPFLAGS, COMPDEFINES, INCS{:}, ...
             LIB_PATH, LIBS{:}, SOURCES{:}, ...
-            '-output', 'acados_solver_sfunction_pendulum' );
+            '-output', 'acados_solver_sfunction_{{ model.name }}' );
 catch exception
     disp('make_sfun failed with the following exception:')
     disp(exception);