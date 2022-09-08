77ae3392d08915a0e0c818dab6b1a243764ef542
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-09-08 20:32:13 +0200

    Python: test ocp_solver.get_from_qp_in

--- examples/acados_python/getting_started/ocp/example_sqp_rti_loop.py
+++ examples/acados_python/getting_started/ocp/example_sqp_rti_loop.py
@@ -126,11 +126,39 @@ simX[N,:] = ocp_solver.get(N, "x")
 
 ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
 
-# 
+#
 cost = ocp_solver.get_cost()
 print("cost function value of solution = ", cost)
 
-A = ocp_solver.dynamics_get(0, "A")
-print("forward sensitivities A at stage 0:", A)
+PRINT_QP = True
+if PRINT_QP:
+    for i in range(N):
+        A_qp = ocp_solver.get_from_qp_in(i, "A")
+        print(f"qp: A at stage {i}: {A_qp}")
 
+    for i in range(N):
+        B_qp = ocp_solver.get_from_qp_in(i, "B")
+        print(f"qp: B at stage {i}: {B_qp}")
+
+    for i in range(N+1):
+        Q_qp = ocp_solver.get_from_qp_in(i, "Q")
+        print(f"qp: Q at stage {i}: {Q_qp}")
+
+    for i in range(N+1):
+        R_qp = ocp_solver.get_from_qp_in(i, "R")
+        print(f"qp: R at stage {i}: {R_qp}")
+
+    for i in range(N+1):
+        S_qp = ocp_solver.get_from_qp_in(i, "S")
+        print(f"qp: S at stage {i}: {S_qp}")
+
+    for i in range(N+1):
+        r_qp = ocp_solver.get_from_qp_in(i, "r")
+        print(f"qp: r at stage {i}: {r_qp}")
+
+    for i in range(N+1):
+        q_qp = ocp_solver.get_from_qp_in(i, "q")
+        print(f"qp: q at stage {i}: {q_qp}")
+
+# plot
 plot_pendulum(np.linspace(0, Tf, N+1), Fmax, simU, simX, latexify=False)
