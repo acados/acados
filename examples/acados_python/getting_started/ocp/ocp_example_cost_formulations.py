5379fc5e992d959121cf126f9b26400e07ef6bfb
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-11-04 16:03:28 +0100

    add main in example

--- examples/acados_python/getting_started/ocp/ocp_example_cost_formulations.py
+++ examples/acados_python/getting_started/ocp/ocp_example_cost_formulations.py
@@ -42,161 +42,172 @@ import scipy.linalg
 from utils import plot_pendulum
 from casadi import vertcat
 
-COST_MODULE = 'LS_Z' # 'LS', 'EXTERNAL', 'NLS', 'NLS_Z'
+COST_VERSIONS = ['LS', 'EXTERNAL', 'NLS', 'NLS_Z', 'LS_Z']
 HESSIAN_APPROXIMATION = 'GAUSS_NEWTON' # 'GAUSS_NEWTON
 EXTERNAL_COST_USE_NUM_HESS = 1
 
-# create ocp object to formulate the OCP
-ocp = AcadosOcp()
+def main(cost_version: str):
+    # create ocp object to formulate the OCP
+    ocp = AcadosOcp()
 
-if COST_MODULE in ['NLS_Z', 'LS_Z']:
-    model = export_augmented_pendulum_model()
-    nz = model.z.size()[0]
-else:
-    model = export_pendulum_ode_model()
-# set model
-ocp.model = model
+    if cost_version in ['NLS_Z', 'LS_Z']:
+        model = export_augmented_pendulum_model()
+        nz = model.z.size()[0]
+    else:
+        model = export_pendulum_ode_model()
 
-Tf = 1.0
-nx = model.x.size()[0]
-nu = model.u.size()[0]
-ny = nx + nu
-ny_e = nx
-N = 20
+    model.name += cost_version
 
-ocp.dims.N = N
+    # set model
+    ocp.model = model
 
-# set cost
-Q = 2*np.diag([1e3, 1e3, 1e-2, 1e-2])
-R = 2*np.diag([1e-2])
+    Tf = 1.0
+    nx = model.x.size()[0]
+    nu = model.u.size()[0]
+    ny = nx + nu
+    ny_e = nx
+    N = 20
 
-x = ocp.model.x
-u = ocp.model.u
+    ocp.dims.N = N
 
-cost_W = scipy.linalg.block_diag(Q, R)
+    # set cost
+    Q = 2*np.diag([1e3, 1e3, 1e-2, 1e-2])
+    R = 2*np.diag([1e-2])
 
-if COST_MODULE == 'LS':
-    ocp.cost.cost_type = 'LINEAR_LS'
-    ocp.cost.cost_type_e = 'LINEAR_LS'
+    x = ocp.model.x
+    u = ocp.model.u
 
-    ocp.cost.Vx = np.zeros((ny, nx))
-    ocp.cost.Vx[:nx,:nx] = np.eye(nx)
+    cost_W = scipy.linalg.block_diag(Q, R)
 
-    Vu = np.zeros((ny, nu))
-    Vu[4,0] = 1.0
-    ocp.cost.Vu = Vu
+    if cost_version == 'LS':
+        ocp.cost.cost_type = 'LINEAR_LS'
+        ocp.cost.cost_type_e = 'LINEAR_LS'
 
-    ocp.cost.Vx_e = np.eye(nx)
+        ocp.cost.Vx = np.zeros((ny, nx))
+        ocp.cost.Vx[:nx,:nx] = np.eye(nx)
 
-elif COST_MODULE == 'LS_Z':
-    ocp.cost.cost_type = 'LINEAR_LS'
-    ocp.cost.cost_type_e = 'LINEAR_LS'
+        Vu = np.zeros((ny, nu))
+        Vu[4,0] = 1.0
+        ocp.cost.Vu = Vu
 
-    ocp.cost.Vx = np.zeros((ny, nx))
-    ocp.cost.Vx[:nx,:nx] = np.eye(nx)
-    ocp.cost.Vx[0, 0] = 0.0
+        ocp.cost.Vx_e = np.eye(nx)
 
-    ocp.cost.Vz = np.zeros((ny, nz))
-    ocp.cost.Vz[0, 0] = 1.0
+    elif cost_version == 'LS_Z':
+        ocp.cost.cost_type = 'LINEAR_LS'
+        ocp.cost.cost_type_e = 'LINEAR_LS'
 
-    Vu = np.zeros((ny, nu))
-    Vu[4,0] = 1.0
-    ocp.cost.Vu = Vu
-    ocp.cost.Vx_e = np.eye(nx)
+        ocp.cost.Vx = np.zeros((ny, nx))
+        ocp.cost.Vx[:nx,:nx] = np.eye(nx)
+        ocp.cost.Vx[0, 0] = 0.0
 
-elif COST_MODULE == 'NLS':
-    ocp.cost.cost_type = 'NONLINEAR_LS'
-    ocp.cost.cost_type_e = 'NONLINEAR_LS'
+        ocp.cost.Vz = np.zeros((ny, nz))
+        ocp.cost.Vz[0, 0] = 1.0
 
-    ocp.model.cost_y_expr = vertcat(x, u)
-    ocp.model.cost_y_expr_e = x
+        Vu = np.zeros((ny, nu))
+        Vu[4,0] = 1.0
+        ocp.cost.Vu = Vu
+        ocp.cost.Vx_e = np.eye(nx)
 
-elif COST_MODULE == 'NLS_Z':
-    ocp.cost.cost_type = 'NONLINEAR_LS'
-    ocp.cost.cost_type_e = 'NONLINEAR_LS'
+    elif cost_version == 'NLS':
+        ocp.cost.cost_type = 'NONLINEAR_LS'
+        ocp.cost.cost_type_e = 'NONLINEAR_LS'
 
-    y_expr_z = vertcat(model.z[0], model.x[1:], u)
-    print(f"{y_expr_z=}")
-    ocp.model.cost_y_expr = y_expr_z
-    ocp.model.cost_y_expr_e = x
+        ocp.model.cost_y_expr = vertcat(x, u)
+        ocp.model.cost_y_expr_e = x
 
-elif COST_MODULE == 'EXTERNAL':
-    ocp.cost.cost_type = 'EXTERNAL'
-    ocp.cost.cost_type_e = 'EXTERNAL'
+    elif cost_version == 'NLS_Z':
+        ocp.cost.cost_type = 'NONLINEAR_LS'
+        ocp.cost.cost_type_e = 'NONLINEAR_LS'
 
-    ocp.model.cost_expr_ext_cost = vertcat(x, u).T @ cost_W @ vertcat(x, u)
-    ocp.model.cost_expr_ext_cost_e = x.T @ Q @ x
+        y_expr_z = vertcat(model.z[0], model.x[1:], u)
+        print(f"{y_expr_z=}")
+        ocp.model.cost_y_expr = y_expr_z
+        ocp.model.cost_y_expr_e = x
 
-else:
-    raise Exception('Unknown COST_MODULE. Possible values are \'LS\' and \'NLS\'.')
+    elif cost_version == 'EXTERNAL':
+        ocp.cost.cost_type = 'EXTERNAL'
+        ocp.cost.cost_type_e = 'EXTERNAL'
 
-if COST_MODULE in ['LS', 'NLS', 'NLS_Z', 'LS_Z']:
-    ocp.cost.yref = np.zeros((ny, ))
-    ocp.cost.yref_e = np.zeros((ny_e, ))
-    ocp.cost.W_e = Q
-    ocp.cost.W = cost_W
+        ocp.model.cost_expr_ext_cost = vertcat(x, u).T @ cost_W @ vertcat(x, u)
+        ocp.model.cost_expr_ext_cost_e = x.T @ Q @ x
 
-# set constraints
-Fmax = 80
-ocp.constraints.constr_type = 'BGH'
-ocp.constraints.lbu = np.array([-Fmax])
-ocp.constraints.ubu = np.array([+Fmax])
-ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
-ocp.constraints.idxbu = np.array([0])
+    else:
+        raise Exception('Unknown cost_version. Possible values are \'LS\' and \'NLS\'.')
 
-ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
-ocp.solver_options.hessian_approx = HESSIAN_APPROXIMATION
-ocp.solver_options.regularize_method = 'CONVEXIFY'
-ocp.solver_options.integrator_type = 'IRK'
+    if cost_version in ['LS', 'NLS', 'NLS_Z', 'LS_Z']:
+        ocp.cost.yref = np.zeros((ny, ))
+        ocp.cost.yref_e = np.zeros((ny_e, ))
+        ocp.cost.W_e = Q
+        ocp.cost.W = cost_W
 
-ocp.solver_options.qp_solver_cond_N = 5
+    # set constraints
+    Fmax = 80
+    ocp.constraints.constr_type = 'BGH'
+    ocp.constraints.lbu = np.array([-Fmax])
+    ocp.constraints.ubu = np.array([+Fmax])
+    ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
+    ocp.constraints.idxbu = np.array([0])
 
-# set prediction horizon
-ocp.solver_options.tf = Tf
-ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI
-ocp.solver_options.ext_cost_num_hess = EXTERNAL_COST_USE_NUM_HESS
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
+    ocp.solver_options.hessian_approx = HESSIAN_APPROXIMATION
+    ocp.solver_options.regularize_method = 'CONVEXIFY'
+    ocp.solver_options.integrator_type = 'IRK'
 
-ocp_solver = AcadosOcpSolver(ocp, json_file = 'acados_ocp.json')
+    ocp.solver_options.qp_solver_cond_N = 5
 
-# from casadi import jacobian
-# ux = vertcat(ocp.model.u, ocp.model.x)
-# jacobian(jacobian(ocp.model.cost_expr_ext_cost, ux), ux)
-# SX(@1=0.04, @2=4000,
-# [[@1, 00, 00, 00, 00],
-#  [00, @2, 00, 00, 00],
-#  [00, 00, @2, 00, 00],
-#  [00, 00, 00, @1, 00],
-#  [00, 00, 00, 00, @1]])
+    # set prediction horizon
+    ocp.solver_options.tf = Tf
+    ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI
+    if cost_version == 'EXTERNAL':
+        ocp.solver_options.ext_cost_num_hess = EXTERNAL_COST_USE_NUM_HESS
 
-# NOTE: hessian is wrt [u,x]
-if EXTERNAL_COST_USE_NUM_HESS and COST_MODULE == 'EXTERNAL':
+    ocp_solver = AcadosOcpSolver(ocp, json_file = 'acados_ocp.json')
+
+    # from casadi import jacobian
+    # ux = vertcat(ocp.model.u, ocp.model.x)
+    # jacobian(jacobian(ocp.model.cost_expr_ext_cost, ux), ux)
+    # SX(@1=0.04, @2=4000,
+    # [[@1, 00, 00, 00, 00],
+    #  [00, @2, 00, 00, 00],
+    #  [00, 00, @2, 00, 00],
+    #  [00, 00, 00, @1, 00],
+    #  [00, 00, 00, 00, @1]])
+
+    # NOTE: hessian is wrt [u,x]
+    if EXTERNAL_COST_USE_NUM_HESS and cost_version == 'EXTERNAL':
+        for i in range(N):
+            ocp_solver.cost_set(i, "ext_cost_num_hess", np.diag([0.04, 4000, 4000, 0.04, 0.04, ]))
+        ocp_solver.cost_set(N, "ext_cost_num_hess", np.diag([4000, 4000, 0.04, 0.04, ]))
+
+
+    simX = np.ndarray((N+1, nx))
+    simU = np.ndarray((N, nu))
+
+    status = ocp_solver.solve()
+
+    ocp_solver.print_statistics()
+
+    if status != 0:
+        raise Exception(f'acados returned status {status}.')
+
+    # get solution
     for i in range(N):
-        ocp_solver.cost_set(i, "ext_cost_num_hess", np.diag([0.04, 4000, 4000, 0.04, 0.04, ]))
-    ocp_solver.cost_set(N, "ext_cost_num_hess", np.diag([4000, 4000, 0.04, 0.04, ]))
+        simX[i,:] = ocp_solver.get(i, "x")
+        simU[i,:] = ocp_solver.get(i, "u")
+    simX[N,:] = ocp_solver.get(N, "x")
 
 
-simX = np.ndarray((N+1, nx))
-simU = np.ndarray((N, nu))
+    # plot results
+    plot_pendulum(np.linspace(0, Tf, N+1), Fmax, simU, simX, latexify=False)
 
-status = ocp_solver.solve()
+    ocp_solver.store_iterate(filename='solution.json', overwrite=True)
+    ocp_solver.load_iterate(filename='solution.json')
 
-ocp_solver.print_statistics()
-
-if status != 0:
-    raise Exception(f'acados returned status {status}.')
-
-# get solution
-for i in range(N):
-    simX[i,:] = ocp_solver.get(i, "x")
-    simU[i,:] = ocp_solver.get(i, "u")
-simX[N,:] = ocp_solver.get(N, "x")
-
-
-# plot results
-plot_pendulum(np.linspace(0, Tf, N+1), Fmax, simU, simX, latexify=False)
-
-ocp_solver.store_iterate(filename='solution.json', overwrite=True)
-ocp_solver.load_iterate(filename='solution.json')
+if __name__ == "__main__":
+    # for cost_version in COST_VERSIONS:
+    # for cost_version in ['EXTERNAL', 'NLS', 'LS_Z', 'NLS_Z']:
+    for cost_version in ['LS_Z', 'NLS_Z']:
+        main(cost_version=cost_version)
 
 # timings
 # time_tot = 1e8
