aec7c44fa1203a1411b0ddf0aa7eee212c0c7e17
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-11-16 15:11:59 +0100

    Python: RTI loop example add main function

--- examples/acados_python/pendulum_on_cart/ocp/example_sqp_rti_loop.py
+++ examples/acados_python/pendulum_on_cart/ocp/example_sqp_rti_loop.py
@@ -40,126 +40,131 @@ import numpy as np
 import scipy.linalg
 from utils import plot_pendulum
 
-# create ocp object to formulate the OCP
-ocp = AcadosOcp()
+def main():
+    # create ocp object to formulate the OCP
+    ocp = AcadosOcp()
 
-# set model
-model = export_pendulum_ode_model()
-ocp.model = model
+    # set model
+    model = export_pendulum_ode_model()
+    ocp.model = model
 
-Tf = 1.0
-nx = model.x.size()[0]
-nu = model.u.size()[0]
-ny = nx + nu
-ny_e = nx
-N = 20
+    Tf = 1.0
+    nx = model.x.size()[0]
+    nu = model.u.size()[0]
+    ny = nx + nu
+    ny_e = nx
+    N = 20
 
-# set dimensions
-ocp.dims.N = N
+    # set dimensions
+    ocp.dims.N = N
 
-# set cost
-Q = 2*np.diag([1e3, 1e3, 1e-2, 1e-2])
-R = 2*np.diag([1e-2])
+    # set cost
+    Q = 2*np.diag([1e3, 1e3, 1e-2, 1e-2])
+    R = 2*np.diag([1e-2])
 
-ocp.cost.W_e = Q
-ocp.cost.W = scipy.linalg.block_diag(Q, R)
+    ocp.cost.W_e = Q
+    ocp.cost.W = scipy.linalg.block_diag(Q, R)
 
-ocp.cost.cost_type = 'LINEAR_LS'
-ocp.cost.cost_type_e = 'LINEAR_LS'
+    ocp.cost.cost_type = 'LINEAR_LS'
+    ocp.cost.cost_type_e = 'LINEAR_LS'
 
-ocp.cost.Vx = np.zeros((ny, nx))
-ocp.cost.Vx[:nx,:nx] = np.eye(nx)
+    ocp.cost.Vx = np.zeros((ny, nx))
+    ocp.cost.Vx[:nx,:nx] = np.eye(nx)
 
-Vu = np.zeros((ny, nu))
-Vu[4,0] = 1.0
-ocp.cost.Vu = Vu
+    Vu = np.zeros((ny, nu))
+    Vu[4,0] = 1.0
+    ocp.cost.Vu = Vu
 
-ocp.cost.Vx_e = np.eye(nx)
+    ocp.cost.Vx_e = np.eye(nx)
 
-ocp.cost.yref  = np.zeros((ny, ))
-ocp.cost.yref_e = np.zeros((ny_e, ))
+    ocp.cost.yref  = np.zeros((ny, ))
+    ocp.cost.yref_e = np.zeros((ny_e, ))
 
-# set constraints
-Fmax = 80
-ocp.constraints.lbu = np.array([-Fmax])
-ocp.constraints.ubu = np.array([+Fmax])
-ocp.constraints.idxbu = np.array([0])
+    # set constraints
+    Fmax = 80
+    ocp.constraints.lbu = np.array([-Fmax])
+    ocp.constraints.ubu = np.array([+Fmax])
+    ocp.constraints.idxbu = np.array([0])
 
-ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
+    ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
 
-# set options
-ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
-ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
-ocp.solver_options.integrator_type = 'ERK'
-ocp.solver_options.print_level = 0
-ocp.solver_options.nlp_solver_type = 'SQP_RTI' # SQP_RTI, SQP
+    # set options
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
+    ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
+    ocp.solver_options.integrator_type = 'ERK'
+    ocp.solver_options.print_level = 0
+    ocp.solver_options.nlp_solver_type = 'SQP_RTI' # SQP_RTI, SQP
 
-# set prediction horizon
-ocp.solver_options.tf = Tf
+    # set prediction horizon
+    ocp.solver_options.tf = Tf
 
-ocp_solver = AcadosOcpSolver(ocp, json_file = 'acados_ocp.json')
+    ocp_solver = AcadosOcpSolver(ocp, json_file = 'acados_ocp.json')
 
-simX = np.ndarray((N+1, nx))
-simU = np.ndarray((N, nu))
+    simX = np.ndarray((N+1, nx))
+    simU = np.ndarray((N, nu))
 
 
-# call SQP_RTI solver in the loop:
-tol = 1e-6
+    # call SQP_RTI solver in the loop:
+    tol = 1e-6
 
-for i in range(20):
-    status = ocp_solver.solve()
-    ocp_solver.custom_update()
+    for i in range(20):
+        status = ocp_solver.solve()
+        ocp_solver.custom_update()
+        ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
+        residuals = ocp_solver.get_residuals()
+        print("residuals after ", i, "SQP_RTI iterations:\n", residuals)
+        if max(residuals) < tol:
+            break
+
+
+    if status != 0:
+        raise Exception(f'acados returned status {status}.')
+
+    # get solution
+    for i in range(N):
+        simX[i,:] = ocp_solver.get(i, "x")
+        simU[i,:] = ocp_solver.get(i, "u")
+    simX[N,:] = ocp_solver.get(N, "x")
+
     ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
-    residuals = ocp_solver.get_residuals()
-    print("residuals after ", i, "SQP_RTI iterations:\n", residuals)
-    if max(residuals) < tol:
-        break
+
+    #
+    cost = ocp_solver.get_cost()
+    print("cost function value of solution = ", cost)
+
+    PRINT_QP = False
+    if PRINT_QP:
+        for i in range(N):
+            A_qp = ocp_solver.get_from_qp_in(i, "A")
+            print(f"qp: A at stage {i}: {A_qp}")
+
+        for i in range(N):
+            B_qp = ocp_solver.get_from_qp_in(i, "B")
+            print(f"qp: B at stage {i}: {B_qp}")
+
+        for i in range(N+1):
+            Q_qp = ocp_solver.get_from_qp_in(i, "Q")
+            print(f"qp: Q at stage {i}: {Q_qp}")
+
+        for i in range(N+1):
+            R_qp = ocp_solver.get_from_qp_in(i, "R")
+            print(f"qp: R at stage {i}: {R_qp}")
+
+        for i in range(N+1):
+            S_qp = ocp_solver.get_from_qp_in(i, "S")
+            print(f"qp: S at stage {i}: {S_qp}")
+
+        for i in range(N+1):
+            r_qp = ocp_solver.get_from_qp_in(i, "r")
+            print(f"qp: r at stage {i}: {r_qp}")
+
+        for i in range(N+1):
+            q_qp = ocp_solver.get_from_qp_in(i, "q")
+            print(f"qp: q at stage {i}: {q_qp}")
+
+    # plot
+    plot_pendulum(np.linspace(0, Tf, N+1), Fmax, simU, simX, latexify=False)
 
 
-if status != 0:
-    raise Exception(f'acados returned status {status}.')
-
-# get solution
-for i in range(N):
-    simX[i,:] = ocp_solver.get(i, "x")
-    simU[i,:] = ocp_solver.get(i, "u")
-simX[N,:] = ocp_solver.get(N, "x")
-
-ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
-
-#
-cost = ocp_solver.get_cost()
-print("cost function value of solution = ", cost)
-
-PRINT_QP = False
-if PRINT_QP:
-    for i in range(N):
-        A_qp = ocp_solver.get_from_qp_in(i, "A")
-        print(f"qp: A at stage {i}: {A_qp}")
-
-    for i in range(N):
-        B_qp = ocp_solver.get_from_qp_in(i, "B")
-        print(f"qp: B at stage {i}: {B_qp}")
-
-    for i in range(N+1):
-        Q_qp = ocp_solver.get_from_qp_in(i, "Q")
-        print(f"qp: Q at stage {i}: {Q_qp}")
-
-    for i in range(N+1):
-        R_qp = ocp_solver.get_from_qp_in(i, "R")
-        print(f"qp: R at stage {i}: {R_qp}")
-
-    for i in range(N+1):
-        S_qp = ocp_solver.get_from_qp_in(i, "S")
-        print(f"qp: S at stage {i}: {S_qp}")
-
-    for i in range(N+1):
-        r_qp = ocp_solver.get_from_qp_in(i, "r")
-        print(f"qp: r at stage {i}: {r_qp}")
-
-    for i in range(N+1):
-        q_qp = ocp_solver.get_from_qp_in(i, "q")
-        print(f"qp: q at stage {i}: {q_qp}")
-
-# plot
-plot_pendulum(np.linspace(0, Tf, N+1), Fmax, simU, simX, latexify=False)
+if __name__ == "__main__":
+    main()
