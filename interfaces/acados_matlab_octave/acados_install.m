a133b8fc759a39dc27d51b5e4ac5c6618d07792e
Jacob Deleuran Grunnet <jacobdeleuran@shanghai-electric.com>
2022-12-02 15:07:00 +0100

    Matlab: Add script for automatically installing acados

---
+++ interfaces/acados_matlab_octave/acados_install.m
@@ -0,0 +1,92 @@
+function acados_install(acadosPath)
+arguments
+    acadosPath=pwd; %If path is not supplied, assume acados path is current directory
+end
+
+    acadosBuildPath=fullfile(acadosPath,'build');
+
+    %% Acados installation instructions:
+    % Add the subfolders bin and x86_64-w64-mingw32\bin of the above mentioned mingw installation to your environment variable PATH.
+
+    %Store the current PATH environment variable value
+    origEnvPath=getenv('PATH');
+    %Read the mex C compiler configuration and extract the location
+    cCompilerConfig = mex.getCompilerConfigurations('C');
+    pathToCompilerLocation = cCompilerConfig.Location;
+    %Modify the environment PATH varuable for this Matlab session such that 
+    %the mex C compiler takes priority ensuring calls to gcc uses the
+    %configured mex compiler
+    setenv('PATH', [fullfile(pathToCompilerLocation,'bin') ';' origEnvPath]);
+
+    %% Acados installation instructions:
+    % $ACADOS_INSTALL_DIR=$(pwd)
+    % mkdir -p build
+    % cd build
+
+    setenv('ACADOS_INSTALL_DIR',acadosPath);
+    mkdir(acadosBuildPath);
+    cd(acadosBuildPath);
+
+    %% Acados installation instructions:
+    % cmake.exe -G "MinGW Makefiles" -DACADOS_INSTALL_DIR="$ACADOS_INSTALL_DIR" -DBUILD_SHARED_LIBS=OFF -DACADOS_WITH_OSQP=ON ..
+    % # useful options to add above:
+    % # -DACADOS_WITH_QPOASES=ON/OFF -DACADOS_WITH_OSQP=ON/OFF -DACADOS_WITH_QPDUNES=ON/OFF ..
+    % # -DBLASFEO_TARGET=GENERIC -DHPIPM_TARGET=GENERIC
+    % # NOTE: check the output of cmake: -- Installation directory: should be <acados_root_folder>,
+    % #     if this is not the case, set -DACADOS_INSTALL_DIR=<acados_root_folder> explicitly above.
+
+    %Command slightly modified to work with CMD instead of PowerShell
+    cmake_cmd = 'cmake.exe -G "MinGW Makefiles" -DACADOS_INSTALL_DIR=%ACADOS_INSTALL_DIR% -DBUILD_SHARED_LIBS=OFF -DACADOS_WITH_OSQP=OFF ..';
+
+    status=system(cmake_cmd);
+    if (status~=0)
+        error('cmake command failed: %s',cmake_cmd);
+    end
+
+    %% Acados installation instructions:
+    % mingw32-make.exe -j4
+    % mingw32-make.exe install
+
+
+    compile_command='mingw32-make.exe -j4';
+    status = system(compile_command);
+    if status ~= 0
+        %Store the PATH environment variable used during compile for error reporting
+        envPath=getenv('PATH');
+        setenv('PATH',origEnvPath);
+        error('Compilation of acados failed %s\n%s\n%s\n%s\n\n', ...
+            'Compile command was:', compile_command, ...
+            'Environment path was: ', envPath);
+    end
+
+    install_command='mingw32-make.exe install';
+    status = system(install_command);
+    if status ~= 0
+        %Store the PATH environment variable used during compile for error reporting
+        envPath=getenv('PATH');
+        setenv('PATH',origEnvPath);
+        error('Installation of acados failed %s\n%s\n%s\n%s\n\n', ...
+            'Install command was:', install_command, ...
+            'Environment path was: ', envPath);
+    end
+    
+    %Restore path to original
+    setenv('PATH',origEnvPath);
+    
+    %% Install is not complete - download casadi
+    addpath(fullfile(acadosPath,'interfaces\acados_matlab_octave'));
+    run('acados_env_variables_windows');
+    check_acados_requirements(true);
+    
+    %%Install is still not complete - install the renderer 
+    acados_root_dir = getenv('ACADOS_INSTALL_DIR');
+ 
+    %% check if t_renderer is available -> download if not
+    t_renderer_location = fullfile(acados_root_dir,'bin','t_renderer.exe');
+
+
+    if ~exist( t_renderer_location, 'file' )
+        acados_template_mex.set_up_t_renderer( t_renderer_location )
+    end
+    
+end
