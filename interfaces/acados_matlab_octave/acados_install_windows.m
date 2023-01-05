8b1ed9de5f1d004a3a97ce64674d69e3686018b8
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-01-05 11:09:07 +0100

    polish acados_install_windows

--- interfaces/acados_matlab_octave/acados_install_windows.m
+++ interfaces/acados_matlab_octave/acados_install_windows.m
@@ -1,13 +1,12 @@
 function acados_install_windows(varargin)
 
-    %The first variable parameter determines whether to force install
     switch(nargin)
         case 0
-            acadosPath=pwd; 
+            acadosPath=pwd;
         case 1
             acadosPath=varargin{1};
-        otherwise 
-            error('function called with %d parameters, was expecting max 1',nargin);
+        otherwise
+            error('function called with %d parameters, was expecting max 1', nargin);
     end
 
     acadosBuildPath=fullfile(acadosPath,'build');
@@ -15,24 +14,23 @@ function acados_install_windows(varargin)
     %% Acados installation instructions:
     % Add the subfolders bin and x86_64-w64-mingw32\bin of the above mentioned mingw installation to your environment variable PATH.
     fprintf('Setting up environment PATH variable\n');
-    
-    %Store the current PATH environment variable value
+
+    % Store the current PATH environment variable value
     origEnvPath=getenv('PATH');
-    %Read the mex C compiler configuration and extract the location
+    % Read the mex C compiler configuration and extract the location
     cCompilerConfig = mex.getCompilerConfigurations('C');
     pathToCompilerLocation = cCompilerConfig.Location;
-    %Modify the environment PATH varuable for this Matlab session such that 
-    %the mex C compiler takes priority ensuring calls to gcc uses the
-    %configured mex compiler
+    % Modify the environment PATH variable for this Matlab session such that
+    % the mex C compiler takes priority ensuring calls to gcc uses the
+    % configured mex compiler
     setenv('PATH', [fullfile(pathToCompilerLocation,'bin') ';' origEnvPath]);
 
     %% Acados installation instructions:
     % $ACADOS_INSTALL_DIR=$(pwd)
     % mkdir -p build
     % cd build
-    fprintf('Creating build dir in %s\n',acadosBuildPath);
-    
-    setenv('ACADOS_INSTALL_DIR',acadosPath);
+    fprintf('Creating build dir in %s\n', acadosBuildPath);
+    setenv('ACADOS_INSTALL_DIR', acadosPath);
     mkdir(acadosBuildPath);
     cd(acadosBuildPath);
 
@@ -44,13 +42,12 @@ function acados_install_windows(varargin)
     % # NOTE: check the output of cmake: -- Installation directory: should be <acados_root_folder>,
     % #     if this is not the case, set -DACADOS_INSTALL_DIR=<acados_root_folder> explicitly above.
     fprintf('Executing cmake configuration\n');
-    
-    %Command slightly modified to work with CMD instead of PowerShell
+    % Command slightly modified to work with CMD instead of PowerShell
     cmake_cmd = 'cmake.exe -G "MinGW Makefiles" -DACADOS_INSTALL_DIR=%ACADOS_INSTALL_DIR% -DBUILD_SHARED_LIBS=OFF -DACADOS_WITH_OSQP=OFF ..';
 
     status=system(cmake_cmd);
     if (status~=0)
-        error('cmake command failed: %s',cmake_cmd);
+        error('cmake command failed. Command was:\n%s\n', cmake_cmd);
     end
 
     %% Acados installation instructions:
@@ -79,26 +76,25 @@ function acados_install_windows(varargin)
             'Install command was:', install_command, ...
             'Environment path was: ', envPath);
     end
-    
-    %Restore path to original
+
+    % Restore path to original
     setenv('PATH',origEnvPath);
-    
-    %% Install is not complete - download casadi
+
+    %% Download external dependencies
+    % casadi
     fprintf('Downloading casadi\n');
-    addpath(fullfile(acadosPath,'interfaces\acados_matlab_octave'));
+    addpath(fullfile(acadosPath,'interfaces', 'acados_matlab_octave'));
     run('acados_env_variables_windows');
     check_acados_requirements(true);
-    
-    %%Install is still not complete - install the renderer 
+
+    %% renderer
     fprintf('Install the template renderer\n');
     acados_root_dir = getenv('ACADOS_INSTALL_DIR');
- 
     %% check if t_renderer is available -> download if not
-    t_renderer_location = fullfile(acados_root_dir,'bin','t_renderer.exe');
-
+    t_renderer_location = fullfile(acados_root_dir, 'bin', 't_renderer.exe');
 
     if ~exist( t_renderer_location, 'file' )
         acados_template_mex.set_up_t_renderer( t_renderer_location )
     end
-    
+
 end
