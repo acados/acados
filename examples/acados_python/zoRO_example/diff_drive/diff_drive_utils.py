a1690473f085a3c9fbf027df3e27f13015dccbc8
tchenlab <tchenlab@pse-tc319-lt2.egr.duke.edu>
2023-07-14 14:04:25 -0400

    better plotting for diff-drive robot trajectory

--- examples/acados_python/zoRO_example/diff_drive/diff_drive_utils.py
+++ examples/acados_python/zoRO_example/diff_drive/diff_drive_utils.py
@@ -56,7 +56,7 @@ def plot_timings(timing_dict):
         params = get_latex_plot_params()
         matplotlib.rcParams.update(params)
     else:
-        print("WARNING: LATEX not installed")
+        print("LATEX NOT INSTALLED. The plot is not rendered with LATEX")
 
     print("timings\t\tmin\tmean\tmax\n--------------------------------")
     for k, v in timing_dict.items():
@@ -67,20 +67,53 @@ def plot_timings(timing_dict):
     fig = plt.figure(0)
     ax = fig.add_subplot(111)
     ax.boxplot(timing_dict.values(), vert=False,
-        flierprops=green_square, \
-        medianprops=medianprops, showmeans=False)
+               flierprops=green_square, 
+               medianprops=medianprops, showmeans=False
+               )
     ax.set_yticklabels(timing_dict.keys())
     plt.grid()
-    plt.xlabel('CPU time [ms]')
+    plt.xlabel("CPU time [ms]")
     plt.tight_layout()
 
-    if not os.path.exists('figures'):
-        os.makedirs('figures')
+    if not os.path.exists("figures"):
+        os.makedirs("figures")
     plt.savefig(os.path.join("figures", "timings_diff_drive.pdf"),
         bbox_inches='tight', transparent=True, pad_inches=0.05)
 
 
-def compute_min_dis(cfg:MPCParam, s:np.ndarray)->float:
+def plot_trajectory(cfg:MPCParam, traj_ref:np.ndarray, traj_zo:np.ndarray):
+    if shutil.which('latex'):
+        params = get_latex_plot_params()
+        matplotlib.rcParams.update(params)
+    fig = plt.figure(1)
+    ax = fig.add_subplot(1,1,1)
+    for idx_obs in range(cfg.num_obs):
+        if idx_obs == 0:
+            circ = plt.Circle(cfg.obs_pos[idx_obs,:], cfg.obs_radius[idx_obs],
+                            edgecolor="red", facecolor=(1,0,0,.5), label="obstacle"
+                            )
+        else:
+            circ = plt.Circle(cfg.obs_pos[idx_obs,:], cfg.obs_radius[idx_obs],
+                              edgecolor="red", facecolor=(1,0,0,.5)
+                              )
+        ax.add_artist(circ)
+    ax.set_title("Robot Trajectory")
+    ax.plot(traj_ref[:, 0], traj_ref[:, 1], c='m', label='ref')
+    ax.plot(traj_zo[:, 0], traj_zo[:, 1], c='b', label='zoRO')
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    ax.set_xticks(np.arange(-2., 9., 2.))
+    ax.set_yticks(np.arange(0., 11., 2.))
+    ax.set_aspect("equal")
+    ax.legend()
+
+    if not os.path.exists("figures"):
+        os.makedirs("figures")
+    plt.savefig(os.path.join("figures", "diff_drive_sim_trajectory.pdf"),
+        bbox_inches='tight', transparent=True, pad_inches=0.05)
+
+
+def compute_min_dis(cfg:MPCParam, s:np.ndarray) -> float:
     min_dist = np.inf
     for idx_obs in range(cfg.num_obs):
         min_dist = np.min([min_dist, \
