f61ebd944d955a42f01fe54ce8434eaa16f0f9b6
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-07-18 14:52:36 +0200

    fix zoro simulink example

--- examples/acados_python/zoRO_example/pendulum_on_cart/run_simulink.m
+++ examples/acados_python/zoRO_example/pendulum_on_cart/run_simulink.m
@@ -12,7 +12,12 @@ ny = nx+nu;
 ny_e = nx;
 x0 = [0.0, 0.15*pi, 0.0, 0.0];
 
-
-% in zoro test
 cd c_generated_code/
 make_sfun_pendulum
+make_sfun_sim_pendulum
+
+out_sim = sim('simulink_model_closed_loop', 'SaveOutput', 'on');
+
+u_values = out_sim.logsout{1}.Values.Data;
+
+disp('successfully ran Simulink zoro closed loop simulation');
