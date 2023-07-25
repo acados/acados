5341046d94491e64d3f00a14b15a2a59a7663eb4
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-07-25 15:14:20 +0200

    rename compile_main -> compile_ocp_shared_lib; only compile OCP lib on linux

--- interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/compile_ocp_shared_lib.m
+++ interfaces/acados_matlab_octave/acados_template_mex/+acados_template_mex/compile_ocp_shared_lib.m
@@ -29,24 +29,18 @@
 
 %
 
-function compile_main(export_dir)
+function compile_ocp_shared_lib(export_dir)
     return_dir = pwd;
     cd(export_dir);
     %% build main file
     if isunix
-        [ status, result ] = system('make');
-        if status
-            cd(return_dir);
-            error('building templated code failed.\nGot status %d, result: %s',...
-                  status, result);
-        end
-        [ status, result ] = system('make shared_lib');
+        [ status, result ] = system('make ocp_shared_lib');
         if status
             cd(return_dir);
             error('building templated code as shared library failed.\nGot status %d, result: %s',...
                   status, result);
         end
-        fprintf('Successfully built main file!\n');
+        fprintf('Successfully built ocp_shared_lib\n');
     else
         % compile on Windows platform
         [ status, result ] = system('cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_ACADOS_OCP_SOLVER_LIB=ON -S . -B .');
