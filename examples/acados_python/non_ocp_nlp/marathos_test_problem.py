f55ed4fc307226db2270797ad6a4dd321364fa90
aghezzi <ghezzi@veela.syscop.de>
2023-08-24 17:29:06 +0200

    fix get_stats call in examples

--- examples/acados_python/non_ocp_nlp/marathos_test_problem.py
+++ examples/acados_python/non_ocp_nlp/marathos_test_problem.py
@@ -173,7 +173,7 @@ def solve_marathos_problem_with_setting(setting):
     else:
         ocp_solver.solve()
         ocp_solver.print_statistics()
-        iter = ocp_solver.get_stats('sqp_iter')[0]
+        iter = ocp_solver.get_stats('sqp_iter')
         alphas = ocp_solver.get_stats('alpha')[1:]
         qp_iters = ocp_solver.get_stats('qp_iter')
         residuals = ocp_solver.get_stats('statistics')[1:5,1:iter]
