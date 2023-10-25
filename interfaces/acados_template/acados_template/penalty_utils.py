b97c8e6a40e6a42749201c14b38895bc15895d34
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-10-25 14:32:48 +0200

    CONL L2 penalty

--- interfaces/acados_template/acados_template/penalty_utils.py
+++ interfaces/acados_template/acados_template/penalty_utils.py
@@ -26,10 +26,14 @@ def formulate_constraints_as_penalty(
         violation_expr = ca.fmax(violation_expr, (lower_bound - constr_expr))
 
     if penalty_type == "L2":
+        ocp.cost.yref = np.concatenate((ocp.cost.yref, np.zeros(1)))
+        ocp.model.cost_y_expr = ca.vertcat(ocp.model.cost_y_expr, violation_expr)
         if ocp.cost.cost_type == "NONLINEAR_LS":
             ocp.cost.W = block_diag(ocp.cost.W, weight)
-            ocp.cost.yref = np.concatenate((ocp.cost.yref, np.zeros(1)))
-            ocp.model.cost_y_expr = ca.vertcat(ocp.model.cost_y_expr, violation_expr)
+        elif ocp.cost.cost_type == "CONVEX_OVER_NONLINEAR":
+            new_residual = ca.SX.sym('new_residual', violation_expr.shape)
+            ocp.model.cost_r_in_psi_expr = ca.vertcat(ocp.model.cost_r_in_psi_expr, new_residual)
+            ocp.model.cost_psi_expr += weight * new_residual**2
         else:
             raise NotImplementedError(
                 f"Penalty type {penalty_type} is not yet supported for ocp.."
