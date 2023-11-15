4cf34c8e4b3819d29ba6393c298062bc372be225
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-11-15 14:35:00 +0100

    fix header

--- interfaces/acados_template/acados_template/c_templates_tera/constraints.in.h
+++ interfaces/acados_template/acados_template/c_templates_tera/constraints.in.h
@@ -53,6 +53,16 @@ int {{ model.name }}_phi_e_constraint_n_in(void);
 int {{ model.name }}_phi_e_constraint_n_out(void);
 {% endif %}
 
+{% if dims.nphi_0 > 0 %}
+int {{ model.name }}_phi_0_constraint(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
+int {{ model.name }}_phi_0_constraint_work(int *, int *, int *, int *);
+const int *{{ model.name }}_phi_0_constraint_sparsity_in(int);
+const int *{{ model.name }}_phi_0_constraint_sparsity_out(int);
+int {{ model.name }}_phi_0_constraint_n_in(void);
+int {{ model.name }}_phi_0_constraint_n_out(void);
+{% endif %}
+
+
 {% if dims.nh > 0 %}
 int {{ model.name }}_constr_h_fun_jac_uxt_zt(const real_t** arg, real_t** res, int* iw, real_t* w, void *mem);
 int {{ model.name }}_constr_h_fun_jac_uxt_zt_work(int *, int *, int *, int *);
