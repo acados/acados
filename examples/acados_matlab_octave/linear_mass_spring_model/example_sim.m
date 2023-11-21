3b8bb82981824cecf4b31e2c690522a334491b41
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-11-21 10:44:13 +0100

    remove C_sim, C_sim_ext_fun in MEX examples

--- examples/acados_matlab_octave/linear_mass_spring_model/example_sim.m
+++ examples/acados_matlab_octave/linear_mass_spring_model/example_sim.m
@@ -112,9 +112,6 @@ sim_opts.opts_struct
 sim = acados_sim(sim_model, sim_opts);
 % (re)set numerical part of model
 %sim.set('T', 0.5);
-sim.C_sim
-sim.C_sim_ext_fun
-
 
 
 x0 = ones(nx, 1); %x0(1) = 2.0;
