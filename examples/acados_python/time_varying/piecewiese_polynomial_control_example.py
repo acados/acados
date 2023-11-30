a9460e73d310b8ece6695ad9a752c690a5cf241f
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-11-30 12:16:23 +0100

    piecewiese_polynomial_control_example with mocp

--- examples/acados_python/time_varying/piecewiese_polynomial_control_example.py
+++ examples/acados_python/time_varying/piecewiese_polynomial_control_example.py
@@ -29,13 +29,13 @@
 #
 
 
-from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver, casadi_length
+from acados_template import AcadosModel, AcadosOcp, AcadosMultiphaseOcp, AcadosOcpSolver, casadi_length
 import numpy as np
 import casadi as ca
 from scipy.linalg import block_diag
 from polynom_utils import plot_open_loop_trajectory_pwpol_u, export_pendulum_ode_model, augment_model_with_polynomial_control
 
-def create_ocp_formulation(cost_type, N_horizon, degree_u_polynom, explicit_symmetric_penalties=True, penalty_type='L2') -> (AcadosOcp, ca.Function):
+def create_ocp_formulation_without_opts(cost_type, degree_u_polynom, explicit_symmetric_penalties=True, penalty_type='L2') -> (AcadosOcp, ca.Function):
     # create ocp object to formulate the OCP
     ocp = AcadosOcp()
 
@@ -45,9 +45,6 @@ def create_ocp_formulation(cost_type, N_horizon, degree_u_polynom, explicit_symm
 
     ocp.model: AcadosModel = model
     nx = casadi_length(model.x)
-
-    # set dimensions
-    ocp.dims.N = N_horizon
 
     # set cost
     Q_mat = 2*np.diag([1e3, 1e3, 1e-2, 1e-2])
@@ -103,13 +100,20 @@ def create_ocp_formulation(cost_type, N_horizon, degree_u_polynom, explicit_symm
     ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
 
     model, evaluate_polynomial_u_fun = augment_model_with_polynomial_control(model, d=degree_u_polynom)
+    ocp.parameter_values = np.array([1.0]) # dummy parameter
+
 
     return ocp, evaluate_polynomial_u_fun
 
 
 
-def create_ocp_solver(cost_type, N_horizon, degree_u_polynom, explicit_symmetric_penalties=True, penalty_type='L2') -> (AcadosOcpSolver, ca.Function):
-    ocp, evaluate_polynomial_u_fun = create_ocp_formulation(cost_type, N_horizon, degree_u_polynom, explicit_symmetric_penalties=explicit_symmetric_penalties, penalty_type=penalty_type)
+def create_ocp_solver(cost_type, N_horizon, degree_u_polynom,
+                      explicit_symmetric_penalties=True,
+                      penalty_type='L2', nlp_solver_max_iter=None) -> (AcadosOcpSolver, ca.Function):
+    ocp: AcadosOcp
+    ocp, evaluate_polynomial_u_fun = create_ocp_formulation_without_opts(cost_type, degree_u_polynom, explicit_symmetric_penalties=explicit_symmetric_penalties, penalty_type=penalty_type)
+
+    ocp.dims.N = N_horizon
 
     # set options
     ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
@@ -120,6 +124,9 @@ def create_ocp_solver(cost_type, N_horizon, degree_u_polynom, explicit_symmetric
     # ocp.solver_options.print_level = 1
     ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
     ocp.solver_options.cost_discretization = 'INTEGRATOR'
+
+    if nlp_solver_max_iter is not None:
+        ocp.solver_options.nlp_solver_max_iter = nlp_solver_max_iter
 
     # set prediction horizon
     T_horizon = 1.0
@@ -136,7 +143,101 @@ def create_ocp_solver(cost_type, N_horizon, degree_u_polynom, explicit_symmetric
 
     return ocp_solver, evaluate_polynomial_u_fun
 
-def main(cost_type='NONLINEAR_LS', explicit_symmetric_penalties=True, penalty_type='L2'):
+
+def create_mocp_solver(cost_type, N_list, degrees_u_polynom, explicit_symmetric_penalties=True, penalty_type='L2', nlp_solver_max_iter=None) -> (AcadosOcpSolver, ca.Function):
+
+    N_horizon = sum(N_list)
+    n_phases = len(N_list)
+
+    mocp = AcadosMultiphaseOcp(n_phases, N_list)
+    polynomial_u_funs = []
+
+    for i, degree_u_polynom in enumerate(degrees_u_polynom):
+        ocp, evaluate_polynomial_u_fun = create_ocp_formulation_without_opts(cost_type, degree_u_polynom, explicit_symmetric_penalties=explicit_symmetric_penalties, penalty_type=penalty_type)
+
+        polynomial_u_funs.append(evaluate_polynomial_u_fun)
+        mocp.set_phase(ocp, i)
+
+    # set options
+    mocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
+    # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
+    # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
+    mocp.solver_options.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
+    mocp.solver_options.integrator_type = 'IRK'
+    # mocp.solver_options.print_level = 1
+    mocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
+    mocp.solver_options.cost_discretization = 'INTEGRATOR'
+    if nlp_solver_max_iter is not None:
+        mocp.solver_options.nlp_solver_max_iter = nlp_solver_max_iter
+
+    # set prediction horizon
+    T_horizon = 1.0
+    n_short = 1
+    dt_short = 0.02
+    n_long = N_horizon-n_short
+    mocp.solver_options.time_steps = np.array(n_short * [dt_short] + n_long * [(T_horizon - dt_short * n_short) / n_long])
+    mocp.solver_options.tf = T_horizon
+
+    ocp_solver = AcadosOcpSolver(mocp, verbose=True)
+    for i in range(N_horizon):
+        ocp_solver.set(i, 'p', mocp.solver_options.time_steps[i])
+
+    return ocp_solver, polynomial_u_funs
+
+
+
+def main_mocp(cost_type='NONLINEAR_LS', explicit_symmetric_penalties=True, penalty_type='L2'):
+    N_list = [2, 18]
+    N_horizon = sum(N_list)
+    degrees_u_polynom = [3, 2]
+
+    # create solver and extract
+    ocp_solver, polynomial_u_funs = create_mocp_solver(cost_type, N_list, degrees_u_polynom, explicit_symmetric_penalties=explicit_symmetric_penalties, penalty_type=penalty_type)
+    ocp = ocp_solver.acados_ocp
+    model = ocp.model[0]
+    nu_original = model.nu_original
+    nx = casadi_length(model.x)
+
+    # solve OCP
+    status = ocp_solver.solve()
+    ocp_solver.print_statistics()
+
+    if status != 0:
+        raise Exception(f'acados returned status {status}.')
+
+    cost_val = ocp_solver.get_cost()
+    print(f"found optimal cost: {cost_val:.4e}")
+
+    # get solution
+    simX = np.ndarray((N_horizon+1, nx))
+    simU = []
+    for i in range(N_horizon):
+        simX[i,:] = ocp_solver.get(i, "x")
+        simU.append(ocp_solver.get(i, "u"))
+    simX[N_horizon,:] = ocp_solver.get(N_horizon, "x")
+
+    # compute U fine:
+    n_pol_eval = 20
+    U_fine = []
+    i_ocp = 0
+    for i_phase in range(len(N_list)):
+        for _ in range(N_list[i_phase]):
+            dt = ocp.solver_options.time_steps[i_ocp]
+            u_coeff = simU[i_ocp]
+            U_interval = np.zeros((n_pol_eval+1, nu_original))
+            for j in range(n_pol_eval+1):
+                t = (j/n_pol_eval) * dt
+                U_interval[j, :] = polynomial_u_funs[i_phase](u_coeff, t, dt)
+            U_fine.append(U_interval)
+            i_ocp += 1
+    # breakpoint()
+    ocp_solver.store_iterate('mocp_sol.json', overwrite=True)
+
+    # plot
+    plot_open_loop_trajectory_pwpol_u(ocp.solver_options.shooting_nodes, simX, U_fine, title=f'controls: piecewise polynomials with degree {degrees_u_polynom}')
+
+
+def main_ocp(cost_type='NONLINEAR_LS', explicit_symmetric_penalties=True, penalty_type='L2'):
     N_horizon = 20
     degree_u_polynom = 3
 
@@ -177,10 +278,14 @@ def main(cost_type='NONLINEAR_LS', explicit_symmetric_penalties=True, penalty_ty
             U_interval[j, :] = evaluate_polynomial_u_fun(u_coeff, t, dt)
         U_fine.append(U_interval)
 
+    ocp_solver.store_iterate('ocp_sol.json', overwrite=True)
+
     # plot
     plot_open_loop_trajectory_pwpol_u(ocp.solver_options.shooting_nodes, simX, U_fine, title=f'controls: piecewise polynomials of degree {degree_u_polynom}')
 
 
 if __name__ == '__main__':
     # main(cost_type="CONVEX_OVER_NONLINEAR", explicit_symmetric_penalties=True)
-    main(cost_type="CONVEX_OVER_NONLINEAR", explicit_symmetric_penalties=True, penalty_type='Huber')
+    # main_ocp(cost_type="CONVEX_OVER_NONLINEAR", explicit_symmetric_penalties=True, penalty_type='Huber')
+    main_ocp(cost_type="CONVEX_OVER_NONLINEAR", explicit_symmetric_penalties=True, penalty_type='L2')
+    main_mocp(cost_type="CONVEX_OVER_NONLINEAR", explicit_symmetric_penalties=True, penalty_type='L2')
