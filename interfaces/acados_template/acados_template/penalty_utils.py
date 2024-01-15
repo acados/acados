05892003ed290c61fc20c3a74279588147e68745
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-01-15 14:05:39 +0100

    add huber plot

--- interfaces/acados_template/acados_template/penalty_utils.py
+++ interfaces/acados_template/acados_template/penalty_utils.py
@@ -31,6 +31,10 @@
 
 from typing import Optional
 import casadi as ca
+import numpy as np
+
+import matplotlib.pyplot as plt
+from plot_utils import latexify_plot
 
 def huber_loss(var: ca.SX, delta: float, tau: float):
     loss = tau/delta * ca.if_else(
@@ -87,3 +91,48 @@ def symmetric_huber_penalty(u: ca.SX, delta: float, tau: Optional[float] = None,
 
     return penalty, penalty_grad, penalty_hess, penalty_hess_xgn
 
+def plot_symmetric_huber():
+
+    u = ca.SX.sym('u')
+
+    delta = 1e-1
+    w = 1e4
+    penalty, penalty_grad, penalty_hess, penalty_hess_xgn = symmetric_huber_penalty(u, delta, w)
+
+    penalty_hess_fun = ca.Function('penalty_hess_fun', [u], [penalty_hess])
+    hess_val = penalty_hess_fun(1+0.5*delta)
+    print(f"{hess_val=}")
+
+    huber_penalty_fun = ca.Function('penalty', [u], [penalty])
+    print(f"{huber_penalty_fun(0.0)=}")
+    print(f"{huber_penalty_fun(0.999)=}")
+    print(f"{huber_penalty_fun(1.0)=}")
+    print(f"{huber_penalty_fun(1.0001)=}")
+    print(f"{huber_penalty_fun(1.1)=}")
+
+    print(f"{huber_penalty_fun(-1.0)=}")
+    print(f"{huber_penalty_fun(-1.1)=}")
+    print(f"{huber_penalty_fun(-0.999)=}")
+    u_ = ca.SX.sym('u_')
+    huber_penalty_quad_fun = ca.Function('penalty', [u_, u], [penalty + penalty_grad*(u_ - u) + 0.5*penalty_hess_xgn*(u_ - u)**2])
+
+    us = np.linspace(-1.5, 1.5, 800)
+    ys = huber_penalty_fun(us).full()
+    latexify_plot()
+    plt.figure(figsize=(10, 10))
+    plt.plot(us, ys, label='Huber penalty')
+
+    u_lins = [-.999, -.8, .4]
+    for i in range(len(u_lins)):
+        ys_quad = huber_penalty_quad_fun(us, u_lins[i]).full()
+        plt.plot(us, ys_quad, '--', label=f'quadratic approximation {i} at {u_lins[i]:.1f}')
+
+    plt.grid()
+    plt.legend()
+    plt.xlim(us[0], us[-1])
+    plt.ylim(min(ys), max(ys))
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_symmetric_huber()
