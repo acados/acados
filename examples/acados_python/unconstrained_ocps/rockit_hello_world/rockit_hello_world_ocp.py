8b98f47fed4f2e816baf0a189c5f3fe5222b762a
d.kiessling <david.kiessling@kuleuven.be>
2024-04-08 15:34:26 +0200

    Added the rockit hello world example for testing

---
+++ examples/acados_python/unconstrained_ocps/rockit_hello_world/rockit_hello_world_ocp.py
@@ -0,0 +1,131 @@
+#
+# Copyright (c) The acados authors.
+#
+# This file is part of acados.
+#
+# The 2-Clause BSD License
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.;
+#
+
+from acados_template import AcadosOcp, AcadosOcpSolver
+from rockit_hello_world_model import export_rockit_hello_world_model
+import numpy as np
+# from utils import plot_trajectory
+from matplotlib import pyplot as plt
+
+def main():
+
+    # The flag denotes, if the problem should be transformed into a feasibility
+    # problem, or if the unconstrained OCP should be solved.
+    SOLVE_FEASIBILITY_PROBLEM = True
+
+    # create ocp object to formulate the OCP
+    ocp = AcadosOcp()
+
+    # set model
+    model = export_rockit_hello_world_model()
+    ocp.model = model
+
+    Tf = 1.0
+    nx = model.x.rows()
+    nu = model.u.rows()
+    N = 2
+
+    # set dimensions
+    ocp.dims.N = N
+
+    if not SOLVE_FEASIBILITY_PROBLEM:
+        # set cost
+        # the 'EXTERNAL' cost type can be used to define general cost terms
+        # NOTE: This leads to additional (exact) hessian contributions when using GAUSS_NEWTON hessian.
+        ocp.cost.cost_type = 'EXTERNAL'
+        ocp.cost.cost_type_e = 'EXTERNAL'
+        ocp.model.cost_expr_ext_cost = model.x.T @ model.x + model.u.T @ model.u
+        ocp.model.cost_expr_ext_cost_e = model.x.T @ model.x
+
+    # set constraints
+    ocp.constraints.x0 = np.array([0.0, 1.0])
+
+    if SOLVE_FEASIBILITY_PROBLEM:
+        # Path constraints on control
+        u_max = 1.0
+        ocp.constraints.lbu = np.array([-u_max])
+        ocp.constraints.ubu = np.array([+u_max])
+        ocp.constraints.idxbu = np.array([0])
+
+        # Path constraint on x1
+        x_min = -0.25
+        ocp.constraints.lbx = np.array([x_min])
+        ocp.constraints.ubx = np.array([100.0])
+        # ocp.constraints.ubx = np.array([1e8])
+        ocp.constraints.idxbx = np.array([0])
+
+    # set options
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
+    ocp.solver_options.qp_solver_cond_N = N
+    ocp.solver_options.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
+    ocp.solver_options.integrator_type = 'ERK'
+    ocp.solver_options.sim_method_num_steps = 1
+    ocp.solver_options.print_level = 1
+    ocp.solver_options.nlp_solver_type = 'DDP' # SQP_RTI, SQP
+    ocp.solver_options.nlp_solver_max_iter = 1
+
+    # set prediction horizon
+    ocp.solver_options.tf = Tf
+
+    if SOLVE_FEASIBILITY_PROBLEM:
+        ocp.translate_to_feasibility_problem()
+
+    ocp_solver = AcadosOcpSolver(ocp, json_file = 'chen_allgoewer_acados.json')
+
+    sol_X = np.zeros((N+1, nx))
+    sol_U = np.zeros((N, nu))
+
+    # Load and set the initial guess
+    with open('chen_allgoewer_initial_guess.npy', 'rb') as f:
+        X_init = np.load(f)
+        U_init = np.load(f)
+
+    for i in range(N):
+        ocp_solver.set(i, "x", X_init[:,i])
+        ocp_solver.set(i, "u", U_init[:,i])
+    ocp_solver.set(N, "x", X_init[:,N])
+
+    # Solve the problem
+    status = ocp_solver.solve()
+
+    # if status != 0:
+    #     raise Exception(f'acados returned status {status}.')
+
+    # get solution
+    for i in range(N):
+        sol_X[i,:] = ocp_solver.get(i, "x")
+        sol_U[i,:] = ocp_solver.get(i, "u")
+    sol_X[N,:] = ocp_solver.get(N, "x")
+
+    # plot_trajectory([X_init, simX.T], ["Initial guess", "Solution"])
+
+
+if __name__ == '__main__':
+    main()
