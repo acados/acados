9735d668ae65ab7c629c9d094e1ef3f06c0d90d8
akash <akash31994@gmail.com>
2024-04-16 14:38:34 +0200

    octave paths

--- .github/linux/run_tests.sh
+++ .github/linux/run_tests.sh
@@ -30,10 +30,8 @@
 #
 
 # source virtualenv
-ls -R
 source ./../acadosenv/bin/activate;
 which python;
-
 
 # run tests
 ctest -C $BUILD_TYPE --output-on-failure; # use -V for full output # --output-on-failure for less
