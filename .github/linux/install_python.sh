efaa43fbf32bf5f5b966778857121e5561df24f8
akash <akash31994@gmail.com>
2024-04-25 11:52:02 +0200

    make env scipt executable

--- .github/linux/install_python.sh
+++ .github/linux/install_python.sh
@@ -31,13 +31,13 @@
 
 python3 --version;
 
-# # install virtualenv
-# sudo pip3 install virtualenv;
-# # create virtualenv
-# virtualenv --python=python3 acadosenv;
-# # source virtualenv
-# source acadosenv/bin/activate;
-# which python;
+# install virtualenv
+sudo pip3 install virtualenv;
+# create virtualenv
+virtualenv --python=python3 acadosenv;
+# source virtualenv
+source acadosenv/bin/activate;
+which python;
 
 pip install interfaces/acados_template
 
