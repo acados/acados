f522bb3ac1e42ab3065357a7aceaf3505a9ab8a8
akash <subasha@informatik.uni-freiburg.de>
2024-05-13 15:17:24 +0200

    action bump octave7.3.0

--- .github/linux/install_casadi_octave.sh
+++ .github/linux/install_casadi_octave.sh
@@ -36,6 +36,7 @@ OCTAVE_VERSION='6.2.0';
 _CASADI_GITHUB_RELEASES="https://github.com/casadi/casadi/releases/download/${CASADI_VERSION}";
 
 CASADI_OCTAVE_URL="${_CASADI_GITHUB_RELEASES}/casadi-linux-octave-${OCTAVE_VERSION}-v${CASADI_VERSION}.tar.gz";
+CASADI_OCTAVE_URL=https://github.com/casadi/casadi/releases/download/3.6.5/casadi-3.6.5-linux64-octave7.3.0.zip
 
 wget -O casadi-linux-octave.tar.gz "${CASADI_OCTAVE_URL}";
 mkdir -p casadi-octave;
