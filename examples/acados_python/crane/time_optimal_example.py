09f94fc83be55c61e73ab741d9451fdaad0a9915
sandmaennchen <katrin.baumgaertner@posteo.de>
2024-05-17 14:26:48 +0200

    setup of solver and integrator in separate function

--- examples/acados_python/crane/time_optimal_example.py
+++ examples/acados_python/crane/time_optimal_example.py
@@ -32,8 +32,8 @@ import matplotlib.pyplot as plt
 
 from acados_template import AcadosOcp, AcadosOcpSolver, AcadosModel, AcadosSimSolver, AcadosSim
 import numpy as np
-from casadi import SX, vertcat
-
+from casadi import SX, vertcat, diag
+from typing import Tuple
 
 def plot_crane_trajectories(ts, simX, simU):
 
@@ -53,10 +53,12 @@ def plot_crane_trajectories(ts, simX, simU):
     axes[-1].set_xlabel('t')
     axes[-1].set_xlim(ts[0], ts[-1])
 
+    fig.align_ylabels()
+
     plt.show()
 
+def setup_solver_and_integrator(x0: np.ndarray, xf: np.ndarray, N: int, use_cython: bool = True) -> Tuple[AcadosOcpSolver, AcadosSimSolver]:
 
-def main(use_cython=True):
     # (very) simple crane model
     beta = 0.001
     k = 0.9
@@ -87,27 +89,18 @@ def main(use_cython=True):
     model.u = u
     model.name = 'crane_time_opt'
 
-    x0 = np.array([2.0, 0.0, 2.0, 0.0])
-    xf = np.array([0.0, 0.0, 0.0, 0.0])
-
     ocp = AcadosOcp()
     ocp.model = model
-
-    # N - maximum number of bangs
-    N = 7
-    Tf = N
-    nx = model.x.rows()
-    nu = model.u.rows()
-
-    # set dimensions
     ocp.dims.N = N
+    ocp.solver_options.tf = N
 
-    # set cost
     ocp.cost.cost_type = 'EXTERNAL'
     ocp.cost.cost_type_e = 'EXTERNAL'
 
     ocp.model.cost_expr_ext_cost = dt
     ocp.model.cost_expr_ext_cost_e = 0
+
+    ocp.model.cost_expr_ext_cost_custom_hess = diag(vertcat(SX.zeros(1, 1), 1./(dt), SX.zeros(model.x.rows(), 1)))
 
     ocp.constraints.lbu = np.array([-a_max, dt_min])
     ocp.constraints.ubu = np.array([+a_max, dt_max])
@@ -118,21 +111,18 @@ def main(use_cython=True):
     ocp.constraints.ubx_e = xf
     ocp.constraints.idxbx_e = np.array([0, 1, 2, 3])
 
-    # set prediction horizon
-    ocp.solver_options.tf = Tf
-
     # set options
     ocp.solver_options.qp_solver = 'FULL_CONDENSING_QPOASES'#'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
     ocp.solver_options.integrator_type = 'ERK'
     ocp.solver_options.print_level = 3
-    ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
+    ocp.solver_options.nlp_solver_type = 'SQP'
     ocp.solver_options.globalization = 'MERIT_BACKTRACKING'
     ocp.solver_options.nlp_solver_max_iter = 5000
     ocp.solver_options.nlp_solver_tol_stat = 1e-6
-    ocp.solver_options.levenberg_marquardt = 0.1
     ocp.solver_options.sim_method_num_steps = 15
     ocp.solver_options.qp_solver_iter_max = 100
     ocp.solver_options.hessian_approx = 'EXACT'
+
     ocp.solver_options.exact_hess_constr = 0
     ocp.solver_options.exact_hess_dyn = 0
 
@@ -146,12 +136,34 @@ def main(use_cython=True):
 
     ocp_solver.reset()
 
+    # integrator
+    sim = AcadosSim()
+
+    sim.model = model
+    sim.solver_options.integrator_type = 'ERK'
+    sim.solver_options.num_stages = 4
+    sim.solver_options.num_steps = 3
+    sim.solver_options.T = 1.0 # dummy value
+    integrator = AcadosSimSolver(sim)
+
+    return ocp_solver, integrator
+
+def main(use_cython=True):
+
+    nu = 2
+    nx = 4
+
+    N = 7 # N - maximum number of bangs
+
+    x0 = np.array([2.0, 0.0, 2.0, 0.0])
+    xf = np.zeros((nx,))
+
+    ocp_solver, integrator = setup_solver_and_integrator(x0, xf, N, use_cython)
+
+    # initialization
     for i, tau in enumerate(np.linspace(0, 1, N)):
         ocp_solver.set(i, 'x', (1-tau)*x0 + tau*xf)
         ocp_solver.set(i, 'u', np.array([0.1, 0.5]))
-
-    simX = np.zeros((N+1, nx))
-    simU = np.zeros((N, nu))
 
     status = ocp_solver.solve()
 
@@ -160,6 +172,8 @@ def main(use_cython=True):
         raise Exception(f'acados returned status {status}.')
 
     # get solution
+    simX = np.zeros((N+1, nx))
+    simU = np.zeros((N, nu))
     for i in range(N):
         simX[i,:] = ocp_solver.get(i, "x")
         simU[i,:] = ocp_solver.get(i, "u")
@@ -171,17 +185,6 @@ def main(use_cython=True):
     print(f"optimal time: {sum(dts)} s")
 
     # simulate on finer grid
-    sim = AcadosSim()
-
-    # set model
-    sim.model = model
-
-    # set options
-    sim.solver_options.integrator_type = 'ERK'
-    sim.solver_options.num_stages = 4
-    sim.solver_options.num_steps = 3
-    sim.solver_options.T = 1.0 # dummy value
-
     dt_approx = 0.0005
 
     dts_fine = np.zeros((N,))
@@ -200,23 +203,21 @@ def main(use_cython=True):
     simX_fine = np.zeros((N_fine+1, nx))
     simX_fine[0, :] = x0
 
-    acados_integrator = AcadosSimSolver(sim)
-
     k = 0
     for i in range(N):
         u = simU[i, 0]
-        acados_integrator.set("u", np.hstack((u, np.ones(1, ))))
+        integrator.set("u", np.hstack((u, np.ones(1, ))))
 
         # set simulation time
-        acados_integrator.set("T", dts_fine[i])
+        integrator.set("T", dts_fine[i])
 
         for _ in range(Ns_fine[i]):
-            acados_integrator.set("x", simX_fine[k,:])
-            status = acados_integrator.solve()
+            integrator.set("x", simX_fine[k,:])
+            status = integrator.solve()
             if status != 0:
                 raise Exception(f'acados returned status {status}.')
 
-            simX_fine[k+1,:] = acados_integrator.get("x")
+            simX_fine[k+1,:] = integrator.get("x")
             simU_fine[k, :] = u
             ts_fine[k+1] = ts_fine[k] + dts_fine[i]
 
