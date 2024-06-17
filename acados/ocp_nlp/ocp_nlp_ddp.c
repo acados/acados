9c5b7bb05678f517a88cc5d0c6d556f9e2f1204f
d.kiessling <david.kiessling@kuleuven.be>
2024-06-17 16:04:47 +0200

    Moved the compute cost from submodule to adaptive_levenberg_marquardt function

--- acados/ocp_nlp/ocp_nlp_ddp.c
+++ acados/ocp_nlp/ocp_nlp_ddp.c
@@ -756,7 +756,6 @@ int ocp_nlp_ddp(void *config_, void *dims_, void *nlp_in_, void *nlp_out_,
         // linearize NLP, update QP matrices, and add Levenberg-Marquardt term
         acados_tic(&timer1);
         ocp_nlp_approximate_qp_matrices(config, dims, nlp_in, nlp_out, nlp_opts, nlp_mem, nlp_work);
-        ocp_nlp_get_cost_value_from_submodules(config, dims, nlp_in, nlp_out, nlp_opts, nlp_mem, nlp_work); // adds up parts of cost function
         ocp_nlp_add_levenberg_marquardt_term(mem->alpha, ddp_iter, config, dims,
                                              nlp_in, nlp_out, nlp_opts, nlp_mem, nlp_work);
 
