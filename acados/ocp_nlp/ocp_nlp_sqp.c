2b1de33c8865ea0dbed01904bc62551f03458697
d.kiessling <david.kiessling@kuleuven.be>
2024-06-19 12:36:39 +0200

    Corrected plotting of funnel width. Before, always the updated funnel width was shown, but this is not the correct funnel of the iteration

--- acados/ocp_nlp/ocp_nlp_sqp.c
+++ acados/ocp_nlp/ocp_nlp_sqp.c
@@ -678,7 +678,7 @@ static void ocp_nlp_sqp_reset_timers(ocp_nlp_sqp_memory *mem)
  * output functions
  ************************************************/
 static void print_iteration_header(){
-    printf("%6s | %11s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %15s | %10s | %10s | %10s\n",
+    printf("%6s | %11s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %12s | %10s | %10s | %10s\n",
     "iter.",
     "objective",
     "res_eq",
@@ -711,7 +711,7 @@ static void print_iteration(double obj,
     if ((iter_count % 10 == 0) | (iter_count == -1)){
         print_iteration_header();
     }
-    printf("%6i | %11.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %15.4e | %10i | %10i | %10c\n",
+    printf("%6i | %11.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %10.4e | %12.4e | %10i | %10i | %10c\n",
     iter_count,
     obj,
     infeas_eq,
@@ -1222,6 +1222,7 @@ int ocp_nlp_sqp(void *config_, void *dims_, void *nlp_in_, void *nlp_out_,
     }
     int sqp_iter = 0;
     double reg_param_memory = 0.0;
+    double funnel_width_memory = 0.0;
     if (nlp_opts->print_level > 0)
     {
         printf("'with_adaptive_levenberg_marquardt' option is set to: %s\n", opts->nlp_opts->with_adaptive_levenberg_marquardt?"true":"false");
@@ -1273,7 +1274,7 @@ int ocp_nlp_sqp(void *config_, void *dims_, void *nlp_in_, void *nlp_out_,
             print_iteration(nlp_mem->cost_value, sqp_iter, nlp_res->inf_norm_res_eq,
                             nlp_res->inf_norm_res_ineq, nlp_res->inf_norm_res_stat,
                             nlp_res->inf_norm_res_comp, mem->alpha, mem->step_norm,
-                            reg_param_memory, mem->funnel_width, qp_status,
+                            reg_param_memory, funnel_width_memory, qp_status,
                             qp_iter, mem->funnel_iter_type);
         }
         reg_param_memory = nlp_opts->levenberg_marquardt;
@@ -1294,6 +1295,7 @@ int ocp_nlp_sqp(void *config_, void *dims_, void *nlp_in_, void *nlp_out_,
         if (sqp_iter == 0 && nlp_opts->globalization == FUNNEL_METHOD){
             initialize_funnel(mem, opts, mem->l1_infeasibility);
         }
+        funnel_width_memory = mem->funnel_width;
 
         // regularize Hessian
         acados_tic(&timer1);
