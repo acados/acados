f4e301362d12a97c63b00aa2c94a8fa852ae465c
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-07-31 19:19:34 +0200

    use sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -109,22 +109,16 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
 html_logo = "_static/acados_logo.png"
 
+html_theme = 'sphinx_book_theme'
 html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    # 'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-    'logo_only': True,
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/acados/acados",
+    "use_repository_button": True,
+    "use_source_button": True,
+    "use_issues_button": True,
+    "show_navbar_depth": 2,
 }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
