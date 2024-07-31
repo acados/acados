4a8c0101efa4a404efbbd9291f81236403290059
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-07-31 18:32:40 +0200

    set favicon for docs page

--- docs/conf.py
+++ docs/conf.py
@@ -121,12 +121,15 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
+    'logo_only': True,
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_favicon = '_static/acados_favicon.png'
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
