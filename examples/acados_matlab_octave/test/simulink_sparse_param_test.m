d23c0f5f041a0f3be730fbfc13de21f10f4b59c1
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-07-31 10:06:14 +0200

    test: include deactivated port, and port of length 1

--- examples/acados_matlab_octave/test/simulink_sparse_param_test.m
+++ examples/acados_matlab_octave/test/simulink_sparse_param_test.m
@@ -62,7 +62,8 @@ simulink_opts.inputs.parameter_traj = 0;
 simulink_opts.outputs.parameter_traj = 1;
 simulink_opts = add_sparse_param_port_simulink(simulink_opts, 0:7, 'first_8', 0, N);
 simulink_opts = add_sparse_param_port_simulink(simulink_opts, [42, 43], 'p4243', 0, N);
-% simulink_opts = add_sparse_param_port_simulink(simulink_opts, 12, 'p8_stage3', 3, 3);
+simulink_opts = add_sparse_param_port_simulink(simulink_opts, 12, 'p12_stage3', 3, 3);
+simulink_opts = add_sparse_param_port_simulink(simulink_opts, 12, 'p12_stage6', 6, 6);
 
 
 %% create ocp solver
@@ -72,9 +73,14 @@ ocp_solver = acados_ocp(ocp_model, ocp_opts, simulink_opts);
 %% test values
 input_update_port_first8 = ones(8*(N+1) + 1, 1);
 input_update_port_p4243 = [1; 8*ones(2*(N+1), 1)];
+input_update_port_p12_stage3 = [1; 333];
+input_update_port_p12_stage6 = [0; 5000];
+
 
 ocp_solver.set_params_sparse(0:7, input_update_port_first8(2:9))
 ocp_solver.set_params_sparse([42, 43], [8, 8]);
+ocp_solver.set_params_sparse([12], input_update_port_p12_stage3(end), 3);
+
 
 
 p_matlab = zeros(np, N+1);
@@ -85,6 +91,8 @@ end
 p_ref = zeros(np, N+1);
 p_ref(1:8, :) = 1;
 p_ref([43, 44], :) = 8;
+p_ref([13], 3+1) = input_update_port_p12_stage3(end);
+
 
 if any(any(p_ref ~= p_matlab))
     disp('Setting sparse parameters in Matlab does NOT work as expected.');
