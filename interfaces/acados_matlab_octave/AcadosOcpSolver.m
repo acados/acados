962bf92d1f9dd51dceecfd155cd776f6f13d581a
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-08-01 18:28:07 +0200

    add 'static' function in constructor

--- interfaces/acados_matlab_octave/AcadosOcpSolver.m
+++ interfaces/acados_matlab_octave/AcadosOcpSolver.m
@@ -53,9 +53,6 @@ classdef AcadosOcpSolver < handle
             % TODO where do we get these from
             gnsf_transcription_opts = struct();
 
-            [~,~] = mkdir(output_dir);
-            addpath(output_dir);
-
             % detect dimensions & sanity checks
             obj.ocp = ocp;
             obj.ocp.model.make_consistent(obj.ocp.dims);
@@ -116,10 +113,42 @@ classdef AcadosOcpSolver < handle
                 end
             end
 
+            obj._compile_mex_interface_if_needed(output_dir)
+
+            % check for unsupported options:
+            if strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_OSQP") || ...
+                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_HPMPC") || ...
+                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_QPDUNES") || ...
+                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_OOQP")
+                if obj.ocp.dims.ns > 0 || obj.ocp.dims.ns_e > 0
+                    error(['selected QP solver ', obj.ocp.solver_options.nlp_solver_type, ' does not support soft constraints (yet).'])
+                end
+            end
+
+            % generate templated solver
+            ocp_generate_c_code(obj.ocp);
+
+            % templated MEX
+            return_dir = pwd();
+            cd(obj.ocp.code_export_directory)
+
+            mex_solver_name = sprintf('%s_mex_solver', obj.ocp.model.name);
+            mex_solver = str2func(mex_solver_name);
+            obj.t_ocp = mex_solver();
+            addpath(pwd());
+
+            cd(return_dir);
+
+        end
+
+
+        function _compile_mex_interface_if_needed(obj, output_dir)
 
             % check if path contains spaces
+            [~,~] = mkdir(output_dir);
+            addpath(output_dir);
             if ~isempty(strfind(output_dir, ' '))
-                error(strcat('acados_ocp: Path should not contain spaces, got: ',...
+                error(strcat('AcadosOcpSolver: Path should not contain spaces, got: ',...
                     output_dir));
             end
 
@@ -166,33 +195,8 @@ classdef AcadosOcpSolver < handle
             else
                 disp('found compiled acados MEX interface')
             end
-
-
-            % check for unsupported options:
-            if strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_OSQP") || ...
-                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_HPMPC") || ...
-                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_QPDUNES") || ...
-                strcmp(obj.ocp.solver_options.nlp_solver_type, "PARTIAL_CONDENSING_OOQP")
-                if obj.ocp.dims.ns > 0 || obj.ocp.dims.ns_e > 0
-                    error(['selected QP solver ', obj.ocp.solver_options.nlp_solver_type, ' does not support soft constraints (yet).'])
-                end
-            end
-
-            % generate templated solver
-            ocp_generate_c_code(obj.ocp);
-
-            % templated MEX
-            return_dir = pwd();
-            cd(obj.ocp.code_export_directory)
-
-            mex_solver_name = sprintf('%s_mex_solver', obj.ocp.model.name);
-            mex_solver = str2func(mex_solver_name);
-            obj.t_ocp = mex_solver();
-            addpath(pwd());
-
-            cd(return_dir);
-
         end
+
 
 
         function solve(obj)
