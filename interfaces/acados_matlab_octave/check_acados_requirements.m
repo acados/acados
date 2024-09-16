f5ed94c9e00f674d5821db0ab9f814c27be9d557
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-09-16 16:37:27 +0200

    automatically download casadi se in MATLAB

--- interfaces/acados_matlab_octave/check_acados_requirements.m
+++ interfaces/acados_matlab_octave/check_acados_requirements.m
@@ -36,8 +36,8 @@ function check_acados_requirements(varargin)
             error('Please set up CasADi yourself and try again.');
         end
         % download CasADi
-        CasADi_version = '3.6.6'; % NOTE: this needs to be set/updated manually
-        CasADi_release = '3.6.6'; % NOTE: this needs to be set/updated manually
+        CasADi_version = 'se'; % NOTE: this needs to be set/updated manually
+        CasADi_release = 'nightly-se'; % NOTE: this needs to be set/updated manually
         later_than_36 = true; % NOTE: this needs to be set/updated manually
 
         url = strcat('https://github.com/casadi/casadi/releases/download/',...
