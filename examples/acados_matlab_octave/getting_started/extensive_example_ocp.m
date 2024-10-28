98ef47001c6399199c4d9499322978ce339d69c1
Josip Kir Hromatko <josipkh@gmail.com>
2024-10-28 16:45:13 +0100

    fix typo in example comment

--- examples/acados_matlab_octave/getting_started/extensive_example_ocp.m
+++ examples/acados_matlab_octave/getting_started/extensive_example_ocp.m
@@ -259,7 +259,7 @@ cost_val_ocp = ocp_solver.get_cost();
 %        |----- dynamics -----|------ cost --------|---------------------------- constraints ------------------------|
 fields = {'qp_A','qp_B','qp_b','qp_R','qp_Q','qp_r','qp_C','qp_D','qp_lg','qp_ug','qp_lbx','qp_ubx','qp_lbu','qp_ubu'};
 
-% eiter stage-wise ...
+% either stage-wise ...
 for stage = [0,N-1]
     for k = 1:length(fields)
         field = fields{k};
