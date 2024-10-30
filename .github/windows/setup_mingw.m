af63a8d24ae3a0c480b9bf92c794b9ad0f901c1c
sandmaennchen <katrin.baumgaertner@posteo.de>
2024-10-30 14:33:58 +0100

    call setup_mingw again

--- .github/windows/setup_mingw.m
+++ .github/windows/setup_mingw.m
@@ -1,5 +1,3 @@
 
 setenv("MW_MINGW64_LOC", "C:\ProgramData\mingw64\mingw64");
-% setenv("MW_MINGW64_LOC", "C:\tools\mingw64\bin");
-% setenv("MW_MINGW64_LOC",getenv("MW_MINGW64_LOC") + ";C:\ProgramData\mingw64\mingw64\bin");
 mex -v -setup
