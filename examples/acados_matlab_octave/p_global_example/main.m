a993e93a50dc0f0958d735ef844dedb0f24c853d
sandmaennchen <katrin.baumgaertner@posteo.de>
2024-11-05 13:02:22 +0100

    set mocp name

--- examples/acados_matlab_octave/p_global_example/main.m
+++ examples/acados_matlab_octave/p_global_example/main.m
@@ -220,8 +220,8 @@ function [state_trajectories, timing] = run_example_mocp(lut, use_p_global, blaz
 
     % MOCP formulation
     mocp = create_mocp_formulation(p_global, m, l, coefficients, knots, lut, use_p_global, p_global_values, blazing);
-    mocp.model.name = ['blz' mat2str(blazing) '_pglbl_' mat2str(use_p_global) '_lut_' mat2str(lut)];
-    mocp.json_file = ['mocp_' ocp.model.name '.json'];
+    mocp.name = ['blz' mat2str(blazing) '_pglbl_' mat2str(use_p_global) '_lut_' mat2str(lut)];
+    mocp.json_file = ['mocp_' mocp.name '.json'];
 
     % MOCP solver
     mocp_solver = AcadosOcpSolver(mocp);
