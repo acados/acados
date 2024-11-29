643089c162737879e81be1ce89291804a706a0b7
d.kiessling <david.kiessling@kuleuven.be>
2024-11-29 15:10:34 +0100

    Changed description

--- examples/acados_matlab_octave/convex_problem_globalization_needed/convex_problem_globalization_necessary.m
+++ examples/acados_matlab_octave/convex_problem_globalization_needed/convex_problem_globalization_necessary.m
@@ -27,10 +27,11 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.;
 
-% Simplest NLP with Marathos effect
+% Simplest Convex NLP where full-step SQP fails
 %
 % min log(exp(x) + exp(-x))
 %
+% Solution is x* = 0.0
 
 
 import casadi.*
