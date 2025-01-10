d6865bd02da0350daa290e6b35109086dc6fe899
Katrin Baumgaertner <katrin.baumgaertner@posteo.de>
2025-01-10 12:44:14 +0100

    MATLAB: Migrate test to new interface (#1411)

--- examples/acados_matlab_octave/test/simulink_sparse_param_test.m
+++ examples/acados_matlab_octave/test/simulink_sparse_param_test.m
@@ -39,39 +39,38 @@ N = 20; % number of discretization steps
 nx = 3;
 nu = 3;
 np = 100;
-[ocp_model, ocp_opts, simulink_opts, x0] = create_parametric_ocp_qp(N, np);
+[ocp, x0] = create_parametric_ocp_qp(N, np);
+
 % NOTE: here we don't perform iterations and just test initialization
 % functionality
-ocp_opts.set('nlp_solver_max_iter', 0);
+ocp.solver_options.nlp_solver_max_iter = 0;
 
 % deactivate ports.
-simulink_opts.inputs.lbx_0 = 0;
-simulink_opts.inputs.ubx_0 = 0;
-simulink_opts.inputs.lbx = 0;
-simulink_opts.inputs.ubx = 0;
-simulink_opts.inputs.reset_solver = 0;
-simulink_opts.inputs.x_init = 0;
-simulink_opts.inputs.u_init = 0;
-simulink_opts.inputs.pi_init = 0;
-simulink_opts.inputs.ignore_inits = 0;
-simulink_opts.outputs.pi_all = 0;
-simulink_opts.outputs.sqp_iter = 0;
+ocp.simulink_opts.inputs.lbx_0 = 0;
+ocp.simulink_opts.inputs.ubx_0 = 0;
+ocp.simulink_opts.inputs.lbx = 0;
+ocp.simulink_opts.inputs.ubx = 0;
+ocp.simulink_opts.inputs.reset_solver = 0;
+ocp.simulink_opts.inputs.x_init = 0;
+ocp.simulink_opts.inputs.u_init = 0;
+ocp.simulink_opts.inputs.pi_init = 0;
+ocp.simulink_opts.inputs.ignore_inits = 0;
+ocp.simulink_opts.outputs.pi_all = 0;
+ocp.simulink_opts.outputs.sqp_iter = 0;
 
 % parameter ports
-simulink_opts.inputs.parameter_traj = 0;
-simulink_opts.outputs.parameter_traj = 1;
+ocp.simulink_opts.inputs.parameter_traj = 0;
+ocp.simulink_opts.outputs.parameter_traj = 1;
 % define multiple ports for sparse parameter update:
 % Usage:
-% add_sparse_param_port_simulink(simulink_opts, idx_p, port_name, stage_idx_0, stage_idx_e)
-simulink_opts = add_sparse_param_port_simulink(simulink_opts, 0:7, 'first_8', 0, N);
-simulink_opts = add_sparse_param_port_simulink(simulink_opts, [42, 43], 'p4243', 0, N);
-simulink_opts = add_sparse_param_port_simulink(simulink_opts, 12, 'p12_stage3', 3, 3);
-simulink_opts = add_sparse_param_port_simulink(simulink_opts, 12, 'p12_stage6', 6, 6);
-
+% add_sparse_param_port_simulink(ocp.simulink_opts, idx_p, port_name, stage_idx_0, stage_idx_e)
+ocp.simulink_opts = add_sparse_param_port_simulink(ocp.simulink_opts, 0:7, 'first_8', 0, N);
+ocp.simulink_opts = add_sparse_param_port_simulink(ocp.simulink_opts, [42, 43], 'p4243', 0, N);
+ocp.simulink_opts = add_sparse_param_port_simulink(ocp.simulink_opts, 12, 'p12_stage3', 3, 3);
+ocp.simulink_opts = add_sparse_param_port_simulink(ocp.simulink_opts, 12, 'p12_stage6', 6, 6);
 
 %% create ocp solver
-ocp_solver = acados_ocp(ocp_model, ocp_opts, simulink_opts);
-
+ocp_solver = AcadosOcpSolver(ocp);
 
 %% test values
 input_update_port_first8 = ones(8*(N+1) + 1, 1);
@@ -79,12 +78,9 @@ input_update_port_p4243 = [1; 8*ones(2*(N+1), 1)];
 input_update_port_p12_stage3 = [1; 333];
 input_update_port_p12_stage6 = [0; 5000];
 
-
 ocp_solver.set_params_sparse(0:7, input_update_port_first8(2:9))
 ocp_solver.set_params_sparse([42, 43], [8, 8]);
 ocp_solver.set_params_sparse([12], input_update_port_p12_stage3(end), 3);
-
-
 
 p_matlab = zeros(np, N+1);
 for i=0:N
@@ -95,7 +91,6 @@ p_ref = zeros(np, N+1);
 p_ref(1:8, :) = 1;
 p_ref([43, 44], :) = 8;
 p_ref([13], 3+1) = input_update_port_p12_stage3(end);
-
 
 if any(any(p_ref ~= p_matlab))
     disp('Setting sparse parameters in Matlab does NOT work as expected.');
