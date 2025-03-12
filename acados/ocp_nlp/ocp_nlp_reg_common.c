283b6b1c343a0875c514c73e44960f944213f719
d.kiessling <david.kiessling@kuleuven.be>
2025-03-12 15:48:02 +0100

    Updated adaptive project

--- acados/ocp_nlp/ocp_nlp_reg_common.c
+++ acados/ocp_nlp/ocp_nlp_reg_common.c
@@ -256,14 +256,17 @@ void acados_project_adaptive_eps(int dim, double *A, double *V, double *d, doubl
     double max_eig = 0.0;
     double min_eig = ACADOS_INFTY;
     double eps;
+    int eig_0_count = 0;
 
     // compute max and min eigenvalues
     for (i=0; i < dim; i++)
     {
-        max_eig = MAX(max_eig, fabs(d[i]));
+        max_eig = MAX(max_eig, d[i]); // negative eigenvalues are clipped, i.e., not important for condition number
         min_eig = MIN(min_eig, fabs(d[i]));
+        if (fabs(d[i]) == 0.0)
+            eig_0_count += 1;
     }
-    if (min_eig == 0.0 && max_eig == 0.0)
+    if (eig_0_count == dim)
         eps = 1.0;
     else
         eps = max_eig/max_cond_block;
