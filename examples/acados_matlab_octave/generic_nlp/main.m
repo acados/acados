b61233ecfadca1c73a4f2306424de4e53351313e
Confectio <leonard.fichtner@gmail.com>
2025-04-10 17:28:09 +0200

    Updated matlab example to N=0

--- examples/acados_matlab_octave/generic_nlp/main.m
+++ examples/acados_matlab_octave/generic_nlp/main.m
@@ -47,7 +47,6 @@ model = AcadosModel();
 model.name = 'generic_nlp';
 model.x = x;
 model.p = p;
-model.f_expl_expr = casadi.SX.zeros(length(model.x),1);
 
 %% acados ocp formulation
 ocp = AcadosOcp();
@@ -66,19 +65,10 @@ ocp.constraints.uh_e = gub;
 % initial parameter values
 ocp.parameter_values = zeros(length(model.p),1);
 
-% set additional fields to prevent errors/warnings
-ocp.cost.cost_type_0 = 'EXTERNAL';
-ocp.model.cost_expr_ext_cost_0 = 0;
-ocp.cost.cost_type = 'EXTERNAL';
-ocp.model.cost_expr_ext_cost = 0;
-
 %% solver options
-ocp.solver_options.tf = 1;
-ocp.solver_options.N_horizon = 1;
+ocp.solver_options.N_horizon = 0;
 ocp.solver_options.nlp_solver_type = 'SQP';
-ocp.solver_options.integrator_type = 'ERK';
-ocp.solver_options.sim_method_num_stages = 1;
-ocp.solver_options.sim_method_num_steps = 1;
+ocp.solver_options.qp_solver = 'FULL_CONDENSING_HPIPM'; %TODO: Change after PARTIAL_CONDENSING_HPIPM fix 
 
 %% create the solver
 ocp_solver = AcadosOcpSolver(ocp);
