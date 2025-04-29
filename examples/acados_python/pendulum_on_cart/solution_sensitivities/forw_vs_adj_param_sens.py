df83095939630166987107c643f92cd828f4282a
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-04-29 09:43:28 +0200

    forw_vs_adj: cleanup, remove cython, remove hp_sens_solver option

--- examples/acados_python/pendulum_on_cart/solution_sensitivities/forw_vs_adj_param_sens.py
+++ examples/acados_python/pendulum_on_cart/solution_sensitivities/forw_vs_adj_param_sens.py
@@ -34,7 +34,7 @@ from sensitivity_utils import export_parametric_ocp, plot_pendulum
 
 TOL = 1e-6
 
-def main(qp_solver_ric_alg: int, use_cython=False, generate_solvers=True, plot_trajectory=False):
+def main(qp_solver_ric_alg: int, generate_solvers=True, plot_trajectory=False):
     """
     Evaluate policy and calculate its gradient for the pendulum on a cart with a parametric model.
     """
@@ -61,47 +61,15 @@ def main(qp_solver_ric_alg: int, use_cython=False, generate_solvers=True, plot_t
     with_nonlinear_constraint = True
 
     ocp = export_parametric_ocp(x0=x0, N_horizon=N_horizon, T_horizon=T_horizon, Fmax=Fmax, qp_solver_ric_alg=1, cost_scale_as_param=cost_scale_as_param, with_parametric_constraint=with_parametric_constraint, with_nonlinear_constraint=with_nonlinear_constraint)
-    if use_cython:
-        raise NotImplementedError()
-        AcadosOcpSolver.generate(ocp, json_file="parameter_augmented_acados_ocp.json")
-        AcadosOcpSolver.build(ocp.code_export_directory, with_cython=True)
-        ocp_solver = AcadosOcpSolver.create_cython_solver("parameter_augmented_acados_ocp.json")
-    else:
-        ocp_solver = AcadosOcpSolver(ocp, json_file="parameter_augmented_acados_ocp.json", generate=generate_solvers, build=generate_solvers)
+    ocp_solver = AcadosOcpSolver(ocp, json_file="parameter_augmented_acados_ocp.json", generate=generate_solvers, build=generate_solvers)
 
     # create sensitivity solver
     ocp = export_parametric_ocp(x0=x0, N_horizon=N_horizon, T_horizon=T_horizon, Fmax=Fmax, hessian_approx='EXACT', qp_solver_ric_alg=qp_solver_ric_alg, cost_scale_as_param=cost_scale_as_param, with_parametric_constraint=with_parametric_constraint, with_nonlinear_constraint=with_nonlinear_constraint)
     ocp.model.name = 'sensitivity_solver'
     ocp.code_export_directory = f'c_generated_code_{ocp.model.name}'
 
-    hp_sens_solver = True
-    if hp_sens_solver:
-        original_ocp = ocp_solver.acados_ocp
-        # undo some settings that are not needed for HP sens solver
-        ocp.solver_options.globalization_fixed_step_length = 1.0
-        ocp.solver_options.nlp_solver_max_iter = original_ocp.solver_options.nlp_solver_max_iter
-        # to "force" a QP solve
-        ocp.solver_options.tol = original_ocp.solver_options.tol
-        ocp.solver_options.qp_tol = original_ocp.solver_options.tol
-        ocp.solver_options.nlp_solver_max_iter = original_ocp.solver_options.nlp_solver_max_iter
-        # QP warm start
-        # ocp.solver_options.qp_solver_warm_start = 3
-        # ocp.solver_options.nlp_solver_warm_start_first_qp = True
-        # ocp.solver_options.nlp_solver_warm_start_first_qp_from_nlp = True
-        # # HPIPM settings
-        # ocp.solver_options.qp_solver_iter_max = 0
-        # ocp.remove_x0_elimination()
-
-        sensitivity_solver = AcadosOcpSolver(ocp, json_file=f"{ocp.model.name}.json", generate=generate_solvers, build=generate_solvers)
-    else:
-        if use_cython:
-            AcadosOcpSolver.generate(ocp, json_file=f"{ocp.model.name}.json")
-            AcadosOcpSolver.build(ocp.code_export_directory, with_cython=True)
-            sensitivity_solver = AcadosOcpSolver.create_cython_solver(f"{ocp.model.name}.json")
-        else:
-            ocp.solver_options.nlp_solver_warm_start_first_qp = True
-            ocp.solver_options.qp_solver_warm_start = 2
-            sensitivity_solver = AcadosOcpSolver(ocp, json_file=f"{ocp.model.name}.json", generate=generate_solvers, build=generate_solvers)
+    # undo some settings that are not needed for HP sens solver
+    sensitivity_solver = AcadosOcpSolver(ocp, json_file=f"{ocp.model.name}.json", generate=generate_solvers, build=generate_solvers)
 
     # set parameter value
     if cost_scale_as_param:
@@ -110,14 +78,12 @@ def main(qp_solver_ric_alg: int, use_cython=False, generate_solvers=True, plot_t
         p_val = np.array([p_test])
 
     ocp_solver.set_p_global_and_precompute_dependencies(p_val)
-
     sensitivity_solver.set_p_global_and_precompute_dependencies(p_val)
 
+    # nominal solve
     u_opt = ocp_solver.solve_for_x0(x0)[0]
-
     tau_iter = ocp_solver.get_stats("qp_tau_iter")
     print(f"qp tau iter: {tau_iter}\n")
-    iterate = ocp_solver.store_iterate_to_obj()
 
     if with_parametric_constraint:
         lambdas = np.zeros((N_horizon-1, 2))
@@ -128,27 +94,25 @@ def main(qp_solver_ric_alg: int, use_cython=False, generate_solvers=True, plot_t
         max_lam = np.max(np.abs(lambdas))
         print(f"max lambda of parametric constraints: {max_lam:.2f}\n")
 
+    # transfer iterate to sensitivity solver
+    iterate = ocp_solver.store_iterate_to_obj()
     sensitivity_solver.load_iterate_from_obj(iterate)
 
-    if hp_sens_solver:
-        # sensitivity_solver.solve_for_x0(x0, fail_on_nonzero_status=False, print_stats_on_failure=False)
-        sensitivity_solver.setup_qp_matrices_and_factorize()
+    # setup QP matrices and factorize
+    sensitivity_solver.setup_qp_matrices_and_factorize()
 
-    else:
-        sensitivity_solver.solve_for_x0(x0, fail_on_nonzero_status=False, print_stats_on_failure=False)
-
-    sensitivity_solver.print_statistics()
     qp_iter = sensitivity_solver.get_stats("qp_iter")
     print(f"qp iter: {qp_iter}\n")
 
-    for i in range(1, N_horizon-1):
-        P_mat = sensitivity_solver.get_from_qp_in(i, "P")
-        K_mat = sensitivity_solver.get_from_qp_in(i, "K")
-        Lr_mat = sensitivity_solver.get_from_qp_in(i, "Lr")
-        print(f"stage {i} got factorization")
-        # print(f"P_mat = {P_mat}")
-        # print(f"K_mat = {K_mat}")
-        print(f"Lr_mat = {Lr_mat}")
+    # check factorization
+    # for i in range(1, N_horizon-1):
+    #     P_mat = sensitivity_solver.get_from_qp_in(i, "P")
+    #     K_mat = sensitivity_solver.get_from_qp_in(i, "K")
+    #     Lr_mat = sensitivity_solver.get_from_qp_in(i, "Lr")
+    #     print(f"stage {i} got factorization")
+    #     # print(f"P_mat = {P_mat}")
+    #     # print(f"K_mat = {K_mat}")
+    #     print(f"Lr_mat = {Lr_mat}")
 
     if sensitivity_solver.get_status() not in [0, 2]:
         breakpoint()
@@ -243,4 +207,4 @@ def test_failure_message(msg):
     raise Exception(msg)
 
 if __name__ == "__main__":
-    main(qp_solver_ric_alg=0, use_cython=False, generate_solvers=True, plot_trajectory=False)
+    main(qp_solver_ric_alg=0, generate_solvers=False, plot_trajectory=False)
