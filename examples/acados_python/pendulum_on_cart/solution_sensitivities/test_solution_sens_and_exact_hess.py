e53292d6b284e444cd2bfabf782de6b0b571a7dc
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-04-29 10:35:16 +0200

    use faster store and load

--- examples/acados_python/pendulum_on_cart/solution_sensitivities/test_solution_sens_and_exact_hess.py
+++ examples/acados_python/pendulum_on_cart/solution_sensitivities/test_solution_sens_and_exact_hess.py
@@ -248,8 +248,8 @@ def sensitivity_experiment(linearized_dynamics=False, discrete=False, show=True)
     for i, p0 in enumerate(p_vals):
         x0[idxp] = p0
         u0 = acados_ocp_solver_gn.solve_for_x0(x0)
-        acados_ocp_solver_gn.store_iterate(filename='iterate.json', overwrite=True, verbose=False)
-        acados_ocp_solver_exact.load_iterate(filename='iterate.json', verbose=False)
+        iterate = acados_ocp_solver_gn.store_iterate_to_flat_obj()
+        acados_ocp_solver_exact.load_iterate_from_flat_obj(iterate)
         acados_ocp_solver_exact.set(0, 'u', u0+1e-7)
         acados_ocp_solver_exact.solve_for_x0(x0, fail_on_nonzero_status=False, print_stats_on_failure=False)
 
@@ -261,7 +261,6 @@ def sensitivity_experiment(linearized_dynamics=False, discrete=False, show=True)
 
         residuals = acados_ocp_solver_exact.get_stats("residuals")
         print(f"residuals sensitivity_solver {residuals}")
-
 
         # solve with casadi and compare hessians
         nlp_sol = casadi_solver(p=x0, lbg=lbg, ubg=ubg)
@@ -392,8 +391,8 @@ def run_hessian_comparison(linearized_dynamics=False, discrete=False):
     casadi_hess_l = lag_hess_fun(x=nlp_sol['x'], p=x0, lam_f=1.0, lam_g=nlp_sol['lam_g'])['triu_hess_gamma_x_x']
     casadi_hess = ca.triu2symm(ca.triu(casadi_hess_l)).full()
     acados_ocp_solver_gn.solve_for_x0(x0)
-    acados_ocp_solver_gn.store_iterate(filename='iterate.json', overwrite=True)
-    acados_ocp_solver_exact.load_iterate(filename='iterate.json')
+    iterate = acados_ocp_solver_gn.store_iterate_to_flat_obj()
+    acados_ocp_solver_exact.load_iterate_from_flat_obj(iterate)
     acados_ocp_solver_exact.solve_for_x0(x0, fail_on_nonzero_status=False, print_stats_on_failure=False)
 
     _ = compare_hessian(casadi_hess, acados_ocp_solver_exact)
