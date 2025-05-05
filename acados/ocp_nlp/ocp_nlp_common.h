128bcafa5f7e4a2ca4c524023d1c14d56f7d4491
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-05-05 10:52:48 +0200

    remove tmp_qp_in

--- acados/ocp_nlp/ocp_nlp_common.h
+++ acados/ocp_nlp/ocp_nlp_common.h
@@ -494,8 +494,7 @@ typedef struct ocp_nlp_workspace
     void **cost;         // cost_workspace
     void **constraints;  // constraints_workspace
 
-    // temp QP in & out (to be used as workspace in param sens) and merit line search
-    ocp_qp_in *tmp_qp_in;
+    // temp QP out
     ocp_qp_out *tmp_qp_out;
 
     // qp residuals
