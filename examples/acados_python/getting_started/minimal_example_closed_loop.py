bf512fa19f582cd69fe351c7b63b1191c0335a82
Confectio <leonard.fichtner@gmail.com>
2025-09-03 13:35:47 +0200

    made example more minimal

--- examples/acados_python/getting_started/minimal_example_closed_loop.py
+++ examples/acados_python/getting_started/minimal_example_closed_loop.py
@@ -36,7 +36,7 @@ import numpy as np
 import scipy.linalg
 from casadi import vertcat
 
-def setup(x0, Fmax, N_horizon, Tf, RTI=False):
+def setup(x0, Fmax, N_horizon, Tf):
     # create ocp object to formulate the OCP
     ocp = AcadosOcp()
 
@@ -81,12 +81,9 @@ def setup(x0, Fmax, N_horizon, Tf, RTI=False):
     ocp.solver_options.integrator_type = 'IRK'
     ocp.solver_options.sim_method_newton_iter = 10
 
-    if RTI:
-        ocp.solver_options.nlp_solver_type = 'SQP_RTI'
-    else:
-        ocp.solver_options.nlp_solver_type = 'SQP'
-        ocp.solver_options.globalization = 'MERIT_BACKTRACKING' # turns on globalization
-        ocp.solver_options.nlp_solver_max_iter = 150
+    ocp.solver_options.nlp_solver_type = 'SQP'
+    ocp.solver_options.globalization = 'MERIT_BACKTRACKING' # turns on globalization
+    ocp.solver_options.nlp_solver_max_iter = 150
 
     ocp.solver_options.qp_solver_cond_N = N_horizon
 
@@ -100,7 +97,7 @@ def setup(x0, Fmax, N_horizon, Tf, RTI=False):
     return acados_ocp_solver, acados_integrator
 
 
-def main(use_RTI=False):
+def main():
 
     x0 = np.array([0.0, np.pi, 0.0, 0.0])
     Fmax = 80
@@ -108,7 +105,7 @@ def main(use_RTI=False):
     Tf = .8
     N_horizon = 40
 
-    ocp_solver, integrator = setup(x0, Fmax, N_horizon, Tf, use_RTI)
+    ocp_solver, integrator = setup(x0, Fmax, N_horizon, Tf)
 
     nx = ocp_solver.acados_ocp.dims.nx
     nu = ocp_solver.acados_ocp.dims.nu
@@ -119,60 +116,23 @@ def main(use_RTI=False):
 
     simX[0,:] = x0
 
-    if use_RTI:
-        t_preparation = np.zeros((Nsim))
-        t_feedback = np.zeros((Nsim))
-
-    else:
-        t = np.zeros((Nsim))
-
-    # do some initial iterations to start with a good initial guess
-    num_iter_initial = 5
-    for _ in range(num_iter_initial):
-        ocp_solver.solve_for_x0(x0_bar = x0)
+    t = np.zeros((Nsim))
 
     # closed loop
     for i in range(Nsim):
 
-        if use_RTI:
-            # preparation phase
-            ocp_solver.options_set('rti_phase', 1)
-            status = ocp_solver.solve()
-            t_preparation[i] = ocp_solver.get_stats('time_tot')
+        # solve ocp and get next control input
+        simU[i,:] = ocp_solver.solve_for_x0(x0_bar = simX[i, :])
 
-            # set initial state
-            ocp_solver.set(0, "lbx", simX[i, :])
-            ocp_solver.set(0, "ubx", simX[i, :])
-
-            # feedback phase
-            ocp_solver.options_set('rti_phase', 2)
-            status = ocp_solver.solve()
-            t_feedback[i] = ocp_solver.get_stats('time_tot')
-
-            simU[i, :] = ocp_solver.get(0, "u")
-
-        else:
-            # solve ocp and get next control input
-            simU[i,:] = ocp_solver.solve_for_x0(x0_bar = simX[i, :])
-
-            t[i] = ocp_solver.get_stats('time_tot')
+        t[i] = ocp_solver.get_stats('time_tot')
 
         # simulate system
         simX[i+1, :] = integrator.simulate(x=simX[i, :], u=simU[i,:])
 
     # evaluate timings
-    if use_RTI:
-        # scale to milliseconds
-        t_preparation *= 1000
-        t_feedback *= 1000
-        print(f'Computation time in preparation phase in ms: \
-                min {np.min(t_preparation):.3f} median {np.median(t_preparation):.3f} max {np.max(t_preparation):.3f}')
-        print(f'Computation time in feedback phase in ms:    \
-                min {np.min(t_feedback):.3f} median {np.median(t_feedback):.3f} max {np.max(t_feedback):.3f}')
-    else:
-        # scale to milliseconds
-        t *= 1000
-        print(f'Computation time in ms: min {np.min(t):.3f} median {np.median(t):.3f} max {np.max(t):.3f}')
+    # scale to milliseconds
+    t *= 1000
+    print(f'Computation time in ms: min {np.min(t):.3f} median {np.median(t):.3f} max {np.max(t):.3f}')
 
     # plot results
     model = ocp_solver.acados_ocp.model
@@ -182,5 +142,4 @@ def main(use_RTI=False):
 
 
 if __name__ == '__main__':
-    main(use_RTI=False)
-    main(use_RTI=True)
+    main()
