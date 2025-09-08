cc1f309ecbf70cdbcba9930c5f77c4f03444e3a1
Josua Lindemann <josua.lindemann@outlook.de>
2025-09-08 19:02:02 +0200

    changed ros installation again and added ros build dep

--- .github/linux/install_ros2_humble.sh
+++ .github/linux/install_ros2_humble.sh
@@ -28,26 +28,31 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.;
 #
+if [ ! -d "/opt/ros/humble/setup.bash" ]; then
+    echo "ROS 2 not found in cache. Starting fresh installation."
+    # system update
+    sudo apt update
+    sudo apt install -y locales
+    sudo locale-gen en_US en_US.UTF-8
+    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository universe
 
-# system update
-sudo apt update
-sudo apt install -y locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-sudo apt install -y software-properties-common
-sudo add-apt-repository universe
+    # add ros 2 repo
+    sudo apt install -y curl
+    sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# add ros 2 repo
-sudo apt install -y curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+    # install ros 2
+    sudo apt update
+    sudo apt install -y ros-humble-ros-base ros-dev-tools
+else
+    echo "ROS 2 already installed"
+fi
 
-# install ros 2
-sudo apt update
-sudo apt install -y ros-humble-ros-base ros-dev-tools
-
-# install and initialize rosdep
-sudo apt install -y python3-rosdep
-sudo rosdep init
+echo "Initializing and updating rosdep..."
+sudo apt-get update
+sudo apt-get install -y python3-rosdep
+sudo rosdep init || echo "rosdep already initialized."
 rosdep update
