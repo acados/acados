c0f68482cf5bb21930797b18a4a750d0a81a1f63
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-10-02 17:31:50 +0200

    Fixes related to `idxs_rev` in interfaces (#1650)

--- interfaces/acados_matlab_octave/ns_from_idxs_rev.m
+++ interfaces/acados_matlab_octave/ns_from_idxs_rev.m
@@ -1,7 +1,12 @@
-function n = ns_from_idxs_rev(idxs_rev)
+function ns = ns_from_idxs_rev(idxs_rev)
     if isempty(idxs_rev)
-        n = 0;
+        ns = 0;
     else
-        n = max(idxs_rev) + 1;
+        ns = max(idxs_rev) + 1;
+        for i=0:ns-1
+            if ~ismember(i, idxs_rev)
+                error(['Detected ns = ', num2str(ns), ', but i = ', num2str(i), ' is not in idxs_rev = [', num2str(idxs_rev), '], the slack with index ', num2str(i), ' is thus not contained in the problem.']);
+            end
+        end
     end
 end
