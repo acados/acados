dde9351aa16081b92d7cc2e2d4c4b9d0151e838b
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-10-06 11:59:51 +0200

    reset minimal example

--- examples/acados_python/getting_started/minimal_example_ocp.py
+++ examples/acados_python/getting_started/minimal_example_ocp.py
@@ -75,7 +75,7 @@ def main():
     ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
 
     # set options
-    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_CLARABEL' # FULL_CONDENSING_QPOASES
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
     # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
     # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
     ocp.solver_options.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
@@ -83,8 +83,6 @@ def main():
     # ocp.solver_options.print_level = 1
     ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
     ocp.solver_options.globalization = 'MERIT_BACKTRACKING' # turns on globalization
-    ocp.solver_options.qp_solver_iter_max = 1000
-    ocp.solver_options.nlp_solver_ext_qp_res = 1
 
     ocp_solver = AcadosOcpSolver(ocp)
 
