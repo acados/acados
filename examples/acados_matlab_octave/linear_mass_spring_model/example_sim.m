a2877c653f4e18dc7ecbd0b542a09198e3210a2f
Jingtao Xiong <1450062579@qq.com>
2025-11-10 15:22:16 +0100

    update file in linear_mass_spring_model

--- examples/acados_matlab_octave/linear_mass_spring_model/example_sim.m
+++ examples/acados_matlab_octave/linear_mass_spring_model/example_sim.m
@@ -27,72 +27,26 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.;
 
-
-
-% NOTE: `acados` currently supports both an old MATLAB/Octave interface (< v0.4.0)
-% as well as a new interface (>= v0.4.0).
-
-% THIS EXAMPLE still uses the OLD interface. If you are new to `acados` please start
-% with the examples that have been ported to the new interface already.
-% see https://github.com/acados/acados/issues/1196#issuecomment-2311822122)
-
-
 clear all; clc;
 check_acados_requirements();
 
-%% arguments
-compile_interface = 'auto';
-%method = 'erk';
-method = 'irk';
-method = 'irk_gnsf';
-gnsf_detect_struct = 'true';
-sens_forw = 'true';
-num_stages = 4;
-num_steps = 4;
-
 %% model
-model = linear_mass_spring_model();
-
-nx = model.nx;
-nu = model.nu;
-
+model = get_linear_mass_spring_model();
+nx = length(model.x);
+nu = length(model.u);
 %% acados sim model
-sim_model = acados_sim_model();
-sim_model.set('T', 0.5);
-if (strcmp(method, 'erk'))
-	sim_model.set('dyn_type', 'explicit');
-	sim_model.set('dyn_expr_f', model.dyn_expr_f_expl);
-	sim_model.set('sym_x', model.sym_x);
-	if isfield(model, 'sym_u')
-		sim_model.set('sym_u', model.sym_u);
-	end
-else % irk irk_gnsf
-	sim_model.set('dyn_type', 'implicit');
-	sim_model.set('dyn_expr_f', model.dyn_expr_f_impl);
-	sim_model.set('sym_x', model.sym_x);
-	sim_model.set('sym_xdot', model.sym_xdot);
-	if isfield(model, 'sym_u')
-		sim_model.set('sym_u', model.sym_u);
-	end
-end
-
-%% acados sim opts
-sim_opts = acados_sim_opts();
-sim_opts.set('compile_interface', compile_interface);
-sim_opts.set('num_stages', num_stages);
-sim_opts.set('num_steps', num_steps);
-sim_opts.set('method', method);
-sim_opts.set('sens_forw', sens_forw);
-if (strcmp(method, 'irk_gnsf'))
-	sim_opts.set('gnsf_detect_struct', gnsf_detect_struct);
-end
-
+sim = AcadosSim();
+sim.model = model;
+sim.solver_options.Tsim = 0.5;
+sim.solver_options.integrator_type = 'IRK';  % 'ERK', 'IRK'
+sim.solver_options.num_stages = 4;
+sim.solver_options.num_steps = 4;
+sim.solver_options.sens_forw = true;
 %% acados sim
 % create sim
-sim_solver = acados_sim(sim_model, sim_opts);
+sim_solver = AcadosSimSolver(sim);
 % (re)set numerical part of model
 %sim_solver.set('T', 0.5);
-
 
 x0 = ones(nx, 1); %x0(1) = 2.0;
 tic;
