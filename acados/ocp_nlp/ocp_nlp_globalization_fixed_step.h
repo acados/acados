7b4a5ee50ab12e092120c3a0a11c0f4b9b6bdc25
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-11-20 18:48:55 +0100

    implementation with flag

--- acados/ocp_nlp/ocp_nlp_globalization_fixed_step.h
+++ acados/ocp_nlp/ocp_nlp_globalization_fixed_step.h
@@ -73,6 +73,7 @@ void ocp_nlp_globalization_fixed_step_opts_set(void *config, void *opts, const c
 typedef struct
 {
     double alpha;  // dummy value, not used, just to have non-empty struct
+    bool anderson_acceleration_activated;
 } ocp_nlp_globalization_fixed_step_memory;
 
 //
