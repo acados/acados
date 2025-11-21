0ee9d8ed2408ce10378e98fc2f4e4bdcffebbb52
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-11-21 10:42:10 +0100

    refactor anderson experiment

--- examples/acados_python/furuta_pendulum/anderson_convergence_experiment.py
+++ examples/acados_python/furuta_pendulum/anderson_convergence_experiment.py
@@ -32,111 +32,153 @@ from furuta_common import setup_ocp_solver
 from utils import plot_furuta_pendulum
 import numpy as np
 
-from acados_template import AcadosOcpFlattenedIterate, plot_convergence, plot_contraction_rates, ACADOS_INFTY
+from acados_template import (
+    AcadosOcpSolver,
+    AcadosOcpFlattenedIterate,
+    plot_convergence,
+    plot_contraction_rates,
+    ACADOS_INFTY,
+)
 from typing import Tuple
 import matplotlib.pyplot as plt
-N_HORIZON = 8   # number of shooting intervals
-UMAX = .45
+
+N_HORIZON = 8  # number of shooting intervals
+UMAX = 0.45
 
 
-def test_solver(with_anderson_acceleration: bool,
-                variant, tol, anderson_activation_threshold,
-                with_abs_cost) -> Tuple[AcadosOcpFlattenedIterate, np.ndarray]:
+def create_solver(variant, tol, with_abs_cost):
     x0 = np.array([0.0, np.pi, 0.0, 0.0])
 
-    Tf = .350       # total prediction time
-    dt_0 = 0.025    # sampling time = length of first shooting interval
+    Tf = 0.350  # total prediction time
+    dt_0 = 0.025  # sampling time = length of first shooting interval
 
-    if variant == 'GAUSS_NEWTON':
-        hessian_approx = 'GAUSS_NEWTON'
-        regularize_method = 'NO_REGULARIZE'
-    elif variant == 'EXACT':
-        hessian_approx = 'EXACT'
-        regularize_method = 'PROJECT'
-    solver = setup_ocp_solver(x0, UMAX, dt_0, N_HORIZON, Tf,
-                              with_anderson_acceleration=with_anderson_acceleration,
-                              nlp_solver_max_iter = 500, tol = tol,
-                              with_abs_cost=with_abs_cost,
-                              hessian_approx=hessian_approx, regularize_method=regularize_method, anderson_activation_threshold=anderson_activation_threshold)
+    if variant == "GAUSS_NEWTON":
+        hessian_approx = "GAUSS_NEWTON"
+        regularize_method = "NO_REGULARIZE"
+    elif variant == "EXACT":
+        hessian_approx = "EXACT"
+        regularize_method = "PROJECT"
+    solver = setup_ocp_solver(
+        x0,
+        UMAX,
+        dt_0,
+        N_HORIZON,
+        Tf,
+        nlp_solver_max_iter=500,
+        tol=tol,
+        with_abs_cost=with_abs_cost,
+        hessian_approx=hessian_approx,
+        regularize_method=regularize_method,
+        with_anderson_acceleration=True
+    )
 
-    t_grid = solver.acados_ocp.solver_options.shooting_nodes
+    return solver
 
+
+def test_solver(
+    solver: AcadosOcpSolver,
+    initial_guess: AcadosOcpFlattenedIterate,
+    anderson_activation_threshold: float,
+):
+    solver.options_set("anderson_activation_threshold", anderson_activation_threshold)
+
+    solver.load_iterate_from_flat_obj(initial_guess)
     status = solver.solve()
     solver.print_statistics()
     solution = solver.store_iterate_to_flat_obj()
 
-    res_all = solver.get_stats('res_all')
+    res_all = solver.get_stats("res_all")
     kkt_norms = np.linalg.norm(res_all, axis=1)
+    return solution, kkt_norms
 
-    return solution, kkt_norms, t_grid
 
 def raise_test_failure_message(msg: str):
     # print(f"ERROR: {msg}")
     raise Exception(msg)
 
+
 def main(
-         anderson_settings: list,
-         variant: str='GAUSS_NEWTON',
-         plot_trajectory: bool=False,
-         store_plots: bool=False,
-         plot_contraction: bool=False,
-         ignore_checks: bool=True,
-         with_abs_cost: bool=True,
-         tol: float=1e-8):
+    anderson_settings: list,
+    variant: str = "GAUSS_NEWTON",
+    plot_trajectory: bool = False,
+    store_plots: bool = False,
+    plot_contraction: bool = False,
+    ignore_checks: bool = False,
+    with_abs_cost: bool = True,
+    tol: float = 1e-8,
+):
     # test with anderson acceleration
     kkt_norm_list = []
     contraction_rates_list = []
     sol_list = []
     labels = []
-    if variant == 'GAUSS_NEWTON':
+    if variant == "GAUSS_NEWTON":
         base_label = "GN"
-    elif variant == 'EXACT':
+    elif variant == "EXACT":
         base_label = "project exact Hessian"
     else:
         raise ValueError(f"Unknown variant: {variant}")
 
-    for (with_anderson_acceleration, anderson_activation_threshold) in anderson_settings:
+    solver = create_solver(variant, tol, with_abs_cost)
 
-        sol, kkt_norms, t_grid = test_solver(with_anderson_acceleration=with_anderson_acceleration, variant=variant, tol=tol, anderson_activation_threshold=anderson_activation_threshold, with_abs_cost=with_abs_cost)
+    t_grid = solver.acados_ocp.solver_options.shooting_nodes
+    initial_guess = solver.store_iterate_to_flat_obj()
+
+    for anderson_activation_threshold in anderson_settings:
+
+        sol, kkt_norms = test_solver(
+            solver, initial_guess, anderson_activation_threshold
+        )
         # compute contraction rates
-        contraction_rates = kkt_norms[1:-1]/kkt_norms[0:-2]
+        contraction_rates = kkt_norms[1:-1] / kkt_norms[0:-2]
         # append results
         kkt_norm_list.append(kkt_norms)
         contraction_rates_list.append(contraction_rates)
         sol_list.append(sol)
-        if not with_anderson_acceleration:
+        if anderson_activation_threshold <= 0.0:
             label = base_label
         elif anderson_activation_threshold == ACADOS_INFTY:
             label = "AA(1)-" + base_label
         else:
             label = f"AA(1)-{base_label} (thresh={anderson_activation_threshold})"
         labels.append(label)
+
         # checks
         n_iter = len(kkt_norms)
         if ignore_checks:
             print("Ignoring test checks.")
             continue
-        if tol < 1e-5 and anderson_activation_threshold == ACADOS_INFTY:
+        if tol < 1e-5:
             if with_abs_cost:
-                if with_anderson_acceleration:
+                if anderson_activation_threshold == ACADOS_INFTY: # full Anderson
                     if not n_iter < 30:
-                        raise_test_failure_message(f"Expected less than 30 iterations with Anderson acceleration, got {n_iter}")
-                else:
+                        raise_test_failure_message(
+                            f"Expected less than 30 iterations with Anderson acceleration, got {n_iter}"
+                        )
+                elif anderson_activation_threshold <= 0.0: # no Anderson
                     if not n_iter > 60:
-                        raise_test_failure_message(f"Expected more than 60 iterations without Anderson acceleration, got {n_iter}")
+                        raise_test_failure_message(
+                            f"Expected more than 60 iterations without Anderson acceleration, got {n_iter}"
+                        )
             else:
-                if with_anderson_acceleration:
+                if anderson_activation_threshold == ACADOS_INFTY: # full Anderson
                     if not n_iter < 27:
-                        raise_test_failure_message(f"Expected less than 27 iterations with Anderson acceleration, got {n_iter}")
-                else:
+                        raise_test_failure_message(
+                            f"Expected less than 27 iterations with Anderson acceleration, got {n_iter}"
+                        )
+                elif anderson_activation_threshold <= 0.0: # no Anderson
                     if not n_iter > 200:
-                        raise_test_failure_message(f"Expected more than 200 iterations without Anderson acceleration, got {n_iter}")
+                        raise_test_failure_message(
+                            f"Expected more than 200 iterations without Anderson acceleration, got {n_iter}"
+                        )
 
     # checks
     ref_sol = sol_list[0]
     for i, sol in enumerate(sol_list[1:]):
         if not ref_sol.allclose(sol, atol=1e-4):
-            print(f"Solution mismatch for {labels[i]}: difference: {(ref_sol-sol).inf_norm()}")
+            print(
+                f"Solution mismatch for {labels[i]}: difference: {(ref_sol-sol).inf_norm()}"
+            )
 
         else:
             print(f"Solution for {labels[i]} matches reference solution.")
@@ -145,27 +187,49 @@ def main(
     plot_convergence(
         kkt_norm_list,
         labels,
-        fig_filename=f"convergence_{'slack' if with_abs_cost else ''}_{variant}_{tol}_furuta.png" if store_plots else None,
+        figsize=(7.0, 4.0),
+        fig_filename=(
+            f"convergence_{'slack' if with_abs_cost else ''}_{variant}_{tol}_furuta.png"
+            if store_plots
+            else None
+        ),
         title=f"Furuta pendulum {'slack' if with_abs_cost else ''} OCP with tolerance {tol}",
-        show_plot=False
+        show_plot=False,
     )
     if plot_contraction:
         plot_contraction_rates(
             contraction_rates_list,
             labels,
-            fig_filename=f"contraction_rates_{'slack' if with_abs_cost else ''}_{variant}_{tol}_furuta.png" if store_plots else None,
-            show_plot=False
+            fig_filename=(
+                f"contraction_rates_{'slack' if with_abs_cost else ''}_{variant}_{tol}_furuta.png"
+                if store_plots
+                else None
+            ),
+            show_plot=False,
         )
     if plot_trajectory:
-        plot_furuta_pendulum(t_grid, ref_sol.x.reshape((N_HORIZON + 1, -1)), ref_sol.u.reshape((N_HORIZON, -1)), UMAX, plt_show=False)
+        plot_furuta_pendulum(
+            t_grid,
+            ref_sol.x.reshape((N_HORIZON + 1, -1)),
+            ref_sol.u.reshape((N_HORIZON, -1)),
+            UMAX,
+            plt_show=False,
+        )
         sol1 = sol_list[1]
-        plot_furuta_pendulum(t_grid, sol1.x.reshape((N_HORIZON + 1, -1)), sol1.u.reshape((N_HORIZON, -1)), UMAX, plt_show=False)
+        plot_furuta_pendulum(
+            t_grid,
+            sol1.x.reshape((N_HORIZON + 1, -1)),
+            sol1.u.reshape((N_HORIZON, -1)),
+            UMAX,
+            plt_show=False,
+        )
         plt.show()
+
 
 if __name__ == "__main__":
     plot_trajectory = False
-    store_plots = False
-    anderson_settings = [(False, ACADOS_INFTY), (True, ACADOS_INFTY), (True, 1e2), (True, 1e1), (True, 1e0)]
+    store_plots = True
+    anderson_settings = [0.0, ACADOS_INFTY, 1e2, 1e1, 1e0]
 
     main(anderson_settings, "GAUSS_NEWTON", with_abs_cost=False, plot_trajectory=plot_trajectory, store_plots=store_plots)
     main(anderson_settings, "GAUSS_NEWTON", plot_trajectory=plot_trajectory, store_plots=store_plots)
@@ -173,4 +237,3 @@ if __name__ == "__main__":
     # Below case shows that AA with very loose tolerance can slow down convergence.
     main(anderson_settings, "EXACT", plot_trajectory=plot_trajectory, tol=1e-1, ignore_checks=True, store_plots=store_plots)
     plt.show()
-
