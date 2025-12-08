7d2cd8bb2266f333c37ca3f40cc5d8c8e9cb101a
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-12-08 16:25:57 +0100

    load ocp and test it

---
+++ examples/acados_python/tests/load_ocp.py
@@ -0,0 +1,24 @@
+import json, os
+import warnings
+import numpy as np
+from acados_template import AcadosOcp, AcadosOcpSolver
+
+
+def test_render_json(json_file):
+
+    # load json, store options in object
+    with open(json_file, 'r') as f:
+        acados_ocp_json = json.load(f)
+    acados_ocp_json['json_file'] = os.path.abspath(json_file)
+
+    warnings.filterwarnings("ignore", message=".*not in dictionary.*", category=UserWarning)
+    ocp = AcadosOcp().from_dict(acados_ocp_json)
+
+    ocp.render_templates()
+
+    # solver = AcadosOcpSolver(ocp)
+
+
+if __name__ == "__main__":
+    json_file = "acados_ocp.json"
+    test_render_json(json_file)
