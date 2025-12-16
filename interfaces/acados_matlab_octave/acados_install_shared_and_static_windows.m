6e64ee3a3f01c6ab51bb3e623bd65e5f69ec9cc5
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-12-16 11:12:15 +0100

    fix option handling in acados install script

--- interfaces/acados_matlab_octave/acados_install_shared_and_static_windows.m
+++ interfaces/acados_matlab_octave/acados_install_shared_and_static_windows.m
@@ -10,13 +10,18 @@ function acados_install_shared_and_static_windows(varargin)
         error('options BUILD_SHARED_LIBS should not be contained in cmake config.')
     end
 
-    disp('installing acados with shared libs')
-    acados_install_windows('-DBUILD_SHARED_LIBS=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5')
+    % install shared libs
+    disp('installing acados with shared libs');
+    acados_install_windows(['-DBUILD_SHARED_LIBS=ON ', cmakeConfigString])
+
+    % move shared libs to bin
     acados_root_dir = getenv('ACADOS_INSTALL_DIR');
     acados_bin_path = fullfile(acados_root_dir, "bin");
     acados_lib_path = fullfile(acados_root_dir, "lib");
     [status, message ] = movefile(fullfile(acados_lib_path, '*.lib'), acados_bin_path);
 
-    disp('installing acados with static libs')
-    acados_install_windows('-DBUILD_SHARED_LIBS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5')
+    % install static libs
+    disp('installing acados with static libs');
+    acados_install_windows(['-DBUILD_SHARED_LIBS=OFF ', cmakeConfigString])
+
 end
