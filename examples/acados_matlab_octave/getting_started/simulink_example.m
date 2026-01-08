7cc9074196af6cf597b2070414e2ac174a0edd69
Josip Kir Hromatko <36133788+josipkh@users.noreply.github.com>
2026-01-08 15:39:39 +0100

    Simulink: fix example (#1743)
    This PR fixes a Simulink example in the `getting_started` folder (not
    tested on CI), which was failing due to a dimension mismatch.
    I also slightly modified the `test_all_examples.m` script to avoid some
    warnings when running locally on Windows.

--- examples/acados_matlab_octave/getting_started/simulink_example.m
+++ examples/acados_matlab_octave/getting_started/simulink_example.m
@@ -1,10 +1,9 @@
 %% Simulink example
-%
 clear all; clc;
 
 %% Run minimal example
 % get default simulink_opts
-simulink_opts = get_acados_simulink_opts;
+simulink_opts = get_acados_simulink_opts();
 minimal_example_ocp;
 
 
@@ -18,18 +17,20 @@ make_sfun_sim; % integrator
 %% Copy Simulink example blocks into c_generated_code
 source_folder = fullfile(pwd, '..');
 target_folder = pwd;
-copyfile( fullfile(source_folder, 'simulink_model_integrator.slx'), target_folder );
-copyfile( fullfile(source_folder, 'simulink_model_closed_loop.slx'), target_folder );
+copyfile(fullfile(source_folder, 'simulink_model_integrator.slx'), ...
+         fullfile(target_folder, 'simulink_model_integrator_copy.slx'));
+copyfile(fullfile(source_folder, 'simulink_model_closed_loop.slx'), ...
+         fullfile(target_folder, 'simulink_model_closed_loop_copy.slx'));
 
 
 %% Open Simulink example blocks
-open_system(fullfile(target_folder, 'simulink_model_integrator'))
-open_system(fullfile(target_folder, 'simulink_model_closed_loop'))
+open_system(fullfile(target_folder, 'simulink_model_integrator_copy'))
+open_system(fullfile(target_folder, 'simulink_model_closed_loop_copy'))
 
 
 %% Run the models
 try
-    sim('simulink_model_integrator.slx');
+    sim('simulink_model_integrator_copy.slx');
     cd ..
 catch
     cd ..
@@ -38,9 +39,10 @@ end
 
 try
     cd c_generated_code
-    sim('simulink_model_closed_loop.slx');
+    sim('simulink_model_closed_loop_copy.slx');
     cd ..
 catch
     cd ..
     error('Simulink closed loop example failed')
 end
+disp('Both simulations finished successfully.')
