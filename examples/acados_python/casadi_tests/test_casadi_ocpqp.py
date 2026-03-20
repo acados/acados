7b023f41fd50224460d08ecd307f41d40845203a
Jingtao Xiong <1450062579@qq.com>
2026-03-20 18:22:02 +0100

    modify test

--- examples/acados_python/casadi_tests/test_casadi_ocpqp.py
+++ examples/acados_python/casadi_tests/test_casadi_ocpqp.py
@@ -1,40 +1,24 @@
-import os
 from acados_template import AcadosOcpQpSolver, AcadosCasadiOcpQpSolver, AcadosOcpQp, AcadosOcpQpOptions
 import numpy as np
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
 
-json_dir = os.path.abspath(os.path.join(script_dir, '..', 'tests', 'qp_test'))
-
-sqp_qp_sol_pairs = [
-    (os.path.join(json_dir, 'last_qp_nonuniform_pendulum.json'),
-     os.path.join(json_dir, 'sqp_sol_nonuniform_pendulum.json')),
-    (os.path.join(json_dir, 'last_qp_one_sided_test.json'),
-     os.path.join(json_dir, 'sqp_sol_one_sided_test.json')),
-]
-
-# NOTE: create test cases in SQP example using:
-    # id = 'xxx'
-    # ocp_solver.dump_last_qp_to_json(filename=f'last_qp_{id}.json', overwrite=True)
-    # ocp_solver.store_iterate(filename=f'sqp_sol_{id}.json', overwrite=True)
-
-for qp_solver in ['PARTIAL_CONDENSING_HPIPM']:
-    for qp_json_file, sqp_sol_file in sqp_qp_sol_pairs:
+qp_json_files = ['examples/acados_python/casadi_tests/qp_tests/prob_0.json',
+                'examples/acados_python/casadi_tests/qp_tests/pendulum_qp.json',]
+def main():
+    for qp_json_file in qp_json_files:
         qp = AcadosOcpQp.from_json(qp_json_file)
         opts = AcadosOcpQpOptions()
         opts.iter_max = 500
-        opts.qp_solver = qp_solver
+        opts.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
 
         acados_solver = AcadosOcpQpSolver(qp, opts=opts)
         status = acados_solver.solve()
-        iterate = acados_solver.get_iterate()
         acados_u = np.array([acados_solver.get(i, "u") for i in range(qp.N)])
         acados_x = np.array([acados_solver.get(i, "x") for i in range(qp.N+1)])
         acados_lam = np.concatenate([acados_solver.get(i, "lam") for i in range(qp.N+1)])
         acados_pi = np.concatenate([acados_solver.get(i, "pi") for i in range(qp.N)])
 
         casadi_solver = AcadosCasadiOcpQpSolver(qp)
-        casadi_solver.set_iterate(iterate)
         status = casadi_solver.solve()
         casadi_u = np.array([casadi_solver.get(i, "u") for i in range(qp.N)])
         casadi_x = np.array([casadi_solver.get(i, "x") for i in range(qp.N+1)])
@@ -47,3 +31,6 @@ for qp_solver in ['PARTIAL_CONDENSING_HPIPM']:
         assert np.allclose(casadi_lam, acados_lam, atol=1e-5), f"lam mismatch for {qp_json_file}"
         assert np.allclose(casadi_pi, acados_pi, atol=1e-5), f"pi mismatch for {qp_json_file}"
         print(f"QP solution matches for {qp_json_file}.")
+
+if __name__ == "__main__":
+    main()
