2b668a887848c9bb137aca2809b73caf39f8678c
Jonathan Frey <jonathanpaulfrey@gmail.com>
2026-04-20 13:53:58 +0200

    use argparse

--- examples/acados_python/pendulum_on_cart/solution_sensitivities/test_solution_sens_and_exact_hess.py
+++ examples/acados_python/pendulum_on_cart/solution_sensitivities/test_solution_sens_and_exact_hess.py
@@ -29,6 +29,7 @@
 #
 
 import sys
+import argparse
 sys.path.insert(0, '../common')
 
 from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver, latexify_plot, casadi_length
@@ -142,7 +143,7 @@ def create_solver(hessian_approx, linearized_dynamics=False, discrete=False, par
         # implementation detail to avoid nonlinear-least squares module.
         ocp.translate_cost_to_external_cost()
 
-    acados_ocp_solver = AcadosOcpSolver(ocp, json_file=f'solver_f{hessian_approx}.json')
+    acados_ocp_solver = AcadosOcpSolver(ocp, json_file=f'solver_f{hessian_approx}.json', verbose=False)
 
     return acados_ocp_solver
 
@@ -422,27 +423,6 @@ def compute_K(solver: AcadosOcpSolver):
     K_regularized = -np.linalg.solve(P_mat_regularized, S_mat + B_mat.T @ P_mat @ A_mat)
     return K_mat, K_regularized
 
-
-def run_hessian_comparison(linearized_dynamics=False, discrete=False):
-    casadi_solver, lag_hess_fun, lbg, ubg = create_casadi_solver(linearized_dynamics=linearized_dynamics, discrete=discrete)
-
-    acados_ocp_solver_gn = create_solver(hessian_approx='GAUSS_NEWTON', linearized_dynamics=linearized_dynamics, discrete=discrete)
-    acados_ocp_solver_exact = create_solver(hessian_approx='EXACT', linearized_dynamics=linearized_dynamics, discrete=discrete)
-
-    x0 = X0.copy()
-    x0[1] = 0.1
-
-    nlp_sol = casadi_solver(p=x0, lbg=lbg, ubg=ubg)
-    # print(f"{nlp_sol=}")
-    casadi_hess_l = lag_hess_fun(x=nlp_sol['x'], p=x0, lam_f=1.0, lam_g=nlp_sol['lam_g'])['triu_hess_gamma_x_x']
-    casadi_hess = ca.triu2symm(ca.triu(casadi_hess_l)).full()
-    acados_ocp_solver_gn.solve_for_x0(x0)
-    iterate = acados_ocp_solver_gn.get_flat_iterate()
-    acados_ocp_solver_exact.set_iterate(iterate)
-    acados_ocp_solver_exact.solve_for_x0(x0, fail_on_nonzero_status=False, print_stats_on_failure=False)
-
-    _ = compare_hessian(casadi_hess, acados_ocp_solver_exact)
-
 def compare_hessian(casadi_hess, acados_solver: AcadosOcpSolver):
     offset = 0
     min_eigv_total = 1e12
@@ -488,7 +468,40 @@ def compare_hessian(casadi_hess, acados_solver: AcadosOcpSolver):
 
     return min_eigv_total, min_abs_eigv, hess_error_norm_total, min_abs_eig_proj_hess, min_eig_proj_hess, cond_proj_hess, min_eig_P, min_abs_eig_P
 
+
 if __name__ == "__main__":
-    sensitivity_experiment(linearized_dynamics=False, parametric_x0=True, discrete=True)
-    sensitivity_experiment(linearized_dynamics=False, parametric_x0=False, discrete=True)
-    run_hessian_comparison(linearized_dynamics=False, discrete=True)
+    parser = argparse.ArgumentParser(
+        description="Run sensitivity experiment with configurable parameters."
+    )
+    parser.add_argument(
+        "--linearized-dynamics",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Use linearized dynamics.",
+    )
+    parser.add_argument(
+        "--parametric-x0",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Use parametric x0 constraint.",
+    )
+    parser.add_argument(
+        "--discrete",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Use discrete dynamics.",
+    )
+    parser.add_argument(
+        "--show",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Display plots.",
+    )
+    args = parser.parse_args()
+
+    sensitivity_experiment(
+        linearized_dynamics=args.linearized_dynamics,
+        parametric_x0=args.parametric_x0,
+        discrete=args.discrete,
+        show=args.show,
+    )
