304e865bce75a3c4d8350a194cef7cc3ad77b375
sandmaennchen <katrin.baumgaertner@posteo.de>
2026-08-03 16:00:40 +0200

    simulink

--- interfaces/acados_template/acados_template/acados_ocp_options.py
+++ interfaces/acados_template/acados_template/acados_ocp_options.py
@@ -387,17 +387,17 @@ class AcadosOcpOptions:
 
             // This is allowed to allocate memory and store the pointer to it into capsule->custom_update_memory.
 
-            int custom_update_init_function([model.name]_solver_capsule* capsule);
+            int custom_update_init_function([name]_solver_capsule* capsule);
 
             // Custom update function that can be called between solver calls
 
-            int custom_update_function([model.name]_solver_capsule* capsule, double* data, int data_len);
+            int custom_update_function([name]_solver_capsule* capsule, double* data, int data_len);
 
             // Called just before destroying the solver.
 
             // Responsible to free allocated memory, stored at capsule->custom_update_memory.
 
-            int custom_update_terminate_function([model.name]_solver_capsule* capsule);
+            int custom_update_terminate_function([name]_solver_capsule* capsule);
 
         Default: ''.
         """
