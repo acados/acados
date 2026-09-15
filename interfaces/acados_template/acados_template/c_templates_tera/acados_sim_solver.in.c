9e3ee984a82802406934e651644f684a2f0d3d13
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2026-09-15 13:38:58 +0200

    Don't silently ignore `jac_reuse` when doing codegen for a sim problem (#1929)

--- interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.c
+++ interfaces/acados_template/acados_template/c_templates_tera/acados_sim_solver.in.c
@@ -334,6 +334,8 @@ int {{ name }}_acados_sim_create({{ name }}_sim_solver_capsule * capsule)
     sim_opts_set({{ name }}_sim_config, {{ name }}_sim_opts, "num_stages", &tmp_int);
     tmp_int = {{ solver_options.sim_method_num_steps }};
     sim_opts_set({{ name }}_sim_config, {{ name }}_sim_opts, "num_steps", &tmp_int);
+    tmp_bool = {{ solver_options.sim_method_jac_reuse }};
+    sim_opts_set({{ name }}_sim_config, {{ name }}_sim_opts, "jac_reuse", &tmp_bool);
 
     // options that are not available to AcadosOcpSolver
     //  (in OCP they will be determined by other options, like exact_hessian)
