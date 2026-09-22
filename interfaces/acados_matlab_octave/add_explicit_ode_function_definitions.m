26647b9fe2f459803b666e7a37e4d181ba94263f
Jonathan Frey <jonathanpaulfrey@gmail.com>
2026-09-22 13:36:02 +0200

    MATLAB: forgotten files

---
+++ interfaces/acados_matlab_octave/add_explicit_ode_function_definitions.m
@@ -0,0 +1,111 @@
+%
+% Copyright (c) The acados authors.
+%
+% This file is part of acados.
+%
+% The 2-Clause BSD License
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+% this list of conditions and the following disclaimer in the documentation
+% and/or other materials provided with the distribution.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.;
+
+%
+
+
+% Shared implementation for the explicit ODE code generation: ERK and ERK_WITH_COST
+function add_explicit_ode_function_definitions(context, model, model_dir, x, f_expl)
+
+    import casadi.*
+
+    %% load model
+    u = model.u;
+    p = model.p;
+    nx = length(x);
+    nu = length(u);
+    np = length(p);
+
+    % check type
+    if isa(x, 'casadi.SX')
+        isSX = true;
+    else
+        isSX = false;
+    end
+
+    % setup expressions
+    if isSX
+        Sx = SX.sym('Sx', nx, nx);
+        Su = SX.sym('Su', nx, nu);
+        lambdaX = SX.sym('lambdaX', nx, 1);
+        vdeX = SX.zeros(nx, nx);
+        vdeU = SX.zeros(nx, nu) + jacobian(f_expl, u);
+        if context.opts.sens_forw_p
+            Sp   = SX.sym('Sp', nx, np);
+            vdeP = SX.zeros(nx, np) + jacobian(f_expl, p);  % f_p
+        end
+    else
+        Sx = MX.sym('Sx', nx, nx);
+        Su = MX.sym('Su', nx, nu);
+        lambdaX = MX.sym('lambdaX', nx, 1);
+        vdeX = MX.zeros(nx, nx);
+        vdeU = MX.zeros(nx, nu) + jacobian(f_expl, u);
+        if context.opts.sens_forw_p
+            Sp   = MX.sym('Sp', nx, np);
+            vdeP = MX.zeros(nx, np) + jacobian(f_expl, p);  % f_p
+        end
+    end
+
+    vdeX = vdeX + jtimes(f_expl, x, Sx);
+    vdeU = vdeU + jtimes(f_expl, x, Su);
+
+    if context.opts.sens_forw_p
+        vdeP = vdeP + jtimes(f_expl, x, Sp);   % A*Sp + f_p
+    end
+
+    % 'true' at the end tells to transpose the jacobian before multiplication => reverse mode
+    adj = jtimes(f_expl, [x;u], lambdaX, true);
+
+    if context.opts.generate_hess
+        S_forw = vertcat(horzcat(Sx, Su), horzcat(zeros(nu,nx), eye(nu)));
+        hess = S_forw.'*jtimes(adj, [x;u], S_forw);
+        % vectorized lower triangular Hessian
+        hess_vec = hess(hess.sparsity().makeDense().get_lower()+1);
+    end
+
+    fun_name = [model.name,'_expl_ode_fun'];
+    context.add_function_definition(fun_name, {x, u, p}, {f_expl}, model_dir, 'dyn');
+
+    fun_name = [model.name,'_expl_vde_forw'];
+    context.add_function_definition(fun_name, {x, Sx, Su, u, p}, {f_expl, vdeX, vdeU}, model_dir, 'dyn');
+
+    fun_name = [model.name,'_expl_vde_adj'];
+    context.add_function_definition(fun_name, {x, lambdaX, u, p}, {adj}, model_dir, 'dyn');
+
+    if context.opts.generate_hess
+        fun_name = [model.name,'_expl_ode_hess'];
+        context.add_function_definition(fun_name, {x, Sx, Su, lambdaX, u, p}, {adj, hess_vec}, model_dir, 'dyn');
+    end
+
+    % param-direction forward VDE
+    if context.opts.sens_forw_p
+        fun_name = [model.name,'_expl_vde_forw_p'];
+        context.add_function_definition(fun_name, {x, Sp, u, p}, {vdeP}, model_dir, 'dyn');
+    end
+end
---
+++ interfaces/acados_matlab_octave/generate_c_code_explicit_ode_with_cost_state.m
@@ -0,0 +1,47 @@
+%
+% Copyright (c) The acados authors.
+%
+% This file is part of acados.
+%
+% The 2-Clause BSD License
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+% this list of conditions and the following disclaimer in the documentation
+% and/or other materials provided with the distribution.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.;
+
+%
+
+
+function generate_c_code_explicit_ode_with_cost_state(context, model, model_dir)
+
+    import casadi.*
+
+    % check type
+    if isa(model.x, 'casadi.SX')
+        cost_state = SX.sym('cost_state');
+    else
+        cost_state = MX.sym('cost_state');
+    end
+    x_with_cost = [model.x; cost_state];
+
+    add_explicit_ode_function_definitions(context, model, model_dir, x_with_cost, model.f_expl_expr_with_cost);
+end
+
